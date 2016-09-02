cb041f348b4e6fbd2dd0c0692bc3be1289ed99fc
James Robinson <jamesr@google.com>
2016-09-01 18:06:18 -0700

    Update dart_app template and demonstrate deps in hello_dart
    This adds a (trivial) package dependency to hello_dart.dartx and
    updates the dart_app template to know about infer_package_name.
    
    Change-Id: I60ecd9bc9b50d641231b9091f8c15b7392a9dd89

--- runtime/dart_runner/examples/hello_dart/greeting/greeting.dart
+++ runtime/dart_runner/examples/hello_dart/greeting/greeting.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:apps.dart_content_handler.examples.hello_dart.greeting/greeting.dart';
-
-void main(List args, Object request) {
-  print(greeting() + ', Dart!');
+String greeting() {
+    return 'Hello';
 }
