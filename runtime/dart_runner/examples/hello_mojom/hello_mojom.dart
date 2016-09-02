81b6df947de82f1358136c62f6453648fd2df1df
James Robinson <jamesr@google.com>
2016-09-01 21:49:04 -0700

    Update dart_app template for .packages move and add mojom example
    This updates the expected .packages file to include the target name to
    match the rest of the tree and adds a very simple example of using
    Dart types generated from mojom files.
    
    Change-Id: Id57e6d96fe0c1255a414d21abed472a4b1c6c513

---
+++ runtime/dart_runner/examples/hello_mojom/hello_mojom.dart
@@ -0,0 +1,12 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:mojo.services.geometry.interfaces/geometry.mojom.dart';
+
+void main(List args, Object request) {
+    Point p = new Point();
+    p.x = 1;
+    p.y = 2;
+    print('Point is $p');
+}
