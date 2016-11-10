4baef78c1a1dbc36fb2c48d922d926691c0ed009
Adam Barth <abarth@chromium.org>
2016-11-10 00:29:57 -0800

    Update hello_app_dart for new FIDL bindings
    Change-Id: I7657c576af98c30c0113acb6eb75499ca912b630

--- runtime/dart_runner/examples/hello_app_dart/main.dart
+++ runtime/dart_runner/examples/hello_app_dart/main.dart
@@ -2,12 +2,18 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:lib.fidl.dart/core.dart' as core;
+import 'package:lib.fidl.dart/bindings.dart';
 
 import 'package:apps.modular.lib.app.dart/app.dart';
 import 'package:apps.modular.examples.hello_app/hello.fidl.dart';
 
 class HelloImpl extends Hello {
+  final HelloBinding _binding = new HelloBinding();
+
+  void bind(InterfaceRequest<Hello> request) {
+    _binding.bind(this, request);
+  }
+
   @override
   void say(String request, void callback(String response)) {
     callback((request == "hello") ? "hola from Dart!" : "adios from Dart!");
@@ -17,7 +23,7 @@ class HelloImpl extends Hello {
 void main(List<String> args) {
   ApplicationContext context = new ApplicationContext.fromStartupInfo();
 
-  context.outgoingServices.addServiceForName((core.Channel channel) {
-    new HelloStub.fromChannel(channel, new HelloImpl());
+  context.outgoingServices.addServiceForName((request) {
+    new HelloImpl().bind(request);
   }, Hello.serviceName);
 }
