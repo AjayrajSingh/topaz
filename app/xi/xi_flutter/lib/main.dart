932bcef1fa014a5a7a2dbb1b5ee153437d6b8aa2
Jason Campbell <jasoncampbell@google.com>
2017-01-06 15:59:27 -0800

    Add Fuchsia & Flutter boilerplate
    * Adds initial BUILD.gn targets for //apps/xi.
    * Adds a very basic FX Module that communicates with xi-core.
    * Moves rust dir to modules/xi-core.
    * Updates copyright to 2017.
    
    Change-Id: I74403fa045d72fa7f7b41397fa8a8d3ec7af4d45

---
+++ app/xi/xi_flutter/lib/main.dart
@@ -0,0 +1,40 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:widgets/widgets.dart';
+
+void main() {
+  runApp(new App());
+}
+
+/// A light wrapper around [XiApp] for tracking state changes for handling UI
+/// actions.
+class App extends StatefulWidget {
+  @override
+  AppState createState() => new AppState();
+}
+
+/// State for [App].
+class AppState extends State<App> {
+  /// State value for holding the [message] populated as a result of UI
+  /// actions triggered via [handlePingButtonPressed].
+  String message;
+
+  /// Handler passed into [XiApp] for negotiaing IPC calls to the xi-core
+  /// service. Currently this is unsupported for vanilla Flutter.
+  void handlePingButtonPressed() {
+    setState(() {
+      message = 'Pinging Xi Core is not implemented for vanilla Flutter.';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new XiApp(
+      message: message,
+      onPingButtonPressed: handlePingButtonPressed,
+    );
+  }
+}
