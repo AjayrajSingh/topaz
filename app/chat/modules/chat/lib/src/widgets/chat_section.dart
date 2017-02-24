3cd61f5d7b8e4a1a99a4b38e0c7824bd0211e14b
David Yang <dayang@google.com>
2017-02-24 10:56:07 -0800

    feat(chat): Chat Section
    UI Widget that contains a consecutive sequence of Chat Bubbles from
    the same user.
    
    SO-205 #done
    
    Change-Id: Id4abd5a28491c039a1aa6713abfb420f7088ea9d

---
+++ app/chat/modules/chat/lib/src/widgets/chat_section.dart
@@ -0,0 +1,126 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:meta/meta.dart';
+import 'package:models/user.dart';
+import 'package:widgets/user.dart';
+import 'package:util/time_util.dart';
+
+import 'chat_bubble.dart';
+
+/// UI Widget that represents a consecutive sequence of [ChatBubble]s by the
+/// same user.
+///
+/// Like the [ChatBubble], the [ChatSection] also has an orientation
+/// (left/right) depending on if the user is a sender/recipient.
+///
+/// The orientation for the children [ChatBubble]s should be the same as the
+/// [ChatSection]
+class ChatSection extends StatelessWidget{
+
+  /// User of the given chat section
+  final User user;
+
+  /// List of chat bubbles to show inside the section
+  final List<ChatBubble> chatBubbles;
+
+  /// Orientation (left/right) of the chat section
+  final ChatBubbleOrientation orientation;
+
+  /// Timestamp for chat section
+  final DateTime timestamp;
+
+  /// Constructor
+  ChatSection({
+    Key key,
+    @required this.user,
+    @required this.chatBubbles,
+    this.timestamp,
+    ChatBubbleOrientation orientation,
+  })
+      : orientation = orientation ?? ChatBubbleOrientation.left,
+      super(key: key) {
+    assert(user != null);
+    assert(chatBubbles != null);
+    chatBubbles.forEach((ChatBubble bubble) {
+      assert(bubble.orientation == orientation);
+    });
+  }
+
+  String get _displayDate {
+    if(timestamp == null) {
+      return '';
+    }
+    if (TimeUtil.isSameDay(new DateTime.now(), timestamp)) {
+      return new DateFormat.jm().format(timestamp);
+    } else {
+      return new DateFormat.MMMd().format(timestamp);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget alphatar = new Container(
+      margin: const EdgeInsets.symmetric(horizontal: 4.0),
+      child: new Alphatar.fromUser(user: user),
+    );
+    Widget chatColumn = new Expanded(
+      child: new Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: orientation == ChatBubbleOrientation.left ?
+            CrossAxisAlignment.start : CrossAxisAlignment.end,
+        children: chatBubbles,
+      ),
+    );
+
+    // Order avatar & chat bubbles depending on orientation
+    List<Widget> rowChildren;
+    if(orientation == ChatBubbleOrientation.left) {
+      rowChildren = <Widget>[
+        alphatar,
+        chatColumn,
+      ];
+    } else {
+      rowChildren = <Widget>[
+        chatColumn,
+        alphatar,
+      ];
+    }
+    Widget sectionContainer = new Container(
+      margin: const EdgeInsets.only(bottom: 4.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: rowChildren,
+      ),
+    );
+
+    // Add timestamp if it is given
+    if(timestamp != null) {
+      return new Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: orientation == ChatBubbleOrientation.left ?
+            CrossAxisAlignment.start : CrossAxisAlignment.end,
+        children: <Widget>[
+          sectionContainer,
+          new Container(
+            margin: orientation == ChatBubbleOrientation.left ?
+                const EdgeInsets.only(left: 50.0) :
+                const EdgeInsets.only(right: 50.0),
+            child: new Text(
+              _displayDate,
+              style: new TextStyle(
+                fontSize: 12.0,
+                color: Colors.grey[500],
+              ),
+            ),
+          ),
+        ],
+      );
+    } else {
+      return sectionContainer;
+    }
+  }
+}
--- app/chat/modules/chat/lib/src/widgets/home_page.dart
+++ app/chat/modules/chat/lib/src/widgets/home_page.dart
@@ -3,8 +3,10 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:models/user.dart';
 
 import 'chat_bubble.dart';
+import 'chat_section.dart';
 
 /// MyHomePage widget.
 class MyHomePage extends StatelessWidget {
@@ -21,21 +23,48 @@ class MyHomePage extends StatelessWidget {
         title: new Text(title),
       ),
       body: new Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          new ChatBubble(
+          new ChatSection(
+            user: new User(name: 'Coco', email: 'Coco@cute'),
             orientation: ChatBubbleOrientation.left,
-            child: new Text(
-              'Hello',
-              style: const TextStyle(color: Colors.white),
-            ),
+            timestamp: new DateTime.now(),
+            chatBubbles: <ChatBubble>[
+              new ChatBubble(
+                orientation: ChatBubbleOrientation.left,
+                child: new Text(
+                  'Hello',
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+              new ChatBubble(
+                orientation: ChatBubbleOrientation.left,
+                child: new Text(
+                  'Is it me you\'re looking for?',
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
           ),
-          new ChatBubble(
-            orientation: ChatBubbleOrientation.left,
-            child: new Text(
-              ' Is it me you\'re looking for?',
-              style: const TextStyle(color: Colors.white),
-            ),
+          new ChatSection(
+            user: new User(name: 'Yoyo', email: 'Yoyo@cute'),
+            orientation: ChatBubbleOrientation.right,
+            timestamp: new DateTime.now(),
+            chatBubbles: <ChatBubble>[
+              new ChatBubble(
+                backgroundColor: Colors.grey[200],
+                orientation: ChatBubbleOrientation.right,
+                child: new Text(
+                  'Cause I wonder where you are...',
+                ),
+              ),
+              new ChatBubble(
+                backgroundColor: Colors.grey[200],
+                orientation: ChatBubbleOrientation.right,
+                child: new Text(
+                  'and I wonder what you do',
+                ),
+              ),
+            ],
           ),
         ],
       ),
