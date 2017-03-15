5c4bf3e026bab2011c09ddd658fe5abd2de980e8
Greg Simon <gregsimon@gmail.com>
2017-03-15 12:44:16 -0700

    add Fuchsia license

--- app/dashboard/lib/dashboard_app.dart
+++ app/dashboard/lib/dashboard_app.dart
@@ -1,3 +1,7 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 import 'package:html/parser.dart' show parse;
 import 'package:html/dom.dart' as dom;
