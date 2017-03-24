13f6145ff75ddc7833a53314b509f5735b2f7da7
Andrew Wilson <apwilson@google.com>
2017-03-24 11:44:02 -0700

    Fix url.
    Change-Id: Ifb06adccb3a0805db3200630fd2b8a87b43e3634

--- runtime/web_runner/lib/dart/web_view.dart
+++ runtime/web_runner/lib/dart/web_view.dart
@@ -4,4 +4,4 @@
 
 /// The path to web_view.
 const String kWebViewURL =
-    'https://fuchsia-build.storage.googleapis.com/apps/web_view/prebuilt_38c49f3f87ca35505e6249e25ff05e73ddf8fac7';
+    'https://fuchsia-build.storage.googleapis.com/apps/web_view/web_view_38c49f3f87ca35505e6249e25ff05e73ddf8fac7';
