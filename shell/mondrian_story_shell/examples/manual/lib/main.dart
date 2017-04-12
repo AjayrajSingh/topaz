4995ac6494892fdaddf0a0196091445459020658
Alan Gardner <alangardner@google.com>
2017-04-12 09:08:22 -0700

    Create example module for testing mondrian
    Introduces an example module which lets user start modules with custom
    relationships manually, to test the effects in the story shell.
    
    Change-Id: I646356c1b2db0e43bff270d67c037bf9445feeb3

---
+++ shell/mondrian_story_shell/examples/manual/lib/main.dart
@@ -0,0 +1,68 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:application.lib.app.dart/app.dart';
+import 'package:application.services/service_provider.fidl.dart';
+import 'package:apps.modular.services.story/link.fidl.dart';
+import 'package:apps.modular.services.module/module.fidl.dart';
+import 'package:apps.modular.services.module/module_context.fidl.dart';
+import 'package:lib.fidl.dart/bindings.dart';
+
+import 'package:flutter/widgets.dart';
+
+final ApplicationContext _appContext = new ApplicationContext.fromStartupInfo();
+
+/// This is used for keeping the reference around.
+ModuleImpl _module;
+
+void _log(String msg) {
+  print('[Manual Relationships Module] $msg');
+}
+
+class ModuleImpl extends Module {
+  final ModuleBinding _binding = new ModuleBinding();
+
+  final ModuleContextProxy _moduleContext = new ModuleContextProxy();
+  final LinkProxy _link = new LinkProxy();
+
+  void bind(InterfaceRequest<Module> request) {
+    _binding.bind(this, request);
+  }
+
+  @override
+  void initialize(
+      InterfaceHandle<ModuleContext> moduleContextHandle,
+      InterfaceHandle<Link> linkHandle,
+      InterfaceHandle<ServiceProvider> incomingServices,
+      InterfaceRequest<ServiceProvider> outgoingServices) {
+    _log('ModuleImpl::initialize call');
+
+    _moduleContext.ctrl.bind(moduleContextHandle);
+    _link.ctrl.bind(linkHandle);
+  }
+
+  @override
+  void stop(void done()) {
+    _log('ModuleImpl::stop call');
+
+    _moduleContext.ctrl.close();
+    _link.ctrl.close();
+
+    done();
+  }
+}
+
+/// Entry point for this module.
+void main() {
+  /// Add [ModuleImpl] to this application's outgoing ServiceProvider.
+  _appContext.outgoingServices.addServiceForName(
+    (request) {
+      _log('Received binding request for Module');
+      _module = new ModuleImpl()..bind(request);
+    },
+    Module.serviceName,
+  );
+
+  runApp(new Text("Manual Relationships."));
+}
