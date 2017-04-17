01ae037a43fdc6416e13cc55c7aaeefa2841dd82
David Yang <dayang@google.com>
2017-04-17 13:23:53 -0700

    chore: Adding Alphatar to lib.widgets
    Change-Id: I0446ffca2aa1b55be2c7f061efdd126e733f8eb2

---
+++ public/dart/widgets/lib/widgets.dart
@@ -0,0 +1,6 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file
+
+export 'src/widgets/alphatar.dart';
+export 'src/widgets/mondrian_spinner.dart';
