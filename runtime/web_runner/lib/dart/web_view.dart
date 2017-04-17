e60b920ad3c9712ab31ebadf58e1d1c5814abcf0
Ross Wang <rosswang@google.com>
2017-04-17 14:56:33 -0700

    Add a component.json for the web view
    Change-Id: Ie9fa973fff7ce52a1310a9dc8e3518c2e986751a

--- runtime/web_runner/lib/dart/web_view.dart
+++ runtime/web_runner/lib/dart/web_view.dart
@@ -3,5 +3,6 @@
 // found in the LICENSE file.
 
 /// The path to web_view.
+/// Keep in sync with web_view_component.json.
 const String kWebViewURL =
     'https://fuchsia-build.storage.googleapis.com/apps/web_view/web_view_38c49f3f87ca35505e6249e25ff05e73ddf8fac7';
