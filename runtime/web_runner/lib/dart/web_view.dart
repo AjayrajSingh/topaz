c38b46b57dd3e2f6122731f07bbc425563a3f7e6
Ross Wang <rosswang@google.com>
2017-04-19 11:32:03 -0700

    Updating web_view
    Change-Id: I0a14204b4a651535fff9ad82dcb4743f53ec76f8

--- runtime/web_runner/lib/dart/web_view.dart
+++ runtime/web_runner/lib/dart/web_view.dart
@@ -5,4 +5,4 @@
 /// The path to web_view.
 /// Keep in sync with web_view_component.json.
 const String kWebViewURL =
-    'https://fuchsia-build.storage.googleapis.com/apps/web_view/web_view_38c49f3f87ca35505e6249e25ff05e73ddf8fac7';
+    'https://fuchsia-build.storage.googleapis.com/apps/web_view/web_view_b034f1a588a20e87198b83fd37f4ff676b093b6c';
