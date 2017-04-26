1e5867497cf5b6ae042f49295061f790e08e9921
Vardhan Mudunuru <vardhan@google.com>
2017-04-26 15:37:38 -0700

    Update Module.Initialize() and StartModule(): pass Link by name, instead of interface
    Change-Id: Ic7e83bbfb87cf73c9f4f321e0d925de56b4b7a37

--- app/contacts/modules/contacts/lib/modular_main.dart
+++ app/contacts/modules/contacts/lib/modular_main.dart
@@ -28,7 +28,6 @@ class ModuleImpl extends Module {
   @override
   void initialize(
       InterfaceHandle<ModuleContext> moduleContextHandle,
-      InterfaceHandle<Link> linkHandle,
       InterfaceHandle<ServiceProvider> incomingServices,
       InterfaceRequest<ServiceProvider> outgoingServices) {}
 
