b14590c6ea7488a1a6c80871759026162e504eb6
Andrew Wilson <apwilson@google.com>
2017-05-15 11:26:43 -0700

    Refactor classes out of main.
    Change-Id: Ia41d67c43b01b0267ef5e5203d846cb58cfe9e73

--- shell/userpicker_device_shell/lib/main.dart
+++ shell/userpicker_device_shell/lib/main.dart
@@ -2,36 +2,25 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:async';
-
-import 'package:apps.modular.services.auth.account/account.fidl.dart';
-import 'package:apps.modular.services.device/user_provider.fidl.dart';
-import 'package:apps.mozart.lib.flutter/child_view.dart';
-import 'package:apps.mozart.services.views/view_token.fidl.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:lib.fidl.dart/bindings.dart';
-import 'package:lib.widgets/model.dart';
 import 'package:lib.widgets/modular.dart';
 
+import 'authentication_overlay.dart';
+import 'authentication_overlay_model.dart';
 import 'authentication_context_impl.dart';
 import 'child_constraints_changer.dart';
 import 'constraints_model.dart';
-import 'user_picker.dart';
+import 'screen_manager.dart';
 import 'user_picker_device_shell_model.dart';
-import 'user_picker_screen.dart';
-import 'user_watcher_impl.dart';
 
 void main() {
   GlobalKey screenManagerKey = new GlobalKey();
   ConstraintsModel constraintsModel = new ConstraintsModel();
 
   UserPickerDeviceShellModel model = new UserPickerDeviceShellModel();
-  _AuthenticationOverlayModel authenticationOverlayModel =
-      new _AuthenticationOverlayModel();
+  AuthenticationOverlayModel authenticationOverlayModel =
+      new AuthenticationOverlayModel();
   AuthenticationContextImpl authenticationContext =
       new AuthenticationContextImpl(
     onStartOverlay: authenticationOverlayModel.onStartOverlay,
@@ -47,15 +36,15 @@ void main() {
       child: new Stack(
         fit: StackFit.passthrough,
         children: <Widget>[
-          new _ScreenManager(
+          new ScreenManager(
             key: screenManagerKey,
             onLogout: model.refreshUsers,
             onAddUser: model.showNewUserForm,
             onRemoveUser: model.removeUser,
           ),
-          new ScopedModel<_AuthenticationOverlayModel>(
+          new ScopedModel<AuthenticationOverlayModel>(
             model: authenticationOverlayModel,
-            child: new _AuthenticationOverlay(),
+            child: new AuthenticationOverlay(),
           ),
         ],
       ),
@@ -81,258 +70,4 @@ void main() {
 
   constraintsModel.load(rootBundle);
   deviceShellWidget.advertise();
-}
-
-class _ScreenManager extends StatefulWidget {
-  final VoidCallback onLogout;
-  final VoidCallback onAddUser;
-  final OnRemoveUser onRemoveUser;
-
-  _ScreenManager({
-    Key key,
-    this.onLogout,
-    this.onAddUser,
-    this.onRemoveUser,
-  })
-      : super(key: key);
-
-  @override
-  _ScreenManagerState createState() => new _ScreenManagerState();
-}
-
-class _ScreenManagerState extends State<_ScreenManager>
-    with TickerProviderStateMixin {
-  final TextEditingController _userNameController = new TextEditingController();
-  final TextEditingController _serverNameController =
-      new TextEditingController();
-  final FocusNode _userNameFocusNode = new FocusNode();
-  final FocusNode _serverNameFocusNode = new FocusNode();
-  final Set<Account> _draggedUsers = new Set<Account>();
-
-  UserControllerProxy _userControllerProxy;
-  UserWatcherImpl _userWatcherImpl;
-
-  ChildViewConnection _childViewConnection;
-
-  AnimationController _transitionAnimation;
-  CurvedAnimation _curvedTransitionAnimation;
-
-  bool _addingUser = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _transitionAnimation = new AnimationController(
-      value: 0.0,
-      duration: const Duration(seconds: 1),
-      vsync: this,
-    );
-    _curvedTransitionAnimation = new CurvedAnimation(
-      parent: _transitionAnimation,
-      curve: Curves.fastOutSlowIn,
-      reverseCurve: Curves.fastOutSlowIn,
-    );
-    _curvedTransitionAnimation.addStatusListener(_onStatusChange);
-  }
-
-  @override
-  void dispose() {
-    _curvedTransitionAnimation.removeStatusListener(_onStatusChange);
-    super.dispose();
-    _userWatcherImpl?.close();
-    _userWatcherImpl = null;
-    _userControllerProxy?.ctrl?.close();
-    _userControllerProxy = null;
-  }
-
-  void _onStatusChange(_) => setState(() {});
-
-  @override
-  Widget build(BuildContext context) => new AnimatedBuilder(
-        animation: _transitionAnimation,
-        builder: (BuildContext context, Widget child) =>
-            _childViewConnection == null
-                ? child
-                : new Stack(
-                    fit: StackFit.passthrough,
-                    children: <Widget>[
-                      new ChildView(connection: _childViewConnection),
-                      new Opacity(
-                        opacity: 1.0 - _curvedTransitionAnimation.value,
-                        child: child,
-                      ),
-                    ],
-                  ),
-        child: new UserPickerScreen(
-          showBlackHole: _draggedUsers.isNotEmpty,
-          onAddUser: () {
-            //TODO(apwilson): Remove the delay.  It's a workaround to raw
-            // keyboard focus bug.
-            new Timer(
-              const Duration(milliseconds: 1000),
-              () => FocusScope.of(context).requestFocus(_userNameFocusNode),
-            );
-            widget.onAddUser?.call();
-          },
-          userPicker: new UserPicker(
-            onLoginRequest: _login,
-            onAddUserStarted: _addUserStarted,
-            onAddUserFinished: _addUserFinished,
-            userNameController: _userNameController,
-            serverNameController: _serverNameController,
-            userNameFocusNode: _userNameFocusNode,
-            serverNameFocusNode: _serverNameFocusNode,
-            loggingIn: _addingUser ||
-                (_childViewConnection != null &&
-                    (_curvedTransitionAnimation.status ==
-                            AnimationStatus.dismissed ||
-                        _curvedTransitionAnimation.status ==
-                            AnimationStatus.forward)),
-            onUserDragStarted: (Account account) => setState(() {
-                  _draggedUsers.add(account);
-                }),
-            onUserDragCanceled: (Account account) => setState(() {
-                  _draggedUsers.remove(account);
-                }),
-          ),
-          onRemoveUser: (Account account) => setState(() {
-                _draggedUsers.remove(account);
-                widget.onRemoveUser?.call(account);
-              }),
-        ),
-      );
-
-  void _addUserStarted() => setState(() {
-        _addingUser = true;
-      });
-
-  void _addUserFinished() => setState(() {
-        _addingUser = false;
-      });
-
-  void _login(String accountId, UserProvider userProvider) {
-    _userControllerProxy?.ctrl?.close();
-    _userControllerProxy = new UserControllerProxy();
-    _userWatcherImpl?.close();
-    _userWatcherImpl = new UserWatcherImpl(onUserLogout: () {
-      print('UserPickerDeviceShell: User logged out!');
-      _handleLogout();
-    });
-
-    final InterfacePair<ViewOwner> viewOwner = new InterfacePair<ViewOwner>();
-    final UserLoginParams params = new UserLoginParams()
-      ..accountId = accountId
-      ..viewOwner = viewOwner.passRequest()
-      ..userController = _userControllerProxy.ctrl.request();
-    userProvider?.login(params);
-    _userControllerProxy.watch(_userWatcherImpl.getHandle());
-
-    setState(() {
-      _childViewConnection = new ChildViewConnection(
-        viewOwner.passHandle(),
-        onAvailable: (ChildViewConnection connection) {
-          print('UserPickerDeviceShell: Child view connection available!');
-          _transitionAnimation.forward();
-        },
-        onUnavailable: (ChildViewConnection connection) {
-          print('UserPickerDeviceShell: Child view connection unavailable!');
-          _handleLogout();
-        },
-      );
-    });
-  }
-
-  void _handleLogout() {
-    setState(() {
-      widget.onLogout?.call();
-      _transitionAnimation.reverse();
-      // TODO(apwilson): Should not need to remove the child view connection but
-      // it causes a mozart deadlock in the compositor if you don't.
-      _childViewConnection = null;
-    });
-  }
-}
-
-class _AuthenticationOverlayModel extends Model implements TickerProvider {
-  ChildViewConnection _childViewConnection;
-
-  /// If not null, returns the handle of the current requested overlay.
-  ChildViewConnection get childViewConnection => _childViewConnection;
-
-  AnimationController _transitionAnimation;
-  CurvedAnimation _curvedTransitionAnimation;
-
-  CurvedAnimation get animation => _curvedTransitionAnimation;
-
-  /// Starts showing an overlay over all other content.
-  void onStartOverlay(InterfaceHandle<ViewOwner> overlay) {
-    _transitionAnimation = new AnimationController(
-      value: 0.0,
-      duration: const Duration(seconds: 1),
-      vsync: this,
-    );
-    _curvedTransitionAnimation = new CurvedAnimation(
-      parent: _transitionAnimation,
-      curve: Curves.fastOutSlowIn,
-      reverseCurve: Curves.fastOutSlowIn,
-    );
-    _childViewConnection = new ChildViewConnection(
-      overlay,
-      onAvailable: (ChildViewConnection connection) {
-        print('AuthenticationOverlayModel: Child view connection available!');
-        _transitionAnimation.forward();
-      },
-      onUnavailable: (ChildViewConnection connection) {
-        print('AuthenticationOverlayModel: Child view connection unavailable!');
-        _transitionAnimation.reverse();
-        // TODO(apwilson): Should not need to remove the child view
-        // connection but it causes a mozart deadlock in the compositor if you
-        // don't.
-        _childViewConnection = null;
-      },
-    );
-    notifyListeners();
-  }
-
-  /// Stops showing a previously started overlay.
-  void onStopOverlay() {
-    _transitionAnimation.reverse();
-    // TODO(apwilson): Should not need to remove the child view
-    // connection but it causes a mozart deadlock in the compositor if you
-    // don't.
-    _childViewConnection = null;
-    notifyListeners();
-  }
-
-  @override
-  Ticker createTicker(TickerCallback onTick) => new Ticker(onTick);
-}
-
-class _AuthenticationOverlay extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) =>
-      new ScopedModelDescendant<_AuthenticationOverlayModel>(
-        builder: (
-          BuildContext context,
-          Widget child,
-          _AuthenticationOverlayModel model,
-        ) =>
-            model.animation == null
-                ? new Offstage()
-                : new AnimatedBuilder(
-                    animation: model.animation,
-                    builder: (BuildContext context, Widget child) =>
-                        new Opacity(
-                          opacity: model.animation.value,
-                          child: child,
-                        ),
-                    child: new FractionallySizedBox(
-                      widthFactor: 0.75,
-                      heightFactor: 0.75,
-                      child: new ChildView(
-                        connection: model.childViewConnection,
-                      ),
-                    ),
-                  ),
-      );
 }
---
+++ shell/userpicker_device_shell/lib/screen_manager.dart
@@ -0,0 +1,192 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:apps.modular.services.auth.account/account.fidl.dart';
+import 'package:apps.modular.services.device/user_provider.fidl.dart';
+import 'package:apps.mozart.lib.flutter/child_view.dart';
+import 'package:flutter/material.dart';
+import 'package:lib.fidl.dart/bindings.dart';
+
+import 'user_picker.dart';
+import 'user_picker_screen.dart';
+import 'user_watcher_impl.dart';
+
+/// The root widget which displays all the other windows of this app.
+class ScreenManager extends StatefulWidget {
+  /// Called when the user logs out.
+  final VoidCallback onLogout;
+
+  /// Called when the user requests to add a user.
+  final VoidCallback onAddUser;
+
+  /// Called when the user requests to remove a user.
+  final OnRemoveUser onRemoveUser;
+
+  /// Constructor.
+  ScreenManager({
+    Key key,
+    this.onLogout,
+    this.onAddUser,
+    this.onRemoveUser,
+  })
+      : super(key: key);
+
+  @override
+  _ScreenManagerState createState() => new _ScreenManagerState();
+}
+
+class _ScreenManagerState extends State<ScreenManager>
+    with TickerProviderStateMixin {
+  final TextEditingController _userNameController = new TextEditingController();
+  final TextEditingController _serverNameController =
+      new TextEditingController();
+  final FocusNode _userNameFocusNode = new FocusNode();
+  final FocusNode _serverNameFocusNode = new FocusNode();
+  final Set<Account> _draggedUsers = new Set<Account>();
+
+  UserControllerProxy _userControllerProxy;
+  UserWatcherImpl _userWatcherImpl;
+
+  ChildViewConnection _childViewConnection;
+
+  AnimationController _transitionAnimation;
+  CurvedAnimation _curvedTransitionAnimation;
+
+  bool _addingUser = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _transitionAnimation = new AnimationController(
+      value: 0.0,
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    _curvedTransitionAnimation = new CurvedAnimation(
+      parent: _transitionAnimation,
+      curve: Curves.fastOutSlowIn,
+      reverseCurve: Curves.fastOutSlowIn,
+    );
+    _curvedTransitionAnimation.addStatusListener(_onStatusChange);
+  }
+
+  @override
+  void dispose() {
+    _curvedTransitionAnimation.removeStatusListener(_onStatusChange);
+    super.dispose();
+    _userWatcherImpl?.close();
+    _userWatcherImpl = null;
+    _userControllerProxy?.ctrl?.close();
+    _userControllerProxy = null;
+  }
+
+  void _onStatusChange(_) => setState(() {});
+
+  @override
+  Widget build(BuildContext context) => new AnimatedBuilder(
+        animation: _transitionAnimation,
+        builder: (BuildContext context, Widget child) =>
+            _childViewConnection == null
+                ? child
+                : new Stack(
+                    fit: StackFit.passthrough,
+                    children: <Widget>[
+                      new ChildView(connection: _childViewConnection),
+                      new Opacity(
+                        opacity: 1.0 - _curvedTransitionAnimation.value,
+                        child: child,
+                      ),
+                    ],
+                  ),
+        child: new UserPickerScreen(
+          showBlackHole: _draggedUsers.isNotEmpty,
+          onAddUser: () {
+            //TODO(apwilson): Remove the delay.  It's a workaround to raw
+            // keyboard focus bug.
+            new Timer(
+              const Duration(milliseconds: 1000),
+              () => FocusScope.of(context).requestFocus(_userNameFocusNode),
+            );
+            widget.onAddUser?.call();
+          },
+          userPicker: new UserPicker(
+            onLoginRequest: _login,
+            onAddUserStarted: _addUserStarted,
+            onAddUserFinished: _addUserFinished,
+            userNameController: _userNameController,
+            serverNameController: _serverNameController,
+            userNameFocusNode: _userNameFocusNode,
+            serverNameFocusNode: _serverNameFocusNode,
+            loggingIn: _addingUser ||
+                (_childViewConnection != null &&
+                    (_curvedTransitionAnimation.status ==
+                            AnimationStatus.dismissed ||
+                        _curvedTransitionAnimation.status ==
+                            AnimationStatus.forward)),
+            onUserDragStarted: (Account account) => setState(() {
+                  _draggedUsers.add(account);
+                }),
+            onUserDragCanceled: (Account account) => setState(() {
+                  _draggedUsers.remove(account);
+                }),
+          ),
+          onRemoveUser: (Account account) => setState(() {
+                _draggedUsers.remove(account);
+                widget.onRemoveUser?.call(account);
+              }),
+        ),
+      );
+
+  void _addUserStarted() => setState(() {
+        _addingUser = true;
+      });
+
+  void _addUserFinished() => setState(() {
+        _addingUser = false;
+      });
+
+  void _login(String accountId, UserProvider userProvider) {
+    _userControllerProxy?.ctrl?.close();
+    _userControllerProxy = new UserControllerProxy();
+    _userWatcherImpl?.close();
+    _userWatcherImpl = new UserWatcherImpl(onUserLogout: () {
+      print('UserPickerDeviceShell: User logged out!');
+      _handleLogout();
+    });
+
+    final InterfacePair<ViewOwner> viewOwner = new InterfacePair<ViewOwner>();
+    final UserLoginParams params = new UserLoginParams()
+      ..accountId = accountId
+      ..viewOwner = viewOwner.passRequest()
+      ..userController = _userControllerProxy.ctrl.request();
+    userProvider?.login(params);
+    _userControllerProxy.watch(_userWatcherImpl.getHandle());
+
+    setState(() {
+      _childViewConnection = new ChildViewConnection(
+        viewOwner.passHandle(),
+        onAvailable: (ChildViewConnection connection) {
+          print('UserPickerDeviceShell: Child view connection available!');
+          _transitionAnimation.forward();
+        },
+        onUnavailable: (ChildViewConnection connection) {
+          print('UserPickerDeviceShell: Child view connection unavailable!');
+          _handleLogout();
+        },
+      );
+    });
+  }
+
+  void _handleLogout() {
+    setState(() {
+      widget.onLogout?.call();
+      _transitionAnimation.reverse();
+      // TODO(apwilson): Should not need to remove the child view connection but
+      // it causes a mozart deadlock in the compositor if you don't.
+      _childViewConnection = null;
+    });
+  }
+}
