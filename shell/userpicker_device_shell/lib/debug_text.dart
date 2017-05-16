9a6da47b457147ab8e1a37369980c51bbfa4b342
Andrew Wilson <apwilson@google.com>
2017-05-16 10:43:56 -0700

    Add additional warnings if data or networking don't work.
    Change-Id: I6c1c630a65549bf51a1df3d98da29bbcff295191

--- shell/userpicker_device_shell/lib/debug_text.dart
+++ shell/userpicker_device_shell/lib/debug_text.dart
@@ -2,9 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 /// Displays debug text like hostname and ip addresses.
 class DebugText extends StatefulWidget {
@@ -14,6 +16,8 @@ class DebugText extends StatefulWidget {
 
 class _DebugTextState extends State<DebugText> {
   final List<InternetAddress> _addresses = <InternetAddress>[];
+  int _dataSize;
+  bool _networkingReady;
   @override
   void initState() {
     super.initState();
@@ -31,11 +35,53 @@ class _DebugTextState extends State<DebugText> {
       });
     });
     */
+
+    _checkData();
+    _checkNetworking();
+  }
+
+  void _checkData() {
+    new Directory('file:///data').stat().then((FileStat stat) {
+      setState(() {
+        if (!mounted) {
+          return;
+        }
+        _dataSize = stat.size;
+      });
+    }).catchError((_, __) {
+      new Timer(const Duration(seconds: 5), _checkData);
+    });
+  }
+
+  void _checkNetworking() {
+    http.get('http://www.example.com').then((http.Response response) {
+      setState(() {
+        if (!mounted) {
+          return;
+        }
+        _networkingReady = response.statusCode == 200;
+        if (!_networkingReady) {
+          new Timer(const Duration(seconds: 5), _checkNetworking);
+        }
+      });
+    }).catchError((_, __) {
+      new Timer(const Duration(seconds: 5), _checkNetworking);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     List<String> columnChildren = <String>[];
+    if (_dataSize == null) {
+      columnChildren.add('Data is NOT ready!');
+    } else if (_dataSize == 0) {
+      columnChildren.add('Data is NOT persistant!');
+    }
+
+    if (_networkingReady != true) {
+      columnChildren.add('Networking is NOT ready!');
+    }
+
     columnChildren.addAll(
       _addresses.map((InternetAddress address) => address.address).toList(),
     );
