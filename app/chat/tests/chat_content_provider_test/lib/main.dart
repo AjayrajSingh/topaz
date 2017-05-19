30fb7975d2690b054c30a7dc36c59e185944f307
YoungSeok Yoon <youngseokyoon@google.com>
2017-05-19 10:20:22 -0700

    test(agent): add more tests regarding message queue notification
    SO-428 #done
    
    Change-Id: I8e53d135b2eabca5ab7fc95fd42abee85bf46048

--- app/chat/tests/chat_content_provider_test/lib/main.dart
+++ app/chat/tests/chat_content_provider_test/lib/main.dart
@@ -3,21 +3,26 @@
 // found in the LICENSE file.
 
 import 'dart:async';
+import 'dart:convert' show JSON;
 
 import 'package:application.lib.app.dart/app.dart';
 import 'package:application.services/service_provider.fidl.dart';
 import 'package:apps.ledger.services.public/ledger.fidl.dart';
 import 'package:apps.modular.services.component/component_context.fidl.dart';
+import 'package:apps.modular.services.component/message_queue.fidl.dart';
 import 'package:apps.modular.services.module/module.fidl.dart';
 import 'package:apps.modular.services.module/module_context.fidl.dart';
 import 'package:apps.modules.chat.agents.content_provider..chat_content_provider_dart_package/src/chat_content_provider_impl.dart';
 import 'package:apps.modules.chat.services/chat_content_provider.fidl.dart';
 import 'package:apps.test_runner.services..test_runner/test_runner.fidl.dart';
 import 'package:lib.fidl.dart/bindings.dart' hide Message;
+import 'package:meta/meta.dart';
 import 'package:test/test.dart' hide expect;
 
 import 'src/expect.dart';
 import 'src/mock_chat_message_transporter.dart';
+
+const Duration _kTimeout = const Duration(seconds: 1);
 
 final ApplicationContext _context = new ApplicationContext.fromStartupInfo();
 ChatContentProviderTestModule _module;
@@ -79,6 +84,7 @@ class ChatContentProviderTestModule extends Module {
     // Now run the actual tests!
     try {
       await _testFromBlankSlate();
+      await _testMessageQueues();
     } catch (e, stackTrace) {
       _testRunner.fail('Test Error. See the console logs for more details.');
       _log('Test Error:\n$e\n$stackTrace');
@@ -290,6 +296,319 @@ class ChatContentProviderTestModule extends Module {
     expect(message.jsonPayload, equals('My Second Message'));
     expect(message.timestamp, isNotNull);
     // _testRunner.pass('GetLastMessage()');
+  }
+
+  /// Test getting notified of new conversations / messages with message queues.
+  /// This is assumed to be called after [_testFromBlankSlate()].
+  Future<Null> _testMessageQueues() async {
+    ChatStatus status;
+    List<Conversation> conversations;
+    Conversation conversation0, conversation1, conversation2;
+    List<Message> messages;
+    List<int> messageId;
+    Completer<Null> completer1, completer2;
+    dynamic decoded;
+
+    _MessageQueueWrapper mqConversation1 = await _getMessageQueue('conv1');
+    _MessageQueueWrapper mqConversation2 = await _getMessageQueue('conv2');
+    _MessageQueueWrapper mqMessage1 = await _getMessageQueue('msg1');
+    _MessageQueueWrapper mqMessage2 = await _getMessageQueue('msg2');
+
+    // GetConversations() should return a single conversation.
+    await _chatContentProvider.getConversations(
+      mqConversation1.token,
+      (ChatStatus s, List<Conversation> c) {
+        status = s;
+        conversations = c;
+      },
+    );
+    expect(status, equals(ChatStatus.ok));
+    expect(conversations, allOf(isNotNull, hasLength(1)));
+    expect(mqConversation1.receivedMessages, allOf(isNotNull, isEmpty));
+    conversation0 = conversations[0];
+
+    // GetMessages() should return two messages in the conversation.
+    await _chatContentProvider.getMessages(
+      conversation0.conversationId,
+      mqMessage1.token,
+      (ChatStatus s, List<Message> m) {
+        status = s;
+        messages = m;
+      },
+    );
+    expect(status, equals(ChatStatus.ok));
+    expect(messages, allOf(isNotNull, hasLength(2)));
+    expect(mqMessage1.receivedMessages, allOf(isNotNull, isEmpty));
+
+    // Add a new conversation and see if mqConversation1 gets notified.
+    completer1 = new Completer<Null>();
+    mqConversation1.completer = completer1;
+
+    List<String> participants1 = <String>[
+      'foo@example.com',
+      'bar@example.com',
+    ];
+
+    await _chatContentProvider.newConversation(
+      participants1,
+      (ChatStatus s, Conversation c) {
+        status = s;
+        conversation1 = c;
+      },
+    );
+    expect(status, equals(ChatStatus.ok));
+    expect(conversation1.participants, unorderedEquals(participants1));
+
+    // Wait for the message queue notification.
+    await completer1.future.timeout(_kTimeout);
+    expect(mqConversation1.receivedMessages, hasLength(1));
+    decoded = JSON.decode(mqConversation1.receivedMessages.last);
+    expect(decoded, isMap);
+    expect(
+      decoded['conversation_id'],
+      orderedEquals(conversation1.conversationId),
+    );
+    expect(
+      decoded['participants'],
+      unorderedEquals(participants1),
+    );
+
+    // Register another message queue for conversation.
+    await _chatContentProvider.getConversations(
+      mqConversation2.token,
+      (ChatStatus s, List<Conversation> c) {
+        status = s;
+        conversations = c;
+      },
+    );
+    expect(status, equals(ChatStatus.ok));
+    expect(conversations, allOf(isNotNull, hasLength(2)));
+
+    // Create a new conversation. Both message queues should be notified.
+    completer1 = new Completer<Null>();
+    completer2 = new Completer<Null>();
+    mqConversation1.completer = completer1;
+    mqConversation2.completer = completer2;
+
+    List<String> participants2 = <String>[
+      'qux@example.com',
+      'baz@example.com',
+    ];
+
+    await _chatContentProvider.newConversation(
+      participants2,
+      (ChatStatus s, Conversation c) {
+        status = s;
+        conversation2 = c;
+      },
+    );
+    expect(status, equals(ChatStatus.ok));
+    expect(conversation2.participants, unorderedEquals(participants2));
+
+    // Wait for the message queue notifications.
+    await completer1.future.timeout(_kTimeout);
+    expect(mqConversation1.receivedMessages, hasLength(2));
+    decoded = JSON.decode(mqConversation1.receivedMessages.last);
+    expect(decoded, isMap);
+    expect(
+      decoded['conversation_id'],
+      orderedEquals(conversation2.conversationId),
+    );
+    expect(
+      decoded['participants'],
+      unorderedEquals(participants2),
+    );
+
+    await completer2.future.timeout(_kTimeout);
+    expect(mqConversation2.receivedMessages, hasLength(1));
+    decoded = JSON.decode(mqConversation1.receivedMessages.last);
+    expect(decoded, isMap);
+    expect(
+      decoded['conversation_id'],
+      orderedEquals(conversation2.conversationId),
+    );
+    expect(
+      decoded['participants'],
+      unorderedEquals(participants2),
+    );
+
+    // Now send a few messages to the initial conversation and see if that
+    // message is delivered to the message queue.
+    completer1 = new Completer<Null>();
+    mqMessage1.completer = completer1;
+
+    await _chatContentProvider.sendMessage(
+      conversation0.conversationId,
+      'text',
+      JSON.encode('sample message1'),
+      (ChatStatus s, List<int> mid) {
+        status = s;
+        messageId = mid;
+      },
+    );
+    expect(status, equals(ChatStatus.ok));
+
+    await completer1.future.timeout(_kTimeout);
+    expect(mqMessage1.receivedMessages, hasLength(1));
+    decoded = JSON.decode(mqMessage1.receivedMessages.last);
+    expect(decoded, isMap);
+    expect(
+      decoded['conversation_id'],
+      orderedEquals(conversation0.conversationId),
+    );
+    expect(
+      decoded['message_id'],
+      orderedEquals(messageId),
+    );
+
+    // Register another message queue on the initial conversation.
+    await _chatContentProvider.getMessages(
+      conversation0.conversationId,
+      mqMessage2.token,
+      (ChatStatus s, List<Message> m) {
+        status = s;
+        messages = m;
+      },
+    );
+    expect(status, equals(ChatStatus.ok));
+    expect(messages, allOf(isNotNull, hasLength(3)));
+
+    // Send another message and see if both message queues are notified.
+    completer1 = new Completer<Null>();
+    completer2 = new Completer<Null>();
+    mqMessage1.completer = completer1;
+    mqMessage2.completer = completer2;
+
+    await _chatContentProvider.sendMessage(
+      conversation0.conversationId,
+      'text',
+      JSON.encode('sample message2'),
+      (ChatStatus s, List<int> mid) {
+        status = s;
+        messageId = mid;
+      },
+    );
+    expect(status, equals(ChatStatus.ok));
+
+    await completer1.future.timeout(_kTimeout);
+    expect(mqMessage1.receivedMessages, hasLength(2));
+    decoded = JSON.decode(mqMessage1.receivedMessages.last);
+    expect(decoded, isMap);
+    expect(
+      decoded['conversation_id'],
+      orderedEquals(conversation0.conversationId),
+    );
+    expect(
+      decoded['message_id'],
+      orderedEquals(messageId),
+    );
+
+    await completer2.future.timeout(_kTimeout);
+    expect(mqMessage2.receivedMessages, hasLength(1));
+    decoded = JSON.decode(mqMessage1.receivedMessages.last);
+    expect(decoded, isMap);
+    expect(
+      decoded['conversation_id'],
+      orderedEquals(conversation0.conversationId),
+    );
+    expect(
+      decoded['message_id'],
+      orderedEquals(messageId),
+    );
+
+    // Lastly, simulate the situation where a new message arrives from some
+    // other user. Both message queues should be notified.
+    completer1 = new Completer<Null>();
+    completer2 = new Completer<Null>();
+    mqMessage1.completer = completer1;
+    mqMessage2.completer = completer2;
+
+    messageId = const <int>[0, 1, 2, 3, 4];
+    await _mockChatMessageTransporter.mockReceiveMessage(
+      conversation0,
+      new Message()
+        ..messageId = messageId
+        ..sender = 'alice@example.com'
+        ..timestamp = new DateTime.now().millisecondsSinceEpoch
+        ..jsonPayload = JSON.encode('A message from Alice!'),
+    );
+
+    await completer1.future.timeout(_kTimeout);
+    expect(mqMessage1.receivedMessages, hasLength(3));
+    decoded = JSON.decode(mqMessage1.receivedMessages.last);
+    expect(decoded, isMap);
+    expect(
+      decoded['conversation_id'],
+      orderedEquals(conversation0.conversationId),
+    );
+    expect(
+      decoded['message_id'],
+      orderedEquals(messageId),
+    );
+
+    await completer2.future.timeout(_kTimeout);
+    expect(mqMessage2.receivedMessages, hasLength(2));
+    decoded = JSON.decode(mqMessage1.receivedMessages.last);
+    expect(decoded, isMap);
+    expect(
+      decoded['conversation_id'],
+      orderedEquals(conversation0.conversationId),
+    );
+    expect(
+      decoded['message_id'],
+      orderedEquals(messageId),
+    );
+
+    mqConversation1.close();
+    mqConversation2.close();
+    mqMessage1.close();
+    mqMessage2.close();
+  }
+
+  /// Helper method for obtaining a [MessageQueue] from the [ComponentContext].
+  Future<_MessageQueueWrapper> _getMessageQueue(String name) async {
+    MessageQueueProxy queue = new MessageQueueProxy();
+    _componentContext.obtainMessageQueue(
+      name,
+      queue.ctrl.request(),
+    );
+    Completer<String> tokenCompleter = new Completer<String>();
+    queue.getToken(tokenCompleter.complete);
+    String token = await tokenCompleter.future;
+
+    return new _MessageQueueWrapper(queue: queue, token: token);
+  }
+}
+
+class _MessageQueueWrapper {
+  final MessageQueueProxy queue;
+  final String token;
+  final List<String> receivedMessages = <String>[];
+
+  Completer<Null> completer;
+
+  _MessageQueueWrapper({
+    @required this.queue,
+    @required this.token,
+  }) {
+    assert(queue != null);
+    assert(token != null);
+    queue.receive(handleMessage);
+  }
+
+  void handleMessage(String message) {
+    receivedMessages.add(message);
+    queue.receive(handleMessage);
+
+    // If a completer is given from outside, complete it and set it to null.
+    if (completer != null && !completer.isCompleted) {
+      completer.complete();
+      completer = null;
+    }
+  }
+
+  void close() {
+    queue.ctrl.close();
   }
 }
 
