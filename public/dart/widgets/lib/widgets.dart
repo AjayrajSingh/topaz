9e376b55941277740994a1366717e49816de9975
Andrew Wilson <apwilson@google.com>
2017-05-22 14:42:30 -0700

    Add ApplicationWidget.
    Change-Id: Ia370c8ebdc79e1ceb2f163b925d3abbb65a28d8a

--- public/dart/widgets/lib/widgets.dart
+++ public/dart/widgets/lib/widgets.dart
@@ -3,5 +3,6 @@
 // found in the LICENSE file
 
 export 'src/widgets/alphatar.dart';
+export 'src/widgets/application_widget.dart';
 export 'src/widgets/mondrian_spinner.dart';
 export 'src/widgets/window_media_query.dart';
