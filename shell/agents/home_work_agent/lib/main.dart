a104e139e65915aa6df7b642f5a0589c86852b86
Andrew Wilson <apwilson@google.com>
2017-05-31 15:04:20 -0700

    Add some context providing agents.
    Change-Id: I47b6e076e9dbcfc63de45ffdda1018224251a51c

---
+++ shell/agents/home_work_agent/lib/main.dart
@@ -0,0 +1,178 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:application.lib.app.dart/app.dart';
+import 'package:apps.maxwell.services.suggestion/proposal.fidl.dart';
+import 'package:apps.maxwell.services.suggestion/proposal_publisher.fidl.dart';
+import 'package:apps.maxwell.services.suggestion/suggestion_display.fidl.dart';
+import 'package:apps.maxwell.services.context/context_publisher.fidl.dart';
+import 'package:apps.maxwell.services.user/intelligence_services.fidl.dart';
+import 'package:lib.modular/modular.dart';
+import 'package:meta/meta.dart';
+
+const String _kLocationHomeWorkTopic = '/location/home_work';
+
+enum _Location {
+  home,
+  work,
+  unknown,
+}
+
+HomeWorkAgent _agent;
+
+/// An implementation of the [Agent] interface.
+class HomeWorkAgent extends AgentImpl {
+  final ProposalPublisherProxy _proposalPublisher =
+      new ProposalPublisherProxy();
+  final ContextPublisherProxy _contextPublisher = new ContextPublisherProxy();
+  final Set<CustomActionBinding> _bindingSet = new Set<CustomActionBinding>();
+
+  _Location _currentLocation = _Location.unknown;
+
+  /// Constructor.
+  HomeWorkAgent({
+    @required ApplicationContext applicationContext,
+  })
+      : super(applicationContext: applicationContext);
+
+  @override
+  Future<Null> onReady(
+    ApplicationContext applicationContext,
+    AgentContext agentContext,
+    ComponentContext componentContext,
+    TokenProvider tokenProvider,
+    ServiceProviderImpl outgoingServices,
+  ) async {
+    IntelligenceServicesProxy intelligenceServices =
+        new IntelligenceServicesProxy();
+    agentContext.getIntelligenceServices(intelligenceServices.ctrl.request());
+    intelligenceServices.getContextPublisher(_contextPublisher.ctrl.request());
+    intelligenceServices.getProposalPublisher(
+      _proposalPublisher.ctrl.request(),
+    );
+
+    intelligenceServices.ctrl.close();
+    _publish();
+    _propose();
+  }
+
+  @override
+  Future<Null> onStop() async {
+    _contextPublisher.ctrl.close();
+    _proposalPublisher.ctrl.close();
+    _bindingSet.forEach((CustomActionBinding binding) => binding.close());
+  }
+
+  /// Publishes context indicating the user is at home.
+  void publishHome() {
+    _currentLocation = _Location.home;
+    _publish();
+    _propose();
+  }
+
+  /// Publishes context indicating the user is at work.
+  void publishWork() {
+    _currentLocation = _Location.work;
+    _publish();
+    _propose();
+  }
+
+  /// Publishes context indicating the user is neither at home nor work.
+  void publishUnknown() {
+    _currentLocation = _Location.unknown;
+    _publish();
+    _propose();
+  }
+
+  void _publish() {
+    String context = JSON.encode(<String, String>{
+      'location': _locationToString(_currentLocation),
+    });
+    _contextPublisher.publish(_kLocationHomeWorkTopic, context);
+  }
+
+  void _propose() => _proposalPublisher.propose(_proposal);
+
+  String _locationToString(_Location location) {
+    switch (location) {
+      case _Location.home:
+        return 'home';
+      case _Location.work:
+        return 'work';
+      default:
+        return 'unknown';
+    }
+  }
+
+  Proposal get _proposal {
+    CustomActionBinding binding = new CustomActionBinding();
+    _bindingSet.add(binding);
+    final _Location nextLocation = _getNextLocation(_currentLocation);
+    return new Proposal()
+      ..id = 'Home/Work ${_locationToString(_currentLocation)}'
+      ..display = (new SuggestionDisplay()
+        ..headline = 'Set location to ${_locationToString(nextLocation)}.'
+        ..subheadline = ''
+        ..details = ''
+        ..color = 0xFFFF0080
+        ..iconUrls = const <String>[]
+        ..imageType = SuggestionImageType.other
+        ..imageUrl = '')
+      ..onSelected = <Action>[
+        new Action()
+          ..customAction = binding.wrap(
+            new _CustomActionImpl(onExecute: () {
+              switch (nextLocation) {
+                case _Location.home:
+                  publishHome();
+                  break;
+                case _Location.work:
+                  publishWork();
+                  break;
+                default:
+                  publishUnknown();
+                  break;
+              }
+            }),
+          )
+      ];
+  }
+
+  _Location _getNextLocation(_Location location) {
+    switch (location) {
+      case _Location.home:
+        return _Location.work;
+      case _Location.work:
+        return _Location.unknown;
+      default:
+        return _Location.home;
+    }
+  }
+}
+
+typedef void _OnExecute();
+
+class _CustomActionImpl extends CustomAction {
+  final _OnExecute onExecute;
+
+  _CustomActionImpl({this.onExecute});
+
+  @override
+  void execute(void callback(List<Action> actions)) {
+    onExecute();
+    callback(<Action>[]);
+  }
+}
+
+Future<Null> main(List<dynamic> args) async {
+  ApplicationContext applicationContext =
+      new ApplicationContext.fromStartupInfo();
+  _agent = new HomeWorkAgent(
+    applicationContext: applicationContext,
+  );
+  _agent.advertise();
+}
---
+++ shell/agents/walking_agent/lib/main.dart
@@ -0,0 +1,160 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:application.lib.app.dart/app.dart';
+import 'package:apps.maxwell.services.suggestion/proposal.fidl.dart';
+import 'package:apps.maxwell.services.suggestion/proposal_publisher.fidl.dart';
+import 'package:apps.maxwell.services.suggestion/suggestion_display.fidl.dart';
+import 'package:apps.maxwell.services.context/context_publisher.fidl.dart';
+import 'package:apps.maxwell.services.user/intelligence_services.fidl.dart';
+import 'package:lib.modular/modular.dart';
+import 'package:meta/meta.dart';
+
+const String _kActivityWalkingTopic = '/activity/walking';
+
+enum _Activity {
+  walking,
+  unknown,
+}
+
+WalkingAgent _agent;
+
+/// An implementation of the [Agent] interface.
+class WalkingAgent extends AgentImpl {
+  final ProposalPublisherProxy _proposalPublisher =
+      new ProposalPublisherProxy();
+  final ContextPublisherProxy _contextPublisher = new ContextPublisherProxy();
+  final Set<CustomActionBinding> _bindingSet = new Set<CustomActionBinding>();
+
+  _Activity _currentActivity = _Activity.unknown;
+
+  /// Constructor.
+  WalkingAgent({
+    @required ApplicationContext applicationContext,
+  })
+      : super(applicationContext: applicationContext);
+
+  @override
+  Future<Null> onReady(
+    ApplicationContext applicationContext,
+    AgentContext agentContext,
+    ComponentContext componentContext,
+    TokenProvider tokenProvider,
+    ServiceProviderImpl outgoingServices,
+  ) async {
+    IntelligenceServicesProxy intelligenceServices =
+        new IntelligenceServicesProxy();
+    agentContext.getIntelligenceServices(intelligenceServices.ctrl.request());
+    intelligenceServices.getContextPublisher(_contextPublisher.ctrl.request());
+    intelligenceServices.getProposalPublisher(
+      _proposalPublisher.ctrl.request(),
+    );
+    intelligenceServices.ctrl.close();
+    _publish();
+    _propose();
+  }
+
+  @override
+  Future<Null> onStop() async {
+    _contextPublisher.ctrl.close();
+    _proposalPublisher.ctrl.close();
+    _bindingSet.forEach((CustomActionBinding binding) => binding.close());
+  }
+
+  /// Publishes context indicating the user is walking.
+  void publishWalking() {
+    _currentActivity = _Activity.walking;
+    _publish();
+    _propose();
+  }
+
+  /// Publishes context indicating the user is not walking.
+  void publishUnknown() {
+    _currentActivity = _Activity.unknown;
+    _publish();
+    _propose();
+  }
+
+  void _publish() {
+    String context = JSON.encode(<String, String>{
+      'activity': _activityToString(_currentActivity),
+    });
+    _contextPublisher.publish(_kActivityWalkingTopic, context);
+  }
+
+  void _propose() => _proposalPublisher.propose(_proposal);
+
+  String _activityToString(_Activity activity) {
+    switch (activity) {
+      case _Activity.walking:
+        return 'walking';
+      default:
+        return 'unknown';
+    }
+  }
+
+  Proposal get _proposal {
+    CustomActionBinding binding = new CustomActionBinding();
+    _bindingSet.add(binding);
+    final _Activity nextActivity = _getNextActivity(_currentActivity);
+    return new Proposal()
+      ..id = 'Activity ${_activityToString(_currentActivity)}'
+      ..display = (new SuggestionDisplay()
+        ..headline = 'Set activity to ${_activityToString(nextActivity)}.'
+        ..subheadline = ''
+        ..details = ''
+        ..color = 0xFFFF0080
+        ..iconUrls = const <String>[]
+        ..imageType = SuggestionImageType.other
+        ..imageUrl = '')
+      ..onSelected = <Action>[
+        new Action()
+          ..customAction = binding.wrap(
+            new _CustomActionImpl(onExecute: () {
+              switch (nextActivity) {
+                case _Activity.walking:
+                  publishWalking();
+                  break;
+                default:
+                  publishUnknown();
+                  break;
+              }
+            }),
+          )
+      ];
+  }
+
+  _Activity _getNextActivity(_Activity activity) {
+    switch (activity) {
+      case _Activity.walking:
+        return _Activity.unknown;
+      default:
+        return _Activity.walking;
+    }
+  }
+}
+
+typedef void _OnExecute();
+
+class _CustomActionImpl extends CustomAction {
+  final _OnExecute onExecute;
+
+  _CustomActionImpl({this.onExecute});
+
+  @override
+  void execute(void callback(List<Action> actions)) {
+    onExecute();
+    callback(<Action>[]);
+  }
+}
+
+Future<Null> main(List<dynamic> args) async {
+  ApplicationContext applicationContext =
+      new ApplicationContext.fromStartupInfo();
+  _agent = new WalkingAgent(applicationContext: applicationContext);
+  _agent.advertise();
+}
