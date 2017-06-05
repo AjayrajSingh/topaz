a0b0b26adcdf6e6b0f445d0c6121788a5a9dd202
Andrew Wilson <apwilson@google.com>
2017-06-05 11:52:23 -0700

    Fix path to web_view.
    Change-Id: I19c79e913643c7922823d0945995b522ad693137

--- runtime/web_runner/lib/dart/web_view.dart
+++ runtime/web_runner/lib/dart/web_view.dart
@@ -4,5 +4,4 @@
 
 /// The path to web_view.
 /// Keep in sync with web_view_component.json.
-const String kWebViewURL =
-    'https://fuchsia-build.storage.googleapis.com/apps/web_view/web_view_b034f1a588a20e87198b83fd37f4ff676b093b6c';
+const String kWebViewURL = 'file:///system/apps/web_view';
