c8b3aa06ba8f3cc6d5a475dfd465b2d842f96fad
David Yang <dayang@google.com>
2017-06-06 11:15:22 -0700

    feat(model): Travel Info (Distance) Model
    Change-Id: I0be76c4ed10eeefeae8360e72a0c62e6421de311

---
+++ tools/packages/models/lib/src/map/travel_info.dart
@@ -0,0 +1,43 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// This represents the travel time and distance between two locations
+///
+/// Based off the Google Maps Distance Matrix API:
+/// https://developers.google.com/maps/documentation/distance-matrix/start
+class TravelInfo {
+  /// Text representation of distance
+  ///
+  /// ex. 23 mi
+  final String distanceText;
+
+  /// Distance in meters
+  final int distanceInMeters;
+
+  /// Text representation of duration
+  ///
+  /// ex. 2 hr 2 min
+  final String durationText;
+
+  /// Travel duration between the two locations
+  final Duration duration;
+
+  /// Constructor
+  TravelInfo({
+    this.distanceText,
+    this.distanceInMeters,
+    this.durationText,
+    this.duration,
+  });
+
+  /// Creates an Event from JSON data
+  factory TravelInfo.fromJson(Map<String, dynamic> json) {
+    return new TravelInfo(
+      distanceText: json['distance']['text'],
+      distanceInMeters: json['distance']['value'],
+      durationText: json['duration']['text'],
+      duration: new Duration(seconds: json['duration']['value']),
+    );
+  }
+}
