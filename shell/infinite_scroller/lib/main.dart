c7671e3f6c3c4d1a98354ae628645564ddb74026
Andrew Wilson <apwilson@google.com>
2017-06-07 15:46:49 -0700

    Add infinite scroller example app.
    Change-Id: Ib4bde0d14a53f7387c58754e3a5171cecf593cf7

---
+++ shell/infinite_scroller/lib/main.dart
@@ -0,0 +1,76 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+
+const double _kGoldenRatio = 1.61803398875;
+const double _kMaxWidth = 400.0;
+
+Future<Null> main() async {
+  runApp(
+    new LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        double cardHeight =
+            16.0 + math.min(_kMaxWidth, constraints.maxWidth) / _kGoldenRatio;
+        return new Material(
+          color: Colors.grey[200],
+          child: new Center(
+            child: new ConstrainedBox(
+              constraints: new BoxConstraints(
+                maxWidth: _kMaxWidth,
+                maxHeight: constraints.maxHeight,
+              ),
+              child: new ListView.builder(
+                physics: new _FrictionlessScrollPhysics(
+                  parent: const BouncingScrollPhysics(),
+                ),
+                itemExtent: cardHeight,
+                itemBuilder: (BuildContext context, int index) => new Container(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: new Material(
+                        color: index % 2 == 0 ? Colors.blue : Colors.green,
+                        elevation: 3.0,
+                        borderRadius: new BorderRadius.circular(4.0),
+                      ),
+                    ),
+              ),
+            ),
+          ),
+        );
+      },
+    ),
+  );
+}
+
+class _FrictionlessScrollPhysics extends ScrollPhysics {
+  const _FrictionlessScrollPhysics({ScrollPhysics parent})
+      : super(parent: parent);
+
+  @override
+  Simulation createBallisticSimulation(
+    ScrollMetrics position,
+    double velocity,
+  ) =>
+      new _FrictionlessSimulation(velocity: velocity);
+}
+
+class _FrictionlessSimulation extends Simulation {
+  final double velocity;
+
+  _FrictionlessSimulation({this.velocity}) {
+    print('velocity: $velocity');
+  }
+
+  @override
+  double x(double time) => velocity * time;
+
+  @override
+  double dx(double time) => velocity;
+
+  @override
+  bool isDone(double time) => false;
+}
