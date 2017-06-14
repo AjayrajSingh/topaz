0867c82ebf29d9973aaee56c0a1f7b2d8b1d7d17
David Yang <dayang@google.com>
2017-06-14 11:06:28 -0700

    feat(concerts): Adding Header to Event List
    Change-Id: Ie1217de0fa9befe102f0bb9e5e18b19f592a53cf

--- app/music/packages/concert_widgets/lib/src/event_list.dart
+++ app/music/packages/concert_widgets/lib/src/event_list.dart
@@ -4,10 +4,14 @@
 
 import 'package:concert_models/concert_models.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:meta/meta.dart';
 
 import 'event_list_item.dart';
 import 'typedefs.dart';
+
+const double _kHeaderHeight = 160.0;
+const double _kLogoSize = 48.0;
 
 /// UI Widget that represents a list of [Event]s
 class EventList extends StatelessWidget {
@@ -20,6 +24,8 @@ class EventList extends StatelessWidget {
   /// Callback for when an event is selected
   final EventActionCallback onSelect;
 
+  static final DateFormat _kMonthFormat = new DateFormat('MMMM y');
+
   /// Constructor
   EventList({
     Key key,
@@ -31,16 +37,57 @@ class EventList extends StatelessWidget {
     assert(events != null);
   }
 
+  String get _listTitle =>
+      'Concert Guide  -  ${_kMonthFormat.format(new DateTime.now())}';
+
   @override
   Widget build(BuildContext context) {
+    List<Widget> children = <Widget>[
+      new Container(
+        height: _kHeaderHeight,
+        decoration: new BoxDecoration(
+          image: new DecorationImage(
+            image: new AssetImage(
+              'packages/concert_widgets/res/concert_bg.jpg',
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: new Center(
+          child: new Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              new Container(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: new Image.asset(
+                  'packages/concert_widgets/res/plat_logo.png',
+                  height: _kLogoSize,
+                  width: _kLogoSize,
+                ),
+              ),
+              new Text(
+                _listTitle,
+                style: new TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ];
+
+    children.addAll(events
+        .map((Event event) => new EventListItem(
+              event: event,
+              onSelect: onSelect,
+              isSelected: event == selectedEvent,
+            ))
+        .toList());
+
     return new Column(
-      children: events
-          .map((Event event) => new EventListItem(
-                event: event,
-                onSelect: onSelect,
-                isSelected: event == selectedEvent,
-              ))
-          .toList(),
+      children: children,
     );
   }
 }
