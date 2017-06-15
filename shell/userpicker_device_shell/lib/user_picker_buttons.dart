3d4f4494865f6c194326ab0076d393c0eb9dea77
Andrew Wilson <apwilson@google.com>
2017-06-15 10:54:04 -0700

    Optionally add user shell toggle.
    Change-Id: I26d95a724a2168bb10d5d02fcac56b3f570cde10

--- shell/userpicker_device_shell/lib/user_picker_buttons.dart
+++ shell/userpicker_device_shell/lib/user_picker_buttons.dart
@@ -7,6 +7,8 @@ import 'circular_button.dart';
 
 import 'user_picker_device_shell_model.dart';
 
+const bool _kShowUserShellToggle = false;
+
 /// Main buttons (Shutdown, New User) for the User Picker
 class UserPickerButtons extends StatelessWidget {
   /// Called when the add user button is pressed.
@@ -28,43 +30,58 @@ class UserPickerButtons extends StatelessWidget {
   @override
   Widget build(BuildContext context) =>
       new ScopedModelDescendant<UserPickerDeviceShellModel>(
-          builder: (
-        BuildContext context,
-        Widget child,
-        UserPickerDeviceShellModel model,
-      ) =>
-              new Row(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  new CircularButton(
-                    icon: Icons.power_settings_new,
-                    onTap: () => model.deviceShellContext?.shutdown(),
-                  ),
-                  new Container(
-                    margin: const EdgeInsets.only(left: 16.0),
-                    child: new CircularButton(
-                      icon: Icons.person_add,
-                      onTap: () => onAddUser?.call(),
-                    ),
-                  ),
-                  new Container(
-                      margin: const EdgeInsets.only(left: 16.0),
-                      child: new Material(
-                        type: MaterialType.circle,
-                        elevation: 2.0,
-                        color: Colors.grey[200],
-                        child: new InkWell(
-                          onTap: () => onUserShellChange?.call(),
-                          child: new Container(
-                            padding: const EdgeInsets.all(12.0),
-                            child: new Image.asset(
-                              userShellAssetName,
-                              height: 24.0,
-                              width: 24.0,
-                            ),
-                          ),
-                        ),
-                      )),
-                ],
-              ));
+        builder: (
+          BuildContext context,
+          Widget child,
+          UserPickerDeviceShellModel model,
+        ) =>
+            new Row(
+              mainAxisSize: MainAxisSize.min,
+              children: _buildRowChildren(model),
+            ),
+      );
+
+  List<Widget> _buildRowChildren(UserPickerDeviceShellModel model) {
+    List<Widget> rowChildren = <Widget>[
+      _buildShutdownButton(model),
+      new Container(width: 16.0, height: 0.0),
+      _buildAddUserButton(model),
+    ];
+    if (_kShowUserShellToggle) {
+      rowChildren.addAll(<Widget>[
+        new Container(width: 16.0, height: 0.0),
+        _buildUserShellToggleButton(),
+      ]);
+    }
+    return rowChildren;
+  }
+
+  Widget _buildShutdownButton(UserPickerDeviceShellModel model) =>
+      new CircularButton(
+        icon: Icons.power_settings_new,
+        onTap: () => model.deviceShellContext?.shutdown(),
+      );
+
+  Widget _buildAddUserButton(UserPickerDeviceShellModel model) =>
+      new CircularButton(
+        icon: Icons.person_add,
+        onTap: () => onAddUser?.call(),
+      );
+
+  Widget _buildUserShellToggleButton() => new Material(
+        type: MaterialType.circle,
+        elevation: 2.0,
+        color: Colors.grey[200],
+        child: new InkWell(
+          onTap: () => onUserShellChange?.call(),
+          child: new Container(
+            padding: const EdgeInsets.all(12.0),
+            child: new Image.asset(
+              userShellAssetName,
+              height: 24.0,
+              width: 24.0,
+            ),
+          ),
+        ),
+      );
 }
