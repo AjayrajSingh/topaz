fe4d830b35848e3bbedc5b47d3df5e6a95540649
David Yang <dayang@google.com>
2017-07-05 11:50:10 -0700

    feat(concerts): Concert Agent makes proposal based off hotel
    Change-Id: I103331574db14fd6f29cdf7383c572db9ac8b705

--- app/music/agents/concert_agent/lib/main.dart
+++ app/music/agents/concert_agent/lib/main.dart
@@ -10,23 +10,13 @@ import 'package:apps.maxwell.services.context/context_provider.fidl.dart';
 import 'package:apps.maxwell.services.suggestion/proposal.fidl.dart';
 import 'package:apps.maxwell.services.suggestion/proposal_publisher.fidl.dart';
 import 'package:apps.maxwell.services.suggestion/suggestion_display.fidl.dart';
-import 'package:concert_api/api.dart';
-import 'package:concert_models/concert_models.dart';
-import 'package:config/config.dart';
 import 'package:lib.fidl.dart/bindings.dart';
 import 'package:lib.logging/logging.dart';
-import 'package:meta/meta.dart';
 
-/// The Concert Agents subscribes to the 'focal_entities' topic and will
-/// propose event/concert suggestions if any of those Entities is an artist and
-/// has upcoming concerts.
+/// The Concert Agents subscribes to the hotel topic and makes proposals for
+/// upcoming concerts (concert list module).
 
-/// The context topic for "focal entities" for the current story.
-const String _kCurrentFocalEntitiesTopic =
-    '/story/focused/explicit/focal_entities';
-
-/// The Entity type for a music artist.
-const String _kMusicArtistType = 'http://types.fuchsia.io/music/artist';
+const String _kHotelTopic = 'hotel';
 
 /// Global scoping to prevent garbage collection
 final ContextProviderProxy _contextProvider = new ContextProviderProxy();
@@ -34,16 +24,10 @@ ContextListenerImpl _contextListenerImpl;
 final ProposalPublisherProxy _proposalPublisher = new ProposalPublisherProxy();
 final ApplicationContext _context = new ApplicationContext.fromStartupInfo();
 
-/// Concert ContextListener that prints if the given artist has an upcoming
-/// concert in the user's metro area.
+/// Concert ContextListener listens to hotel reservations and makes a concert
+/// list proposal
 class ContextListenerImpl extends ContextListener {
   final ContextListenerBinding _binding = new ContextListenerBinding();
-
-  /// Songkick API key
-  final String apiKey;
-
-  /// Constructor
-  ContextListenerImpl({@required this.apiKey});
 
   /// Gets the [InterfaceHandle]
   ///
@@ -52,43 +36,22 @@ class ContextListenerImpl extends ContextListener {
 
   @override
   Future<Null> onUpdate(ContextUpdate result) async {
-    if (!result.values.containsKey(_kCurrentFocalEntitiesTopic)) {
+    if (!result.values.containsKey(_kHotelTopic)) {
       return;
     }
 
-    List<dynamic> data =
-        JSON.decode(result.values[_kCurrentFocalEntitiesTopic]);
-    for (dynamic entity in data) {
-      if (!(entity is Map<String, dynamic>)) continue;
-      if (entity.containsKey('@type') && entity['@type'] == _kMusicArtistType) {
-        log.fine('artist update: ${entity['name']}');
-        List<Event> events = await Api.searchEventsByArtist(
-          entity['name'],
-          apiKey,
-        );
-        if (events != null && events.length > 0) {
-          log.fine('concerts found for: ${entity['name']}');
-          _createProposal(events, entity['name']);
-        } else {
-          log.fine('no concerts found for: ${entity['name']}');
-        }
-      }
+    dynamic data = JSON.decode(result.values[_kHotelTopic]);
+    if (data['name'] is String) {
+      _createProposal(data['name']);
     }
   }
 
-  /// Creates a proposal given the artist and events
-  ///
-  /// We need to pass in the original artist name that was used to query for
-  /// upcoming coming concerts because songkick events can have more than one
-  /// artist.
-  ///
-  /// This only creates a proposal for the top ranked event based on the
-  /// search.
-  void _createProposal(List<Event> events, String artistName) {
-    String headline = 'Buy tickets for $artistName at ${events[0].venue.name}';
+  /// Creates a concert list proposal
+  void _createProposal(String hotelName) {
+    String headline = 'Upcoming concerts near $hotelName this week';
 
     Proposal proposal = new Proposal()
-      ..id = 'Songkick Events ${events[0].id}'
+      ..id = 'Concerts Near Hotel'
       ..display = (new SuggestionDisplay()
         ..headline = headline
         ..subheadline = 'powered by Songkick'
@@ -96,17 +59,13 @@ class ContextListenerImpl extends ContextListener {
         ..color = 0xFFFF0080
         ..iconUrls = const <String>[]
         ..imageType = SuggestionImageType.other
-        ..imageUrl = events[0].venue.imageUrl
+        ..imageUrl =
+            'https://images.unsplash.com/photo-1486591978090-58e619d37fe7?dpr=1&auto=format&fit=crop&w=300&h=300&q=80&cs=tinysrgb&crop=&bg='
         ..annoyance = AnnoyanceType.none)
       ..onSelected = <Action>[
         new Action()
           ..createStory = (new CreateStory()
-            // TODO (dayang@): Update once the Concert module is implemented
-            // https://fuchsia.atlassian.net/browse/SO-376
-            ..moduleId = 'file:///system/apps/concert_event'
-            ..initialData = JSON.encode(
-              <String, dynamic>{'songkick:eventId': events[0].id},
-            ))
+            ..moduleId = 'file:///system/apps/concert_event_list')
       ];
 
     log.fine('proposing concert suggestion');
@@ -114,28 +73,12 @@ class ContextListenerImpl extends ContextListener {
   }
 }
 
-/// Retrieves the Songkick API Key
-Future<String> _readAPIKey() async {
-  Config config = await Config.read('/system/data/modules/config.json');
-  String apiKey = config.get('songkick_api_key');
-  if (apiKey == null) {
-    log.fine('"songkick_api_key" value is not specified in config.json.');
-    return null;
-  } else {
-    return apiKey;
-  }
-}
-
 Future<Null> main(List<dynamic> args) async {
   setupLogger();
 
-  String apiKey = await _readAPIKey();
-  if (apiKey != null) {
-    connectToService(_context.environmentServices, _contextProvider.ctrl);
-    connectToService(_context.environmentServices, _proposalPublisher.ctrl);
-    ContextQuery query =
-        new ContextQuery.init(<String>[_kCurrentFocalEntitiesTopic], null);
-    _contextListenerImpl = new ContextListenerImpl(apiKey: apiKey);
-    _contextProvider.subscribe(query, _contextListenerImpl.getHandle());
-  }
+  connectToService(_context.environmentServices, _contextProvider.ctrl);
+  connectToService(_context.environmentServices, _proposalPublisher.ctrl);
+  ContextQuery query = new ContextQuery.init(<String>[_kHotelTopic], null);
+  _contextListenerImpl = new ContextListenerImpl();
+  _contextProvider.subscribe(query, _contextListenerImpl.getHandle());
 }
