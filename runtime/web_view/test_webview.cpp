5e3fac0db9a35d8b5d4239e8746e4f02a163b061
Rob Tsuk <robtsuk@google.com>
2017-07-17 15:53:52 -0700

    Add a smoke test
    Add a binary that tests that we can load the google
    home page successfully. Returns 0 if it works and
    prints an error and returns 1 on failure.
    
    Change-Id: I1539f5bed5e2479774d00a14e2e5281c388a0479

---
+++ runtime/web_view/test_webview.cpp
@@ -0,0 +1,35 @@
+#include "WebView.h"
+
+#include <iostream>
+#include <vector>
+
+#include <magenta/pixelformat.h>
+
+using namespace std;
+
+int main(int argc, const char** argv) {
+    size_t kTestWidth = 1024;
+    size_t kTestHeight = 728;
+    auto format = MX_PIXEL_FORMAT_RGB_x888;
+
+    vector<unsigned char> buffer(kTestWidth * kTestHeight * 4);
+
+    WebView webView;
+    webView.setup(&buffer[0], format, kTestWidth, kTestHeight, kTestWidth* 4);
+    webView.setURL("https://google.com/");
+
+    webView.setFocused(true);
+    webView.setVisible(true);
+
+    while (!webView.isMainFrameLoaded()) {
+        webView.iterateEventLoop();
+        webView.layoutAndPaint();
+    }
+
+    auto err = webView.getMainDocumentError();
+    if (err.length() > 0) {
+      cerr << "test_webview failed: " << err << endl;
+      return 1;
+    }
+    return 0;
+}
