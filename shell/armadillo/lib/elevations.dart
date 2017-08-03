1f2e6a952c05ec1aeccf203e18958be0f6b10edb
Andrew Wilson <apwilson@google.com>
2017-08-03 11:30:23 -0700

    Adjust elevations to handle story shell height.
    Change-Id: Idf9f5d1ff3e4253e1919f8fe182d6b26eee851f9

--- shell/armadillo/lib/elevations.dart
+++ shell/armadillo/lib/elevations.dart
@@ -4,26 +4,41 @@
 
 /// PhysicalModel elevation values for various UI elements
 class Elevations {
+  /// The maximum elevation a module has.  This is an estimate.
+  static const double moduleMaxElevation = 50.0;
+
+  /// The maximum elevation story shell is using.  This is an estimate.
+  static const double storyShellMaxElevation = 250.0;
+
+  static const double _storyShellWithModuleMaxElevation =
+      moduleMaxElevation + storyShellMaxElevation;
+
   /// Elevation for the suggestion list overlay
-  static const double suggestionList = 410.0;
+  static const double suggestionList =
+      (3 * _storyShellWithModuleMaxElevation) + 10.0;
 
   /// Elevation for an interruption
-  static const double interruption = 420.0;
+  static const double interruption =
+      (3 * _storyShellWithModuleMaxElevation) + 20.0;
 
   /// Elevation for story cluster when it is being dragged
-  static const double draggedStoryCluster = 400.0;
+  static const double draggedStoryCluster =
+      (3 * _storyShellWithModuleMaxElevation);
 
   /// Elevation for story cluster when it is focused
-  static const double focusedStoryCluster = 100.0;
+  static const double focusedStoryCluster = _storyShellWithModuleMaxElevation;
 
   /// Elecvation for quick settings overlay
-  static const double quickSettings = 420.0;
+  static const double quickSettings =
+      (3 * _storyShellWithModuleMaxElevation) + 20.0;
 
   /// Elevation for suggestion expand overlay
-  static const double suggestionExpand = 420.0;
+  static const double suggestionExpand =
+      (3 * _storyShellWithModuleMaxElevation) + 20.0;
 
   /// Elevation for a story cluster when it is in inline preview mode
-  static const double storyClusterInlinePreview = 300.0;
+  static const double storyClusterInlinePreview =
+      2 * _storyShellWithModuleMaxElevation;
 
   /// Additional elevation to give a focused story tab
   static const double focusedStoryTab = 8.0;
