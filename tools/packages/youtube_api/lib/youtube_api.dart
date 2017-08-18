7f7674429fcc18578e447d2c20e968eb538f7e80
YoungSeok Yoon <youngseokyoon@google.com>
2017-08-17 19:17:34 -0700

    refactor(modules): move prototype agents / modules to the new repository
    These agents and modules are moved to //apps/modules/prototypes.
    
    SO-633 #done
    
    Change-Id: Idcb3b5a6fd1181b8bb499c9d9f17832ff4bac9c9

--- tools/packages/youtube_api/lib/youtube_api.dart
+++
@@ -1,6 +0,0 @@
-// Copyright 2017 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-export 'src/googleapis_youtube_api.dart';
-export 'src/youtube_api.dart';
