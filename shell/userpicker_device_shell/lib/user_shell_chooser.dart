f039862ac51c26548acaf1300f227d655918d432
Andrew Wilson <apwilson@google.com>
2017-08-31 10:08:04 -0700

    Make user shell to launch configurable with a file.
    Change-Id: Id98f10d56b712d502adc6811eac724404683d11c

--- shell/userpicker_device_shell/lib/user_shell_chooser.dart
+++ shell/userpicker_device_shell/lib/user_shell_chooser.dart
@@ -2,35 +2,24 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:io';
+
 /// Keeps track of the currently chosen user shell.
 class UserShellChooser {
-  final List<_UserShellEntry> _userShells = <_UserShellEntry>[
-    new _UserShellEntry(
-      assetName: 'packages/userpicker_device_shell/res/ArmadilloSilhouette.png',
-      appUrl: 'armadillo_user_shell',
-    ),
-    new _UserShellEntry(
-      assetName: 'packages/userpicker_device_shell/res/CapybaraSilhouette.png',
-      appUrl: 'capybara_user_shell',
-    ),
-  ];
+  String _userShellAppUrl = 'armadillo_user_shell';
 
-  int _currentUserShellIndex = 0;
-
-  /// Chooses the next user shell in the list.
-  void next() {
-    _currentUserShellIndex = (_currentUserShellIndex + 1) % _userShells.length;
+  /// Constructor.
+  UserShellChooser() {
+    File file = new File('/system/data/sysui/user_shell_to_launch');
+    file.exists().then(
+          (bool exists) => file.readAsString().then(
+                (String userShellAppUrl) {
+                  _userShellAppUrl = userShellAppUrl;
+                },
+              ),
+        );
   }
 
-  /// Gets the current user shell's asset name.
-  String get assetName => _userShells[_currentUserShellIndex].assetName;
-
   /// Gets the current user shell's app url.
-  String get appUrl => _userShells[_currentUserShellIndex].appUrl;
-}
-
-class _UserShellEntry {
-  final String assetName;
-  final String appUrl;
-  _UserShellEntry({this.assetName, this.appUrl});
+  String get appUrl => _userShellAppUrl;
 }
