68c2f4d948ae10bff9f969d9074ed9896d9c1638
P.Y. Laligand <pylaligand@google.com>
2017-09-21 20:18:51 -0700

    Moved Modular to //peridot.
    Change-Id: I3b4ab0b28d69cef4a044b9986a84b30e5d7bb360

--- app/contacts/modules/contact_list/lib/src/modular/contact_list_module_model.dart
+++ app/contacts/modules/contact_list/lib/src/modular/contact_list_module_model.dart
@@ -4,9 +4,9 @@
 
 import 'dart:async';
 
-import 'package:apps.modular.services.agent.agent_controller/agent_controller.fidl.dart';
-import 'package:apps.modular.services.module/module_context.fidl.dart';
-import 'package:apps.modular.services.story/link.fidl.dart';
+import 'package:lib.agent.fidl.agent_controller/agent_controller.fidl.dart';
+import 'package:lib.module.fidl/module_context.fidl.dart';
+import 'package:lib.story.fidl/link.fidl.dart';
 import 'package:lib.app.fidl/service_provider.fidl.dart';
 import 'package:lib.logging/logging.dart';
 import 'package:lib.widgets/modular.dart';
