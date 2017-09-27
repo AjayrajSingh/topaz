3f821c2fb9b4d922289e9c695cb920ce6bcd9dd5
Andrew Wilson <apwilson@google.com>
2017-09-27 16:50:27 -0700

    Set font sizes based on available height.
    Change-Id: I9c8affa3bbf7f1e97a11a286cf40c78984fea018

--- app/dashboard/lib/build_status_widget.dart
+++ app/dashboard/lib/build_status_widget.dart
@@ -11,36 +11,7 @@ import 'package:lib.widgets/widgets.dart';
 import 'build_status_model.dart';
 import 'info_text.dart' show toConciseString;
 
-const double _kFontSize = 20.0;
-const double _kTimerFontSize = 12.0;
-const double _kErrorFontSize = 12.0;
 const double _kSpaceBetween = 4.0;
-
-final TextStyle _kImportantStyle = new TextStyle(
-  color: Colors.black,
-  fontSize: _kFontSize,
-  fontWeight: FontWeight.w500,
-);
-
-final TextStyle _kUnimportantStyle = _kImportantStyle.copyWith(
-  fontWeight: FontWeight.w300,
-);
-
-final TextStyle _kTimerStyle = _kImportantStyle.copyWith(
-  fontSize: _kTimerFontSize,
-);
-
-final TextStyle _kErrorStyle = new TextStyle(
-  color: Colors.red[900],
-  fontWeight: FontWeight.w900,
-  fontSize: _kErrorFontSize,
-);
-
-final TextStyle _kRefreshDurationStyle = new TextStyle(
-  color: Colors.black,
-  fontWeight: FontWeight.w100,
-  fontSize: _kErrorFontSize,
-);
 
 const Color _kFuchsiaColor = const Color(0xFFFF0080);
 
@@ -58,6 +29,7 @@ class BuildStatusWidget extends StatefulWidget {
 
 class _BuildStatusWidgetState extends State<BuildStatusWidget> {
   Timer _timer;
+  Size _size;
 
   @override
   void initState() {
@@ -76,124 +48,115 @@ class _BuildStatusWidgetState extends State<BuildStatusWidget> {
   }
 
   @override
-  Widget build(BuildContext context) =>
-      new ScopedModelDescendant<BuildStatusModel>(
-        builder: (BuildContext context, Widget child, BuildStatusModel model) {
-          bool hasError = model.errorMessage?.isNotEmpty ?? false;
-          List<Widget> columnChildren = <Widget>[
-            new Text(
-              model.type,
-              textAlign: TextAlign.center,
-              style: hasError
-                  ? _kUnimportantStyle.copyWith(fontSize: _kErrorFontSize)
-                  : _kUnimportantStyle,
-            ),
-            new Container(height: 4.0),
-            new Text(
-              model.name,
-              textAlign: TextAlign.center,
-              style: hasError
-                  ? _kImportantStyle.copyWith(fontSize: _kErrorFontSize)
-                  : _kImportantStyle,
-            ),
-          ];
-          if (hasError) {
-            columnChildren.addAll(<Widget>[
+  Widget build(BuildContext context) => new LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+        _size = constraints.biggest;
+        return new ScopedModelDescendant<BuildStatusModel>(
+          builder:
+              (BuildContext context, Widget child, BuildStatusModel model) {
+            bool hasError = model.errorMessage?.isNotEmpty ?? false;
+            List<Widget> columnChildren = <Widget>[
+              new Text(
+                model.type,
+                textAlign: TextAlign.center,
+                style: hasError
+                    ? _unimportantStyle.copyWith(fontSize: _errorFontSize)
+                    : _unimportantStyle,
+              ),
               new Container(height: 4.0),
               new Text(
-                model.errorMessage,
-                textAlign: TextAlign.left,
-                style: _kErrorStyle,
+                model.name,
+                textAlign: TextAlign.center,
+                style: hasError
+                    ? _importantStyle.copyWith(fontSize: _errorFontSize)
+                    : _importantStyle,
               ),
-            ]);
-          }
+            ];
+            if (hasError) {
+              columnChildren.addAll(<Widget>[
+                new Container(height: 4.0),
+                new Text(
+                  model.errorMessage,
+                  textAlign: TextAlign.left,
+                  style: _errorStyle,
+                ),
+              ]);
+            }
 
-          List<Widget> stackChildren = <Widget>[
-            new Center(
-              child: new Column(
-                mainAxisSize: MainAxisSize.min,
-                children: columnChildren,
-              ),
-            ),
-          ];
-
-          if (model.lastRefreshEnded == null) {
-            stackChildren.add(
-              new Align(
-                alignment: FractionalOffset.bottomCenter,
-                child: new Container(
-                  margin: const EdgeInsets.only(bottom: 8.0),
-                  width: 16.0,
-                  height: 16.0,
-                  child: new FuchsiaSpinner(),
+            List<Widget> stackChildren = <Widget>[
+              new Center(
+                child: new Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: columnChildren,
                 ),
               ),
-            );
-          } else {
-            stackChildren.add(
-              new Align(
-                alignment: FractionalOffset.bottomCenter,
+            ];
+
+            if (model.lastRefreshEnded == null) {
+              stackChildren.add(
+                new Align(
+                  alignment: FractionalOffset.bottomCenter,
+                  child: new Container(
+                    margin: const EdgeInsets.only(bottom: 8.0),
+                    width: 16.0,
+                    height: 16.0,
+                    child: new FuchsiaSpinner(),
+                  ),
+                ),
+              );
+            }
+
+            if (model.lastFailTime != null) {
+              Duration lastFailureTime =
+                  new DateTime.now().difference(model.lastFailTime);
+              stackChildren.add(
+                new Align(
+                  alignment: FractionalOffset.topRight,
+                  child: new Container(
+                    margin: const EdgeInsets.only(top: 8.0),
+                    child: new Text(
+                      toConciseString(lastFailureTime),
+                      style: _timerStyle,
+                    ),
+                  ),
+                ),
+              );
+            } else if (model.lastPassTime != null) {
+              Duration lastPassTime = new DateTime.now().difference(
+                model.lastPassTime,
+              );
+              stackChildren.add(
+                new Align(
+                  alignment: FractionalOffset.topRight,
+                  child: new Container(
+                    margin: const EdgeInsets.only(top: 8.0),
+                    child: new Text(
+                      toConciseString(lastPassTime),
+                      style: _timerStyle,
+                    ),
+                  ),
+                ),
+              );
+            }
+
+            return new Material(
+              elevation: 2.0,
+              color: _colorFromBuildStatus(model.buildStatus),
+              borderRadius: new BorderRadius.circular(8.0),
+              child: new InkWell(
+                onTap: () => widget.onTap?.call(),
                 child: new Container(
-                  margin: const EdgeInsets.only(bottom: 8.0, right: 8.0),
-                  child: new Text(
-                    '${model.lastRefreshEnded.difference(model.lastRefreshStarted).inMilliseconds} ms',
-                    style: _kRefreshDurationStyle,
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: new Stack(
+                    fit: StackFit.passthrough,
+                    children: stackChildren,
                   ),
                 ),
               ),
             );
-          }
-
-          if (model.lastFailTime != null) {
-            Duration lastFailureTime =
-                new DateTime.now().difference(model.lastFailTime);
-            stackChildren.add(
-              new Align(
-                alignment: FractionalOffset.topRight,
-                child: new Container(
-                  margin: const EdgeInsets.only(top: 8.0),
-                  child: new Text(
-                    toConciseString(lastFailureTime),
-                    style: _kTimerStyle,
-                  ),
-                ),
-              ),
-            );
-          } else if (model.lastPassTime != null) {
-            Duration lastPassTime = new DateTime.now().difference(
-              model.lastPassTime,
-            );
-            stackChildren.add(
-              new Align(
-                alignment: FractionalOffset.topRight,
-                child: new Container(
-                  margin: const EdgeInsets.only(top: 8.0),
-                  child: new Text(
-                    toConciseString(lastPassTime),
-                    style: _kTimerStyle,
-                  ),
-                ),
-              ),
-            );
-          }
-
-          return new Material(
-            elevation: 2.0,
-            color: _colorFromBuildStatus(model.buildStatus),
-            borderRadius: new BorderRadius.circular(8.0),
-            child: new InkWell(
-              onTap: () => widget.onTap?.call(),
-              child: new Container(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: new Stack(
-                  fit: StackFit.passthrough,
-                  children: stackChildren,
-                ),
-              ),
-            ),
-          );
-        },
-      );
+          },
+        );
+      });
 
   Color _colorFromBuildStatus(BuildStatus status) {
     switch (status) {
@@ -207,4 +170,27 @@ class _BuildStatusWidgetState extends State<BuildStatusWidget> {
         return Colors.grey[300];
     }
   }
+
+  double get _fontSize => _size.height / 6.0;
+  double get _timerFontSize => _size.height / 10.0;
+  double get _errorFontSize => _size.height / 10.0;
+  TextStyle get _importantStyle => new TextStyle(
+        color: Colors.black,
+        fontSize: _fontSize,
+        fontWeight: FontWeight.w500,
+      );
+
+  TextStyle get _unimportantStyle => _importantStyle.copyWith(
+        fontWeight: FontWeight.w300,
+      );
+
+  TextStyle get _timerStyle => _importantStyle.copyWith(
+        fontSize: _timerFontSize,
+      );
+
+  TextStyle get _errorStyle => new TextStyle(
+        color: Colors.red[900],
+        fontWeight: FontWeight.w900,
+        fontSize: _errorFontSize,
+      );
 }
