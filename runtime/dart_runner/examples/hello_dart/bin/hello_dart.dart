2b7afa4e40505e8e8660d8a1d2b637f10da5ab6a
P.Y. Laligand <pylaligand@google.com>
2017-10-16 20:00:08 -0700

    Attempt to fix build flakiness.
    This is likely due to the new analysis method. The analysis server expects a package structure a bit more strict than the old method, which is why we can't nest packages anymore.
    
    Change-Id: Icb22d49b4e383907bc220d0518d3fa2e852311ca

--- runtime/dart_runner/examples/hello_dart/bin/hello_dart.dart
+++ runtime/dart_runner/examples/hello_dart/bin/hello_dart.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:topaz.runtime.dart_runner.examples.hello_dart.greeting/greeting.dart';
+import 'package:topaz.runtime.dart_runner.examples.greeting/greeting.dart';
 
 void main(List args) {
   print(greeting() + ', Dart!');
