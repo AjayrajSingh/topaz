0a3ee463c52768c331f6b351a83d9bd2a4de6cab
Andrew Wilson <apwilson@google.com>
2017-10-19 10:05:53 -0700

    Allow idle model timeout to be changed.
    Change-Id: Ie3d26e0d451ba1649ae744cd088e8994d5862074

--- public/dart/widgets/lib/src/model/idle_model.dart
+++ public/dart/widgets/lib/src/model/idle_model.dart
@@ -11,32 +11,46 @@ export 'model.dart' show ScopedModel, Model, ScopedModelDescendant;
 
 const RK4SpringDescription _kSimulationDesc =
     const RK4SpringDescription(tension: 250.0, friction: 50.0);
-const Duration _kIdleModeTimeout = const Duration(seconds: 60);
+const Duration _kIdleModeTimeout = const Duration(seconds: 600);
 
 /// Handles transitioning into Idle mode.
 class IdleModel extends SpringModel {
   Timer _userInteractionTimer;
   bool _isIdle = false;
 
+  /// The time to wait before triggering idle mode due to user inaction.
+  Duration _timeout;
+
   /// Constructor.
-  IdleModel() : super(springDescription: _kSimulationDesc) {
+  IdleModel({Duration timeout: _kIdleModeTimeout})
+      : assert(timeout != null),
+        _timeout = timeout,
+        super(springDescription: _kSimulationDesc) {
+    this.timeout = timeout;
+  }
+
+  /// Sets a new timeout to wait before entering idle mode due to user inaction.
+  set timeout(Duration newTimeout) {
+    _timeout = newTimeout;
+    _userInteractionTimer?.cancel();
     _userInteractionTimer = new Timer(
-      _kIdleModeTimeout,
-      _enterIdleMode,
+      _timeout,
+      enterIdleMode,
     );
   }
 
   /// Adjusts idle mode based on the user interaction.
   void onUserInteraction() {
-    _leaveIdleMode();
+    leaveIdleMode();
     _userInteractionTimer?.cancel();
     _userInteractionTimer = new Timer(
-      _kIdleModeTimeout,
-      _enterIdleMode,
+      _timeout,
+      enterIdleMode,
     );
   }
 
-  void _enterIdleMode() {
+  /// Enters idle mode.
+  void enterIdleMode() {
     target = 1.0;
     if (!_isIdle) {
       _isIdle = true;
@@ -44,7 +58,8 @@ class IdleModel extends SpringModel {
     }
   }
 
-  void _leaveIdleMode() {
+  /// Leaves idle mode.
+  void leaveIdleMode() {
     target = 0.0;
     if (_isIdle) {
       _isIdle = false;
