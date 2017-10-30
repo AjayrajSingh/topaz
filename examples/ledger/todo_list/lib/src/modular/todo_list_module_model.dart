c71c57ea64e3ecc4f1b70b1db4a967ee22d01df1
YoungSeok Yoon <youngseokyoon@google.com>
2017-10-30 14:03:39 +0900

    Add ledger todo_list example
    Change-Id: I73d8b022637edaf91730586a0bdb508829336401

---
+++ examples/ledger/todo_list/lib/src/modular/todo_list_module_model.dart
@@ -0,0 +1,118 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:lib.app.fidl/service_provider.fidl.dart';
+import 'package:lib.fidl.dart/bindings.dart';
+import 'package:lib.ledger.fidl/ledger.fidl.dart' as ledger;
+import 'package:lib.module.fidl/module_context.fidl.dart';
+import 'package:lib.story.fidl/link.fidl.dart';
+import 'package:lib.widgets/modular.dart';
+
+import '../ledger_helpers.dart';
+
+/// The model for the todo list module.
+class TodoListModuleModel extends ModuleModel implements ledger.PageWatcher {
+  final Random _random = new Random(new DateTime.now().millisecondsSinceEpoch);
+
+  final ComponentContextProxy _componentContext = new ComponentContextProxy();
+
+  final ledger.PageWatcherBinding _pageWatcherBinding =
+      new ledger.PageWatcherBinding();
+
+  final ledger.LedgerProxy _ledger = new ledger.LedgerProxy();
+
+  final ledger.PageProxy _page = new ledger.PageProxy();
+
+  /// The todo items. The source of truth is Ledger, so [_items] should never be
+  /// modified directly.
+  Map<List<int>, String> _items = <List<int>, String>{};
+
+  /// Retrieves the todo items.
+  Map<List<int>, String> get items => _items;
+
+  /// Implementation of ModuleModel.onReady():
+  @override
+  void onReady(ModuleContext moduleContext, Link link,
+      ServiceProvider incomingServiceProvider) {
+    moduleContext.getComponentContext(_componentContext.ctrl.request());
+    _componentContext.getLedger(
+      _ledger.ctrl.request(),
+      handleLedgerResponse('getLedger'),
+    );
+    _ledger.getRootPage(
+      _page.ctrl.request(),
+      handleLedgerResponse('getRootPage'),
+    );
+
+    ledger.PageSnapshotProxy snapshot = new ledger.PageSnapshotProxy();
+    _page.getSnapshot(
+      snapshot.ctrl.request(),
+      null,
+      _pageWatcherBinding.wrap(this),
+      handleLedgerResponse('Watch'),
+    );
+
+    _readItems(snapshot);
+    super.onReady(moduleContext, link, incomingServiceProvider);
+  }
+
+  /// Implementation of ModuleModel.onStop():
+  @override
+  void onStop() {
+    _pageWatcherBinding.close();
+    _componentContext.ctrl.close();
+    _ledger.ctrl.close();
+    _page.ctrl.close();
+  }
+
+  /// Implementation of PageWatcher.onChange().
+  @override
+  void onChange(ledger.PageChange pageChange, ledger.ResultState resultState,
+      void callback(InterfaceRequest<ledger.PageSnapshot> snapshotRequest)) {
+    if (resultState != ledger.ResultState.completed &&
+        resultState != ledger.ResultState.partialStarted) {
+      print(
+          '[Todo List] Unexpected result state in Ledger watcher: $resultState');
+      callback(null);
+      return;
+    }
+    ledger.PageSnapshotProxy snapshot = new ledger.PageSnapshotProxy();
+    callback(snapshot.ctrl.request());
+    _readItems(snapshot);
+  }
+
+  /// Marks the item of the given [id] as done.
+  void markItemDone(List<int> id) {
+    _page.delete(id, handleLedgerResponse('Delete'));
+  }
+
+  /// Adds a new todo item with the given [content].
+  void addItem(String content) {
+    _page.put(_makeKey(), UTF8.encode(content), handleLedgerResponse('Put'));
+  }
+
+  void _readItems(ledger.PageSnapshotProxy snapshot) {
+    getEntriesFromSnapshot(snapshot,
+        (ledger.Status status, Map<List<int>, String> items) {
+      if (handleLedgerResponse('getEntries')(status)) {
+        return;
+      }
+
+      _items = items;
+      notifyListeners();
+      snapshot.ctrl.close();
+    });
+  }
+
+  List<int> _makeKey() {
+    List<int> key = <int>[];
+    for (int i = 0; i < 16; i++) {
+      key.add(_random.nextInt(256));
+    }
+    return key;
+  }
+}
---
+++ examples/ledger/todo_list/lib/src/modular/todo_list_module_screen.dart
@@ -0,0 +1,93 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:lib.widgets/modular.dart';
+
+import '../widgets/new_item_input.dart';
+import 'todo_list_module_model.dart';
+
+/// Widget rendering a single todo item.
+class _TodoItem extends StatelessWidget {
+  const _TodoItem({Key key, this.content, this.onDone}) : super(key: key);
+
+  final String content;
+  final VoidCallback onDone;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+
+    List<Widget> rowChildren = <Widget>[
+      new Expanded(child: new Text(content)),
+      new SizedBox(
+        width: 72.0,
+        child: new IconButton(
+          icon: new Icon(Icons.done),
+          color: themeData.primaryColor,
+          onPressed: onDone,
+        ),
+      ),
+    ];
+    return new Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: new Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: rowChildren),
+    );
+  }
+}
+
+/// Callback type for an item becoming done.
+typedef void ItemDoneCallback(List<int> id);
+
+/// The top level widget for the todo list module
+class TodoListModuleScreen extends StatelessWidget {
+  final double _appBarHeight = 120.0;
+
+  /// Constructor
+  const TodoListModuleScreen({Key key, this.onNewItem, this.onItemDone})
+      : super(key: key);
+
+  /// New item callback.
+  final NewItemCallback onNewItem;
+
+  /// Item done callback.
+  final ItemDoneCallback onItemDone;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new ScopedModelDescendant<TodoListModuleModel>(builder: (
+        BuildContext context,
+        Widget child,
+        TodoListModuleModel model,
+      ) {
+        List<Widget> listItems = <Widget>[];
+        model.items.forEach((List<int> key, String value) {
+          listItems.add(new _TodoItem(
+            content: value,
+            onDone: () {
+              onItemDone(key);
+            },
+          ));
+        });
+
+        List<Widget> slivers = <Widget>[
+          new SliverAppBar(
+            expandedHeight: _appBarHeight,
+            pinned: true,
+            flexibleSpace: const FlexibleSpaceBar(
+              title: const Text('Todo List'),
+            ),
+          ),
+          new SliverToBoxAdapter(child: new NewItemInput(onNewItem: onNewItem)),
+          new SliverList(delegate: new SliverChildListDelegate(listItems))
+        ];
+
+        return new Material(child: new CustomScrollView(slivers: slivers));
+      }),
+    );
+  }
+}
