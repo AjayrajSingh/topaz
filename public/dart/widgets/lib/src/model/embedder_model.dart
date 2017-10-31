a8b7eb7a0f3cddb90b8539909714686bdef26af2
Jason Campbell <jasoncampbell@google.com>
2017-10-31 16:23:52 -0700

    chore(widgets/embedder): remove startModule
    Change-Id: Ia823d5e3f21bc0d70f594bc7d0dc81409f3e9889

--- public/dart/widgets/lib/src/model/embedder_model.dart
+++ public/dart/widgets/lib/src/model/embedder_model.dart
@@ -2,10 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:async';
-
 import 'package:flutter/widgets.dart';
-import 'package:meta/meta.dart';
 
 import 'model.dart';
 
@@ -44,11 +41,4 @@ abstract class EmbedderModel extends Model {
   /// The build method to be used by builders of any
   /// ScopedModelDescendant<EmbedderModel>.
   Widget build(BuildContext context);
-
-  /// Stub for starting a module.
-  Future<Null> startModule({
-    @required String uri,
-    @required String name,
-    @required String data,
-  }) async {}
 }
