bd7e21c9a657672c11d1f1dcae636da3dc27ff7b
Jason Campbell <jasoncampbell@google.com>
2017-11-02 16:38:19 -0700

    TBR feat(codelab): add fuchsia dart package
    Related to SO-800 - refactor Dart libraries for Fuchsia interactions.
    
    Change-Id: I9298a300bc49159c0e1560b116411371cae84780

---
+++ public/dart/fuchsia/lib/src/peridot/module_impl.dart
@@ -0,0 +1,169 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:fuchsia' as fuchsia;
+
+import 'package:lib.app.dart/app.dart';
+import 'package:lib.app.fidl/service_provider.fidl.dart';
+import 'package:lib.fidl.dart/bindings.dart';
+import 'package:lib.lifecycle.fidl/lifecycle.fidl.dart';
+import 'package:lib.module.fidl/module.fidl.dart';
+import 'package:lib.module.fidl/module_context.fidl.dart';
+import 'package:lib.story.fidl/link.fidl.dart';
+import 'package:lib.user.fidl/device_map.fidl.dart';
+
+import 'link_watcher_impl.dart';
+
+/// Called when [Module.initialize] occurs.
+typedef void OnModuleReady(
+  ModuleContext moduleContext,
+  Link link,
+  ServiceProvider incomingServiceProvider,
+);
+
+/// Called at the beginning of [Lifecycle.terminate].
+typedef void OnModuleStopping();
+
+/// Called at the conclusion of [Lifecycle.terminate].
+typedef void OnModuleStop();
+
+/// Called when the device map entry for the current device changes.
+typedef void OnDeviceMapChange(DeviceMapEntry deviceMapEntry);
+
+/// Implements a Module for receiving the services a [Module] needs to
+/// operate.  When [initialize] is called, the services it receives are routed
+/// by this class to the various classes which need them.
+class ModuleImpl implements Module, Lifecycle {
+  final ModuleContextProxy _moduleContextProxy = new ModuleContextProxy();
+  final LinkProxy _linkProxy = new LinkProxy();
+  final ServiceProviderProxy _incomingServiceProviderProxy =
+      new ServiceProviderProxy();
+  final ServiceProviderBinding _outgoingServiceProviderBinding =
+      new ServiceProviderBinding();
+  final DeviceMapProxy _deviceMapProxy = new DeviceMapProxy();
+  final DeviceMapWatcherBinding _deviceMapWatcherBinding =
+      new DeviceMapWatcherBinding();
+
+  LinkWatcherBinding _linkWatcherBinding;
+  LinkWatcherImpl _linkWatcherImpl;
+
+  /// The [ServiceProvider] to provide when outgoing services are requested.
+  final ServiceProvider outgoingServiceProvider;
+
+  /// The application context to use to get various system services.
+  final ApplicationContext applicationContext;
+
+  /// Called when [Module] is initialied with its services.
+  final OnModuleReady onReady;
+
+  /// Called at the beginning of [Lifecycle.terminate].
+  final OnModuleStopping onStopping;
+
+  /// Called at the conclusion of [Lifecycle.terminate].
+  final OnModuleStop onStop;
+
+  /// Called when [LinkWatcher.notify] is called.
+  final OnNotify onNotify;
+
+  /// Called when the device map entry for the current device changes.
+  final OnDeviceMapChange onDeviceMapChange;
+
+  /// Indicates whether the [LinkWatcher] should watch for all changes including
+  /// the changes made by this [Module]. If `true`, it calls [Link.watchAll] to
+  /// register the [LinkWatcher], and [Link.watch] otherwise. Only takes effect
+  /// when the [onNotify] callback is also provided. Defaults to `false`.
+  final bool watchAll;
+
+  /// Constuctor.
+  ModuleImpl({
+    this.applicationContext,
+    this.outgoingServiceProvider,
+    this.onReady,
+    this.onStopping,
+    this.onStop,
+    this.onNotify,
+    this.onDeviceMapChange,
+    bool watchAll,
+  })
+      : watchAll = watchAll ?? false;
+
+  @override
+  void initialize(
+    InterfaceHandle<ModuleContext> moduleContext,
+    InterfaceHandle<ServiceProvider> incomingServices,
+    InterfaceRequest<ServiceProvider> outgoingServices,
+  ) {
+    print('===> module_impl initializing...');
+    if (onReady != null) {
+      print('===> registering on ready callback');
+      _moduleContextProxy.ctrl.bind(moduleContext);
+      _moduleContextProxy.getLink(null, _linkProxy.ctrl.request());
+
+      if (incomingServices != null) {
+        _incomingServiceProviderProxy.ctrl.bind(incomingServices);
+      }
+
+      onReady(_moduleContextProxy, _linkProxy, _incomingServiceProviderProxy);
+    }
+
+    if (outgoingServices != null && outgoingServiceProvider != null) {
+      _outgoingServiceProviderBinding.bind(
+        outgoingServiceProvider,
+        outgoingServices,
+      );
+    }
+
+    if (onNotify != null) {
+      print('===> registering onNotify callback');
+
+      _linkWatcherImpl = new LinkWatcherImpl(onNotify: onNotify);
+      _linkWatcherBinding = new LinkWatcherBinding();
+
+      if (watchAll) {
+        _linkProxy.watchAll(_linkWatcherBinding.wrap(_linkWatcherImpl));
+      } else {
+        print('===> watching link');
+        _linkProxy.watch(_linkWatcherBinding.wrap(_linkWatcherImpl));
+      }
+    }
+
+    if (onDeviceMapChange != null) {
+      connectToService(
+        applicationContext.environmentServices,
+        _deviceMapProxy.ctrl,
+      );
+      _deviceMapProxy.watchDeviceMap(
+        _deviceMapWatcherBinding.wrap(
+          new _DeviceMapWatcherImpl(onDeviceMapChange),
+        ),
+      );
+    }
+  }
+
+  @override
+  void terminate() {
+    print('===> terminating');
+    onStopping?.call();
+    _linkWatcherBinding?.close();
+    _moduleContextProxy.ctrl.close();
+    _linkProxy.ctrl.close();
+    _incomingServiceProviderProxy.ctrl.close();
+    _outgoingServiceProviderBinding.close();
+    _deviceMapProxy.ctrl.close();
+    _deviceMapWatcherBinding.close();
+    onStop?.call();
+    fuchsia.exit(0);
+  }
+}
+
+class _DeviceMapWatcherImpl extends DeviceMapWatcher {
+  OnDeviceMapChange _onDeviceMapChange;
+
+  _DeviceMapWatcherImpl(this._onDeviceMapChange);
+
+  @override
+  void onDeviceMapChange(DeviceMapEntry entry) {
+    _onDeviceMapChange?.call(entry);
+  }
+}
