5b5873b5028b6f660a4ef18237028339a72adf51
Arman Uguray <armansito@google.com>
2017-11-22 23:48:14 -0800

    [bt] Add local GATT service example
    Added a toy module that displays a square which can be remotely
    manipulated using GATT. The module demonstrates registering a
    local GATT service and accepting connections as an LE peripheral.
    
    NET-359 #done
    
    Change-Id: Ib159fb02271733ebc2e0add5cfe9e29a47fdb46e

---
+++ examples/bluetooth/ble_rect/lib/main.dart
@@ -0,0 +1,29 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:lib.app.dart/app.dart';
+import 'package:lib.logging/logging.dart';
+import 'package:lib.widgets/modular.dart';
+import 'src/modular/module_model.dart';
+import 'src/screen.dart';
+
+void main() {
+  setupLogger();
+
+  ApplicationContext appContext = new ApplicationContext.fromStartupInfo();
+
+  BLERectModuleModel model = new BLERectModuleModel(appContext);
+
+  ModuleWidget<BLERectModuleModel> moduleWidget =
+      new ModuleWidget<BLERectModuleModel>(
+          moduleModel: model,
+          applicationContext: appContext,
+          child: new BLERectScreen());
+
+  model.start();
+
+  runApp(new MaterialApp(home: moduleWidget));
+}
---
+++ examples/bluetooth/ble_rect/lib/src/modular/module_model.dart
@@ -0,0 +1,346 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:math' as math;
+import 'dart:typed_data';
+import 'dart:ui';
+
+import 'package:lib.app.dart/app.dart';
+import 'package:lib.bluetooth.fidl/common.fidl.dart' as bt;
+import 'package:lib.bluetooth.fidl/gatt.fidl.dart' as gatt;
+import 'package:lib.bluetooth.fidl/low_energy.fidl.dart' as ble;
+import 'package:lib.logging/logging.dart';
+import 'package:lib.widgets/modular.dart';
+
+typedef bool _ValueWriteFunc(List<int> value);
+
+/// The [ModuleModel] for the GATT Server example.
+class BLERectModuleModel extends ModuleModel
+    implements ble.PeripheralDelegate, gatt.ServiceDelegate {
+  // Custom UUID for our service.
+  static const String _serviceUuid = '548c2932-f58c-4c0b-9a4d-92110695a591';
+
+  // Our service exposes 3 characteristics to control the color, rotation, and
+  // scale, transforms of our square. Each characteristic comes with a
+  // descriptor that describes the characteristic's function in a human readable
+  // form.
+  static const String _colorUuid = '2bf96f76-f872-422e-8dbd-d2b425850d91';
+  static const int _colorId = 0;
+  static const int _colorDescId = 1;
+  static const String _colorDesc = 'RGB triplet (3 octets)';
+
+  static const String _scaleUuid = '4939518b-b222-404d-90b5-7f675f13f27f';
+  static const int _scaleId = 2;
+  static const int _scaleDescId = 3;
+  static const String _scaleDesc = 'scale percentage (uint8)';
+
+  static const String _rotateUuid = 'f1121828-32b3-4675-a46e-db826531c348';
+  static const int _rotateId = 4;
+  static const int _rotateDescId = 5;
+  static const String _rotateDesc = 'rotation in degrees (uint16)';
+
+  // UUID for the "Characteristic User Description" descriptor.
+  static const String _descUuid = '00002901-0000-1000-8000-00805F9B34FB';
+
+  /// Constructor.
+  BLERectModuleModel(this.applicationContext) : super();
+
+  /// We use the |applicationContext| to obtain handles to environment services.
+  final ApplicationContext applicationContext;
+
+  /// Returns the last FIDL status.
+  bt.Status get lastStatus => _lastStatus;
+
+  /// Returns true if a Central device is currently connected to us.
+  bool get isCentralConnected => _currentCentral != null;
+
+  /// Returns the name of the currently connected central, or null if not
+  /// available.
+  String get connectedCentralId => _currentCentral?.identifier;
+
+  // The low_energy.Peripheral service is used to accept connections from
+  // centrals.
+  final ble.PeripheralProxy _peripheral = new ble.PeripheralProxy();
+  final ble.PeripheralDelegateBinding _peripheralDelegate =
+      new ble.PeripheralDelegateBinding();
+
+  // The gatt.Server service is used to publish our service.
+  final gatt.ServerProxy _server = new gatt.ServerProxy();
+
+  // The delegate binding.
+  final gatt.ServiceDelegateBinding _serviceDelegate =
+      new gatt.ServiceDelegateBinding();
+
+  // The gatt.Service interface can be used to perform service actions, such as
+  // sending characteristic value notifications.
+  final gatt.ServiceProxy _service = new gatt.ServiceProxy();
+
+  // The currently connected BLE central, if any.
+  ble.RemoteDevice _currentCentral;
+
+  // The most recent request status. We use this to display an error when
+  // something goes wrong.
+  bt.Status _lastStatus;
+
+  /// The current color.
+  Color get color => _color;
+  Color _color = const Color(0xFF00FF00);
+
+  /// The current scale factor.
+  double get scale => _scale;
+  double _scale = 1.0;
+
+  /// The current rotation in radians.
+  double get radians => _radians;
+  double _radians = 0.0;
+
+  // Publishes our GATT service.
+  void _publishService() {
+    // Our characteristics have the lowest security requirement.
+    final gatt.SecurityRequirements sec = const gatt.SecurityRequirements(
+      encryptionRequired: false,
+      authenticationRequired: false,
+      authorizationRequired: false,
+    );
+
+    // Reads are allowed without security. Writes are not allowed.
+    final gatt.AttributePermissions readOnlyPermissions =
+        new gatt.AttributePermissions(read: sec);
+
+    // Writes are allowed without security. Reads are not allowed.
+    final gatt.AttributePermissions writeOnlyPermissions =
+        new gatt.AttributePermissions(write: sec);
+
+    // All of our characteristics have this property.
+    final List<gatt.CharacteristicProperty> props =
+        <gatt.CharacteristicProperty>[gatt.CharacteristicProperty.write];
+
+    // Color
+    final gatt.Characteristic color = new gatt.Characteristic(
+        id: _colorId,
+        type: _colorUuid,
+        properties: props,
+        permissions: writeOnlyPermissions,
+        descriptors: <gatt.Descriptor>[
+          new gatt.Descriptor(
+              id: _colorDescId,
+              type: _descUuid,
+              permissions: readOnlyPermissions)
+        ]);
+
+    // Scale
+    final gatt.Characteristic scale = new gatt.Characteristic(
+        id: _scaleId,
+        type: _scaleUuid,
+        properties: props,
+        permissions: writeOnlyPermissions,
+        descriptors: <gatt.Descriptor>[
+          new gatt.Descriptor(
+              id: _scaleDescId,
+              type: _descUuid,
+              permissions: readOnlyPermissions)
+        ]);
+
+    // Rotate
+    final gatt.Characteristic rotate = new gatt.Characteristic(
+        id: _rotateId,
+        type: _rotateUuid,
+        properties: props,
+        permissions: writeOnlyPermissions,
+        descriptors: <gatt.Descriptor>[
+          new gatt.Descriptor(
+              id: _rotateDescId,
+              type: _descUuid,
+              permissions: readOnlyPermissions)
+        ]);
+
+    final gatt.ServiceInfo service = new gatt.ServiceInfo(
+        id: 0,
+        primary: true,
+        type: _serviceUuid,
+        characteristics: <gatt.Characteristic>[color, scale, rotate]);
+
+    _service.ctrl.close();
+    _serviceDelegate.close();
+
+    _server.publishService(
+        service, _serviceDelegate.wrap(this), _service.ctrl.request(),
+        (bt.Status status) {
+      if (status.error != null) {
+        _lastStatus = status;
+      }
+      log.info('publishService (status: $status)');
+      notifyListeners();
+    });
+  }
+
+  // Initiates LE advertising.
+  void _startAdvertising() {
+    _currentCentral = null;
+
+    final ble.AdvertisingData data = const ble.AdvertisingData(
+      name: 'BLE Rect',
+      serviceUuids: const <String>[_serviceUuid],
+    );
+
+    // Unbind if the delegate was previously bound.
+    _peripheralDelegate.close();
+
+    // Make this app connectable by providing a peripheral delegate.
+    _peripheral
+        .startAdvertising(data, null, _peripheralDelegate.wrap(this), 60, false,
+            (bt.Status status, String advertisementId) {
+      log.info('startAdverising status: $status');
+      if (status.error != null) {
+        _lastStatus = status;
+      }
+
+      notifyListeners();
+    });
+
+    notifyListeners();
+  }
+
+  /// Connect to the BLE environment services and bootstrap the GATT service.
+  void start() {
+    connectToService(applicationContext.environmentServices, _server.ctrl);
+    connectToService(applicationContext.environmentServices, _peripheral.ctrl);
+
+    _publishService();
+    _startAdvertising();
+  }
+
+  // ModuleModel override:
+  @override
+  void onStop() {
+    _peripheral.ctrl.close();
+    _server.ctrl.close();
+    _service.ctrl.close();
+
+    _peripheralDelegate.close();
+    _serviceDelegate.close();
+
+    super.onStop();
+  }
+
+  // ble.PeripheralDelegate overrides:
+
+  @override
+  void onCentralConnected(String advertisementId, ble.RemoteDevice central) {
+    log.info('Central connected: $central');
+    _currentCentral = central;
+    notifyListeners();
+  }
+
+  @override
+  void onCentralDisconnected(String deviceId) {
+    log.info('Central disconnected: $deviceId');
+
+    if (deviceId == _currentCentral?.identifier) {
+      // Start listening for new incoming connections.
+      _startAdvertising();
+    }
+  }
+
+  // gatt.ServiceDelegate overrides:
+
+  @override
+  void onCharacteristicConfiguration(
+      int characteristicId, String peerId, bool notify, bool indicate) {}
+
+  @override
+  void onReadValue(int id, int offset,
+      void callback(List<int> value, gatt.ErrorCode status)) {
+    if (offset != 0) {
+      callback(null, gatt.ErrorCode.invalidOffset);
+      return;
+    }
+
+    String description;
+    switch (id) {
+      case _colorDescId:
+        description = _colorDesc;
+        break;
+      case _scaleDescId:
+        description = _scaleDesc;
+        break;
+      case _rotateDescId:
+        description = _rotateDesc;
+        break;
+      default:
+        callback(null, gatt.ErrorCode.notPermitted);
+        return;
+    }
+
+    callback(description.runes.toList(), gatt.ErrorCode.noError);
+  }
+
+  bool _writeColor(final List<int> value) {
+    if (value.length != 3) {
+      log.info('Malformed color value (size: ${value.length})');
+      return false;
+    }
+
+    _color = new Color.fromARGB(255, value[0], value[1], value[2]);
+    log.info('Color written: $_color');
+
+    return true;
+  }
+
+  bool _writeScale(final List<int> value) {
+    if (value.length != 1) {
+      log.info('Malformed scale value (size: ${value.length})');
+      return false;
+    }
+
+    _scale = value[0].toDouble() / 100.0;
+    log.info('Scale written: $_scale');
+
+    return true;
+  }
+
+  bool _writeRotate(final List<int> value) {
+    if (value.length != 2) {
+      log.info('Malformed rotation value (size: ${value.length})');
+      return false;
+    }
+
+    ByteBuffer buffer = new Uint8List.fromList(value).buffer;
+    ByteData bdata = new ByteData.view(buffer);
+    double angle = bdata.getUint16(0, Endianness.LITTLE_ENDIAN).toDouble();
+    _radians = angle * math.PI / 180.0;
+    log.info('Rotate written (angle: $angle, radians: $_radians)');
+
+    return true;
+  }
+
+  @override
+  void onWriteValue(int id, int offset, List<int> value,
+      void callback(gatt.ErrorCode status)) {
+    if (offset != 0) {
+      callback(gatt.ErrorCode.invalidOffset);
+      return;
+    }
+
+    _ValueWriteFunc func;
+    if (id == _colorId) {
+      func = _writeColor;
+    } else if (id == _scaleId) {
+      func = _writeScale;
+    } else if (id == _rotateId) {
+      func = _writeRotate;
+    }
+
+    if (!func(value)) {
+      callback(gatt.ErrorCode.invalidValueLength);
+      return;
+    }
+
+    callback(gatt.ErrorCode.noError);
+    notifyListeners();
+  }
+
+  @override
+  void onWriteWithoutResponse(int id, int offset, List<int> value) {}
+}
