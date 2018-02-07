2a153af5a26ebc530b0c3771adc2e4d19ca7b726
David Yang <dayang@google.com>
2018-02-06 17:59:17 -0800

    [lib.widgets] Simplified (dank) User Shell Widget
    Change-Id: Ifc328dac782564187329216f63637df5b8460047

--- public/dart/widgets/lib/modular.dart
+++ public/dart/widgets/lib/modular.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+export 'src/modular/dank_user_shell_widget.dart';
 export 'src/modular/device_shell_model.dart';
 export 'src/modular/device_shell_widget.dart';
 export 'src/modular/module_model.dart';
---
+++ public/dart/widgets/lib/src/modular/dank_user_shell_widget.dart
@@ -0,0 +1,119 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:lib.app.dart/app.dart';
+import 'package:lib.lifecycle.fidl/lifecycle.fidl.dart';
+import 'package:lib.user.fidl/user_shell.fidl.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:lib.fidl.dart/bindings.dart';
+import 'package:lib.user.dart/user.dart';
+
+import '../widgets/window_media_query.dart';
+
+export 'package:lib.app.dart/app.dart' show ApplicationContext;
+
+/// A wrapper widget intended to be the root of the application that is
+/// a [UserShell].  Its main purpose is to hold the [ApplicationContext] and
+/// [UserShell] instances so they aren't garbage collected.
+/// For convenience, [advertise] does the advertising of the app as a
+/// [UserShell] to the rest of the system via the [ApplicationContext].
+class DankUserShellWidget extends StatelessWidget {
+  /// The [ApplicationContext] to [advertise] its [UserShell] services to.
+  final ApplicationContext _applicationContext;
+
+  /// The binding for the [UserShell] service implemented by [UserShellImpl].
+  final UserShellBinding _userShellBinding;
+
+  /// The binding for the [Lifecycle] service implemented by [UserShellImpl].
+  final LifecycleBinding _lifecycleBinding;
+
+  /// The [UserShellImpl] whose services to [advertise].
+  final DankUserShellImpl _userShell;
+
+  /// Callback that is fired when the screen dimensions of the UserShell changes
+  final VoidCallback onWindowMetricsChanged;
+
+  /// [Widget] for the rest of the User Shell
+  final Widget child;
+
+  /// Callback that is fired when the [DankUserShellImpl] is initialized and
+  /// passes the [UserShellContext]
+  final OnDankUserShellReady onReady;
+
+  /// Callback that is fired when the [DankUserShellImpl] is stopped.
+  final VoidCallback onStop;
+
+  /// Constructor.
+  factory DankUserShellWidget({
+    ApplicationContext applicationContext,
+    VoidCallback onWindowMetricsChanged,
+    Widget child,
+    OnDankUserShellReady onReady,
+    VoidCallback onStop,
+  }) {
+    return new DankUserShellWidget._create(
+      applicationContext: applicationContext,
+      onWindowMetricsChanged: onWindowMetricsChanged,
+      child: child,
+      onReady: onReady,
+      onStop: onStop,
+      userShellBinding: new UserShellBinding(),
+      lifecycleBinding: new LifecycleBinding(),
+    );
+  }
+
+  DankUserShellWidget._create({
+    ApplicationContext applicationContext,
+    UserShellBinding userShellBinding,
+    LifecycleBinding lifecycleBinding,
+    this.child,
+    this.onWindowMetricsChanged,
+    this.onReady,
+    this.onStop,
+  })
+      : _applicationContext = applicationContext,
+        _userShellBinding = userShellBinding,
+        _lifecycleBinding = lifecycleBinding,
+        _userShell = new DankUserShellImpl(
+          onStop: () {
+            userShellBinding.close();
+            lifecycleBinding.close();
+            onStop?.call();
+          },
+          onReady: (UserShellContext userShellContext) {
+            onReady?.call(userShellContext);
+          },
+        );
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Material(
+          child: new Directionality(
+        textDirection: TextDirection.ltr,
+        child: new WindowMediaQuery(
+          onWindowMetricsChanged: onWindowMetricsChanged,
+          child: child,
+        ),
+      )),
+    );
+  }
+
+  /// Advertises [_userShell] as a [UserShell] to the rest of the system via
+  /// the [ApplicationContext].
+  void advertise() {
+    _applicationContext.outgoingServices
+      ..addServiceForName(
+        (InterfaceRequest<UserShell> request) =>
+            _userShellBinding.bind(_userShell, request),
+        UserShell.serviceName,
+      )
+      ..addServiceForName(
+        (InterfaceRequest<Lifecycle> request) =>
+            _lifecycleBinding.bind(_userShell, request),
+        Lifecycle.serviceName,
+      );
+  }
+}
