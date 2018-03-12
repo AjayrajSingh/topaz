1f7bdf22e85be9077d7107f259b442f2cb6cbe60
P.Y. Laligand <pylaligand@google.com>
2018-03-12 02:32:01 -0700

    [manifest] Roll Peridot.
    Change-Id: I8675209aacff8cd14582871a4f0fcd3118073ba4

--- examples/ui/sketchy_flutter/lib/src/session.dart
+++ examples/ui/sketchy_flutter/lib/src/session.dart
@@ -4,10 +4,11 @@
 
 import 'package:lib.app.dart/app.dart';
 import 'package:lib.app.fidl._service_provider/service_provider.fidl.dart';
+import 'package:lib.ui.mozart.fidl/commands.fidl.dart';
+import 'package:lib.ui.mozart.fidl/mozart.fidl.dart';
+import 'package:lib.ui.mozart.fidl/session.fidl.dart';
 import 'package:lib.ui.mozart.fidl._presentation_info/presentation_info.fidl.dart';
 import 'package:lib.ui.scenic.fidl/ops.fidl.dart';
-import 'package:lib.ui.scenic.fidl/scene_manager.fidl.dart';
-import 'package:lib.ui.scenic.fidl/session.fidl.dart';
 import 'package:zircon/zircon.dart' as zircon;
 
 export 'package:lib.ui.mozart.fidl._presentation_info/presentation_info.fidl.dart'
@@ -20,28 +21,28 @@ typedef void SessionPresentCallback(PresentationInfo info);
 class Session {
   int _nextResourceId = 1;
   final SessionProxy _session = new SessionProxy();
-  List<Op> _ops = <Op>[];
+  List<Command> _ops = <Command>[];
 
-  Session.fromSceneManager(SceneManagerProxy sceneManager) {
-    sceneManager.createSession(_session.ctrl.request(), null);
+  Session.fromMozart(MozartProxy mozart) {
+    mozart.createSession(_session.ctrl.request(), null);
   }
 
   factory Session.fromServiceProvider(ServiceProvider serviceProvider) {
-    final SceneManagerProxy sceneManager = new SceneManagerProxy();
-    connectToService(serviceProvider, sceneManager.ctrl);
-    return new Session.fromSceneManager(sceneManager);
+    final MozartProxy mozart = new MozartProxy();
+    connectToService(serviceProvider, mozart.ctrl);
+    return new Session.fromMozart(mozart);
   }
 
   bool get hasEnqueuedOps => _ops.isNotEmpty;
 
   void enqueue(Op op) {
-    _ops.add(op);
+    _ops.add(new Command.withScenic(op));
   }
 
   void present(int presentationTime, SessionPresentCallback callback) {
     if (_ops.isNotEmpty) {
       _session.enqueue(_ops);
-      _ops = const <Op>[];
+      _ops = const <Command>[];
     }
     _session.present(
         presentationTime, <zircon.Handle>[], <zircon.Handle>[], callback);
