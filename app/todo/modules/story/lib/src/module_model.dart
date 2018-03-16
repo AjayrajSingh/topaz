0c2a86c0ef2a3698e8c1b680118bec61f4ad254f
Przemyslaw Pietrzkiewicz <ppi@google.com>
2018-03-16 13:40:44 +0100

    [todo] drop the unfinished Rust-backed TODO app.
    This patch removes a skeleton of an unfinished TODO app. (Including a
    backend provider in Rust, which we apparently cannot compile in topaz at
    the moment.
    
    (Side-note: the idea of splitting the app into an entity provider agent
    and the UI module seems interesting. We could aim to rework this way the
    other todo app in topaz: //topaz/examples/ledger/todo_list)
    
    Change-Id: I91993a86de954a809d605f05b65797efc7e87a4a

--- app/todo/modules/story/lib/src/module_model.dart
+++
@@ -1,9 +0,0 @@
-// Copyright 2017 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-import 'package:lib.ui.flutter/child_view.dart';
-import 'package:lib.widgets/modular.dart';
-
-/// A [ModuleModel] providing the [ChildViewConnection]s of the child modules.
-class StoryModuleModel extends ModuleModel {}
