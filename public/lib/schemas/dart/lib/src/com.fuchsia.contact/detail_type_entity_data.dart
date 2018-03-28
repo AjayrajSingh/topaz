73d0b04be3267f0d1cc5545d1ac97679650dab67
Meiyi Alice Li <meiyili@google.com>
2018-03-27 20:09:44 -0700

    [contact] Migrate contact entity schemas into entity codecs.
    MS-1342
    
    Change-Id: Iea24353481a21fd14f3fe6f91beb3bd80cbb5116

---
+++ public/lib/schemas/dart/lib/src/com.fuchsia.contact/detail_type_entity_data.dart
@@ -0,0 +1,15 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// The type of detail to show with the contact's display name
+enum DetailTypeEntityData {
+  /// Primary email of the contact
+  email,
+
+  /// Primary phone number of the contact
+  phoneNumber,
+
+  /// Custom contact information
+  custom,
+}
