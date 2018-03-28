ae62da4908e1db6f03e885995b8a3914cf32fb8e
Meiyi Alice Li <meiyili@google.com>
2018-03-27 17:59:00 -0700

    [contacts] Remove legacy contacts module.
    MS-1334 #done
    
    Change-Id: Ida44032cffad42c01e777ca3ac790a356891d48e

--- app/contacts/modules/contacts/lib/src/screens.dart
+++
@@ -1,6 +0,0 @@
-// Copyright 2016 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-export 'screens/contact_card_screen.dart';
-export 'screens/contact_list_screen.dart';
