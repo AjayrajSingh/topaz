c532e4ed6d1df34a3ab845e76dda6f077482db1b
Ian McKellar <ianloic@google.com>
2018-03-29 13:49:14 -0700

    [fidl2] dart_runner embedder snapshot
    Change-Id: Id09e07129f48ff8e0d3c66ec78fae298a2bea37b

--- runtime/dart_runner/embedder/script_runner_snapshot.dart
+++ runtime/dart_runner/embedder/script_runner_snapshot.dart
@@ -25,55 +25,7 @@ import 'package:zircon/zircon.dart';
 
 // FIDL bindings and application libraries.
 import 'package:lib.app.dart/app.dart';
-import 'package:lib.fidl.dart/bindings.dart';
+import 'package:fidl/fidl.dart';
 
-// From //peridot/public/lib/agent
-import 'package:lib.agent.fidl/agent.fidl.dart';
-import 'package:lib.agent.fidl/agent_context.fidl.dart';
-import 'package:lib.agent.fidl/agent_provider.fidl.dart';
-
-// From //peridot/public/lib/clipboard
-import 'package:lib.clipboard.fidl/clipboard.fidl.dart';
-
-// From //peridot/public/lib/daisy
-import 'package:lib.daisy.fidl/daisy.fidl.dart';
-
-// From //peridot/public/lib/device
-import 'package:lib.device.fidl/device_shell.fidl.dart';
-import 'package:lib.device.fidl/device_runner_monitor.fidl.dart';
-import 'package:lib.device.fidl/user_provider.fidl.dart';
-
-// From //peridot/public/lib/entity
-import 'package:lib.entity.fidl/entity.fidl.dart';
-import 'package:lib.entity.fidl/entity_provider.fidl.dart';
-import 'package:lib.entity.fidl/entity_reference_factory.fidl.dart';
-import 'package:lib.entity.fidl/entity_resolver.fidl.dart';
-
-// From //peridot/public/lib/module
-import 'package:lib.module.fidl/module.fidl.dart';
-import 'package:lib.module.fidl/module_context.fidl.dart';
-
-// From //peridot/public/lib/module_resolver
-import 'package:lib.module_resolver.fidl/module_resolver.fidl.dart';
-
-// From //peridot/public/lib/resolver
-import 'package:lib.resolver.fidl/resolver.fidl.dart';
-
-// From //peridot/public/lib/story
-import 'package:lib.story.fidl/create_link.fidl.dart';
-import 'package:lib.story.fidl/link.fidl.dart';
-import 'package:lib.story.fidl/link_change.fidl.dart';
-import 'package:lib.story.fidl/per_device_story_info.fidl.dart';
-import 'package:lib.story.fidl/story_controller.fidl.dart';
-import 'package:lib.story.fidl/story_info.fidl.dart';
-import 'package:lib.story.fidl/story_marker.fidl.dart';
-import 'package:lib.story.fidl/story_provider.fidl.dart';
-import 'package:lib.story.fidl/story_state.fidl.dart';
-
-// From //peridot/public/lib/user
-import 'package:lib.user.fidl/device_map.fidl.dart';
-import 'package:lib.user.fidl/user_shell.fidl.dart';
-
-// From //peridot/public/lib/user_intelligence
-import 'package:lib.user_intelligence.fidl/intelligence_services.fidl.dart';
-import 'package:lib.user_intelligence.fidl/user_intelligence_provider.fidl.dart';
+// From //peridot/public/lib:fidl
+import 'package:fuchsia.fidl.modular/modular.dart';
