0978d3035ace93c1751c513859f6d2f00e0c2766
Ian McKellar <ianloic@google.com>
2018-03-30 16:31:56 -0700

    [fidl2] port examples/ui/sketchy_flutter
    Change-Id: I8b451111de43c039f5890e72f253a87e4dc4f0bb

--- examples/ui/sketchy_flutter/lib/src/session.dart
+++ examples/ui/sketchy_flutter/lib/src/session.dart
@@ -8,11 +8,11 @@
 import 'package:lib.app.dart/app.dart';
 import 'package:fuchsia.fidl.component/component.dart';
 import 'package:fuchsia.fidl.images/images.dart';
-import 'package:fuchsia.fidl.gfx/gfx.dart' as ui_gfx;
+import 'package:fuchsia.fidl.gfx/gfx.dart' as gfx;
+import 'package:fuchsia.fidl.ui/ui.dart' as ui;
 import 'package:zircon/zircon.dart' as zircon;
 
-export 'package:fuchsia.fidl.images/images.dart'
-    show PresentationInfo;
+export 'package:fuchsia.fidl.images/images.dart' show PresentationInfo;
 
 // ignore_for_file: public_member_api_docs
 
@@ -20,29 +20,29 @@ typedef void SessionPresentCallback(PresentationInfo info);
 
 class Session {
   int _nextResourceId = 1;
-  final SessionProxy _session = new SessionProxy();
-  List<scenic.Command> _commands = <scenic.Command>[];
+  final ui.SessionProxy _session = new ui.SessionProxy();
+  List<ui.Command> _commands = <ui.Command>[];
 
-  Session.fromScenic(ScenicProxy scenic) {
-    ui_gfx.createSession(_session.ctrl.request(), null);
+  Session.fromScenic(ui.ScenicProxy scenic) {
+    scenic.createSession(_session.ctrl.request(), null);
   }
 
   factory Session.fromServiceProvider(ServiceProvider serviceProvider) {
-    final ScenicProxy scenic = new ScenicProxy();
+    final ui.ScenicProxy scenic = new ui.ScenicProxy();
     connectToService(serviceProvider, scenic.ctrl);
     return new Session.fromScenic(scenic);
   }
 
   bool get hasEnqueuedCommands => _commands.isNotEmpty;
 
-  void enqueue(ui_gfx.Command command) {
-    _commands.add(new scenic.Command.withGfx(command));
+  void enqueue(gfx.Command command) {
+    _commands.add(new ui.Command.withGfx(command));
   }
 
   void present(int presentationTime, SessionPresentCallback callback) {
     if (_commands.isNotEmpty) {
       _session.enqueue(_commands);
-      _commands = const <scenic.Command>[];
+      _commands = const <ui.Command>[];
     }
     _session.present(
         presentationTime, <zircon.Handle>[], <zircon.Handle>[], callback);
