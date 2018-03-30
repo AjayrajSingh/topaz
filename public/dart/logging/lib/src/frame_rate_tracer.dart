f337a5815cc7ee38319889019f0d8c5fddfa2656
Andrew Wilson <apwilson@google.com>
2018-03-30 13:49:26 -0700

    [logging] Add optional cobalt metric logging to frame rate tracer.
    Change-Id: I78b5b2291185cd3c7eb8b571331d8fbdd722e32c

--- public/dart/logging/lib/src/frame_rate_tracer.dart
+++ public/dart/logging/lib/src/frame_rate_tracer.dart
@@ -2,23 +2,34 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:fuchsia.fidl.cobalt/cobalt.dart';
+import 'package:meta/meta.dart';
+
 import 'logging.dart';
+
+const int _kNoOpEncodingId = 1;
 
 /// Traces the frame rate of an animation.
 class FrameRateTracer {
   /// Name of the anmiation for tracing purposes.
   final String name;
 
+  /// Optional cobalt encoder.  If not null the frame rate will be logged as
+  /// an observation to cobalt.
+  final CobaltEncoder cobaltEncoder;
+
   DateTime _animationStart = new DateTime.now();
   int _frames = 0;
   String _currentTargetName;
+  int _currentCobaltMetricId;
 
   /// Constructor.
-  FrameRateTracer({this.name});
+  FrameRateTracer({@required this.name, this.cobaltEncoder});
 
   /// Starts tracking an animation.
-  void start({String targetName}) {
+  void start({String targetName, int cobaltMetricId}) {
     _currentTargetName = targetName;
+    _currentCobaltMetricId = cobaltMetricId;
     _animationStart = new DateTime.now();
     _frames = 0;
   }
@@ -43,5 +54,20 @@ class FrameRateTracer {
       '$prefix: ${frameRate.toStringAsPrecision(3)} fps '
           '($_frames/${microSeconds/1000000.0}s)',
     );
+    if (cobaltEncoder != null && _currentCobaltMetricId != null) {
+      cobaltEncoder.addDoubleObservation(
+        _currentCobaltMetricId,
+        _kNoOpEncodingId,
+        frameRate,
+        (Status status) {
+          if (status != Status.ok) {
+            log.warning(
+              'Failed to observe frame rate metric '
+                  '$_currentCobaltMetricId: $status. ',
+            );
+          }
+        },
+      );
+    }
   }
 }
