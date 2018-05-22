fa16a3001661c16ee626f4e228620d3da74d1f62
Ian McKellar <ianloic@google.com>
2018-05-22 08:44:57 -0700

    [dart][fidl] Compatibility test server for Dart
    This works once the fixes for the bugs I found land.
    To actually run it you need to patch
    //garnet/bin/fidl_compatibility_test/compatibility_test.cc to know about
    this test.
    
    It has only been tested against itself and C++ because no other language
    tests are in tree yet.
    
    FIDL-182 #done
    
    Change-Id: Ib427591e0b50676be6660773a85fd9a939c92c16

---
+++ bin/fidl_compatibility_test/main.dart
@@ -0,0 +1,72 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:lib.app.dart/app.dart';
+import 'package:fidl_compatibility_test_service/fidl.dart';
+import 'package:fidl/fidl.dart' show InterfaceRequest;
+import 'package:fidl_component/fidl.dart';
+
+class EchoImpl implements Echo {
+  final ApplicationContext context;
+
+  EchoImpl(this.context);
+
+  void proxyEcho(Struct value, void Function(Struct value) callback) {
+    assert(value.forwardToServer.isNotEmpty);
+
+    final Services services = new Services();
+    final ApplicationLaunchInfo launchInfo = new ApplicationLaunchInfo(
+        url: value.forwardToServer, directoryRequest: services.request());
+    final ApplicationControllerProxy controller =
+        new ApplicationControllerProxy();
+    context.launcher.createApplication(launchInfo, controller.ctrl.request());
+
+    final Struct newValue = new Struct(
+      primitiveTypes: value.primitiveTypes,
+      defaultValues: value.defaultValues,
+      arrays: value.arrays,
+      arrays2d: value.arrays2d,
+      vectors: value.vectors,
+      handles: value.handles,
+      strings: value.strings,
+      defaultEnum: value.defaultEnum,
+      i8Enum: value.i8Enum,
+      i16Enum: value.i16Enum,
+      i32Enum: value.i32Enum,
+      i64Enum: value.i64Enum,
+      u8Enum: value.u8Enum,
+      u16Enum: value.u16Enum,
+      u32Enum: value.u32Enum,
+      u64Enum: value.u64Enum,
+      structs: value.structs,
+      unions: value.unions,
+      b: value.b,
+      forwardToServer: null,
+    );
+
+    final EchoProxy echo = new EchoProxy();
+    services.connectToService(echo.ctrl);
+
+    echo.echoStruct(newValue, callback);
+  }
+
+  @override
+  void echoStruct(Struct value, void Function(Struct value) callback) {
+    if (value.forwardToServer != null && value.forwardToServer.isNotEmpty) {
+      proxyEcho(value, callback);
+    } else {
+      callback(value);
+    }
+  }
+}
+
+final EchoBinding _echoBinding = new EchoBinding();
+
+void main(List<String> args) {
+  final ApplicationContext context = new ApplicationContext.fromStartupInfo();
+  final EchoImpl echoImpl = new EchoImpl(context);
+  context.outgoingServices.addServiceForName(
+      (InterfaceRequest<Echo> request) => _echoBinding.bind(echoImpl, request),
+      Echo.$serviceName);
+}
