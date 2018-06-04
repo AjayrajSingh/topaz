a2595033e49ba43203c307915a433b98b77bfdf9
Eric Jia <ejia@google.com>
2018-06-04 18:35:16 +0000

    [settings][bt] first draft of bluetooth settings
    Renamed ApplicationContext -> StartupContext to fix conflict
    
    This reverts commit 51cd80fb707fbc974e138f80509e90c48bf19e83.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > Revert "[settings][bt] first draft of bluetooth settings"
    >
    > This reverts commit 1cfc8d022f6a844b97cd1baf68c3861a5f0846a8.
    >
    > Reason for revert: <INSERT REASONING HERE>
    >
    > Original change's description:
    > > [settings][bt] first draft of bluetooth settings
    > >
    > > Change-Id: I0432d112e87b757f160da751b53838de1ea9a17a
    >
    > TBR=ejia@google.com,iamjack@google.com
    >
    > Change-Id: Ie411987a76b85127670085adf170ddac2f033c39
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    
    TBR=ejia@google.com,sanjayc@google.com
    
    Change-Id: Id6b4f355e1c50d486ca915a134bfa7ff958397fe
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true

--- bin/bluetooth_settings/lib/src/bluetooth_model.dart
+++ bin/bluetooth_settings/lib/src/bluetooth_model.dart
@@ -1,8 +1,103 @@
 // Copyright 2018 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
+import 'package:fidl_bluetooth_control/fidl.dart';
+import 'package:lib.app.dart/app.dart';
 import 'package:lib.widgets/model.dart';
 
 /// Model containing state needed for the bluetooth settings app.
-class BluetoothSettingsModel extends Model {}
+class BluetoothSettingsModel extends Model {
+  /// Bluetooth controller proxy.
+  final ControlProxy _control = new ControlProxy();
+
+  List<AdapterInfo> _adapters;
+  AdapterInfo _activeAdapter;
+  List<RemoteDevice> _remoteDevices;
+  bool _discoverable = true;
+
+  BluetoothSettingsModel() {
+    _onStart();
+  }
+
+  void setDiscoverable({bool discoverable}) {
+    _control.setDiscoverable(discoverable, (status) {
+      _discoverable = discoverable;
+      notifyListeners();
+    });
+  }
+
+  bool get discoverable => _discoverable;
+
+  /// Bluetooth devices that are seen, but are not connected.
+  Iterable<RemoteDevice> get availableDevices =>
+      _remoteDevices
+          ?.where((remoteDevice) => remoteDevice.connected == false) ??
+      [];
+
+  /// Bluetooth devices that are connected to the current adapter.
+  Iterable<RemoteDevice> get connectedDevices =>
+      _remoteDevices
+          ?.where((remoteDevice) => remoteDevice.connected == false) ??
+      [];
+
+  /// The current adapter that is being used
+  AdapterInfo get activeAdapter => _activeAdapter;
+
+  /// All adapters that are not currently active.
+  Iterable<AdapterInfo> get inactiveAdapters =>
+      _adapters?.where((adapter) => activeAdapter.address != adapter.address) ??
+      [];
+
+  void _onStart() {
+    final startupContext = StartupContext.fromStartupInfo();
+    connectToService(startupContext.environmentServices, _control.ctrl);
+
+    // Just for first draft purposes, refresh whenever there are any changes.
+    // TODO: handle errors, refresh more gracefully
+    _control
+      ..onActiveAdapterChanged = (_) {
+        _refresh();
+      }
+      ..onAdapterRemoved = (_) {
+        _refresh();
+      }
+      ..onAdapterUpdated = (_) {
+        _refresh();
+      }
+      ..onDeviceUpdated = (_) {
+        _refresh();
+      }
+      ..onDeviceRemoved = (_) {
+        _refresh();
+      }
+      ..requestDiscovery(true, (status) {
+        _refresh();
+      })
+      ..setDiscoverable(true, (status) {
+        _refresh();
+      });
+  }
+
+  /// Updates all the state that the model gets from bluetooth.
+  void _refresh() {
+    _control
+      ..getAdapters((adapters) {
+        _adapters = adapters;
+        notifyListeners();
+      })
+      ..getActiveAdapterInfo((adapter) {
+        _activeAdapter = adapter;
+        notifyListeners();
+      })
+      ..getKnownRemoteDevices((remoteDevices) {
+        _remoteDevices = remoteDevices;
+        notifyListeners();
+      });
+  }
+
+  /// Closes the connection to the bluetooth control, thus ending active
+  /// scanning.
+  void dispose() {
+    _control.ctrl.close();
+  }
+}
