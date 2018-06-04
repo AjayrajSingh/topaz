51cd80fb707fbc974e138f80509e90c48bf19e83
Eric Jia <ejia@google.com>
2018-06-04 18:26:15 +0000

    Revert "[settings][bt] first draft of bluetooth settings"
    This reverts commit 1cfc8d022f6a844b97cd1baf68c3861a5f0846a8.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > [settings][bt] first draft of bluetooth settings
    > 
    > Change-Id: I0432d112e87b757f160da751b53838de1ea9a17a
    
    TBR=ejia@google.com,iamjack@google.com
    
    Change-Id: Ie411987a76b85127670085adf170ddac2f033c39
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true

--- bin/bluetooth_settings/lib/src/bluetooth_settings.dart
+++ bin/bluetooth_settings/lib/src/bluetooth_settings.dart
@@ -2,10 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:fidl_bluetooth_control/fidl.dart';
 import 'package:flutter/widgets.dart';
 import 'package:lib.widgets/model.dart';
-import 'package:lib.settings/widgets.dart';
 import 'package:flutter/material.dart';
 
 import 'bluetooth_model.dart';
@@ -13,6 +11,7 @@ import 'bluetooth_model.dart';
 /// Widget that displays bluetooth information, and allows users to
 /// connect and disconnect from devices.
 class BluetoothSettings extends StatelessWidget {
+  /// Const constructor.
   const BluetoothSettings();
 
   @override
@@ -25,126 +24,5 @@ class BluetoothSettings extends StatelessWidget {
       ) =>
               new LayoutBuilder(
                   builder: (BuildContext context, BoxConstraints constraints) =>
-                      Material(
-                          child: _buildBluetoothSettings(
-                              model: model,
-                              scale:
-                                  constraints.maxHeight > 360.0 ? 1.0 : 0.5))));
+                      const Material(child: null)));
 }
-
-typedef BluetoothSettingsSectionBuilder = SettingsSection Function(
-    BluetoothSettingsModel model, double scale);
-
-Widget _buildBluetoothSettings(
-    {@required BluetoothSettingsModel model, @required double scale}) {
-  if (model.activeAdapter == null) {
-    return SettingsPage(
-      scale: scale,
-      sections: [
-        SettingsSection.error(
-          scale: scale,
-          description: 'No bluetooth adapters were found',
-        )
-      ],
-    );
-  }
-
-  return SettingsPage(
-    scale: scale,
-    sections: [_connectedDevices, _availableDevices, _adapters, _settings]
-        .map((BluetoothSettingsSectionBuilder sectionBuilder) =>
-            sectionBuilder(model, scale))
-        .toList(),
-  );
-}
-
-SettingsSection _settings(BluetoothSettingsModel model, double scale) {
-  final discoverableSetting = SettingsSwitchTile(
-    scale: scale,
-    state: model.discoverable,
-    text: 'Discoverable',
-    onSwitch: (value) => model.setDiscoverable(discoverable: value),
-  );
-
-  return SettingsSection(
-      title: 'Settings',
-      scale: scale,
-      child: SettingsItemList(
-        items: [discoverableSetting],
-      ));
-}
-
-SettingsSection _connectedDevices(BluetoothSettingsModel model, double scale) {
-  if (model.connectedDevices.isEmpty) {
-    return SettingsSection.empty();
-  }
-
-  return SettingsSection(
-      title: 'Connected devices',
-      scale: scale,
-      child: SettingsItemList(
-        items:
-            model.connectedDevices.map((device) => _deviceTile(device, scale)),
-      ));
-}
-
-SettingsSection _availableDevices(BluetoothSettingsModel model, double scale) {
-  if (model.availableDevices.isEmpty) {
-    return SettingsSection.error(
-      scale: scale,
-      title: _availableDevicesTitle,
-      description: 'No bluetooth devices available to connect',
-    );
-  }
-
-  return SettingsSection(
-      title: _availableDevicesTitle,
-      scale: scale,
-      child: SettingsItemList(
-        items:
-            model.availableDevices.map((device) => _deviceTile(device, scale)),
-      ));
-}
-
-/// Section containing the list of adapters, both active and not.
-///
-/// In future, this should probably be moved somewhere more hidden, as in the
-/// vast majority of cases, thre should be either one or no adapters.
-SettingsSection _adapters(BluetoothSettingsModel model, double scale) {
-  final _adapters = [_activeAdapterTile(model.activeAdapter, scale)]..addAll(
-      model.inactiveAdapters.map((adapter) => _adapterTile(adapter, scale)));
-
-  return SettingsSection(
-    title: 'Adapters',
-    scale: scale,
-    child: SettingsItemList(items: _adapters),
-  );
-}
-
-SettingsTile _deviceTile(RemoteDevice device, double scale) {
-  return SettingsTile(
-    text: device.name,
-    description: 'ID: ${device.identifier}',
-    iconData: device.connected ? Icons.bluetooth_connected : Icons.bluetooth,
-    scale: scale,
-  );
-}
-
-SettingsTile _adapterTile(AdapterInfo adapter, double scale) {
-  return SettingsTile(
-    text: adapter.identifier,
-    iconData: Icons.bluetooth_disabled,
-    scale: scale,
-  );
-}
-
-SettingsTile _activeAdapterTile(AdapterInfo adapter, double scale) {
-  return SettingsTile(
-    text: adapter.identifier,
-    description: 'connected',
-    iconData: Icons.bluetooth_searching,
-    scale: scale,
-  );
-}
-
-const String _availableDevicesTitle = 'Available Device';
