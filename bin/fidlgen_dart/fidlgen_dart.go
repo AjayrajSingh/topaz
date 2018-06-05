f8e595cf2dd174a2a2fb946bc7f8ada39674aaf7
Ian McKellar <ianloic@google.com>
2018-06-05 16:03:07 -0700

    [dart][fidl] Copy generator into Topaz
    The Dart FIDL generator should be in the Topaz layer with all other
    things Dart. This copies the generator code from Garnet and creates a
    new front-end executable called fidlgen_dart.
    
    Once this lands, https://fuchsia.cl/162580 will switch the Dart FIDL
    rules over to using fidlgen_dart and we can delete the old code from
    Garnet.
    
    FIDL-124 #comment
    
    Change-Id: I96d746676041193f581c56c79d52533543a2192b

---
+++ bin/fidlgen_dart/fidlgen_dart.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fidl/compiler/backend/cmdline"
+	"fidlgen_dart/backend"
+	"flag"
+	"log"
+	"os"
+)
+
+func main() {
+	flags := cmdline.BaseFlags()
+	flag.Parse()
+	if !flag.Parsed() || !flags.Valid() {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	fidl := flags.FidlTypes()
+	config := flags.Config()
+
+	err := backend.GenerateFidl(fidl, &config)
+	if err != nil {
+		log.Fatalf("Error running generator: %v", err)
+	}
+}
