760802950359c54c0c9dec7458960700b081529f
Zach Anderson <zra@google.com>
2018-06-07 10:44:33 -0700

    [flutter_runner] Roll engine. Add back resource release
    Change-Id: I6a394037b8cef19a30d90aa9ffe77df27586b008

--- runtime/flutter_runner/vulkan_surface_producer.cc
+++ runtime/flutter_runner/vulkan_surface_producer.cc
@@ -100,9 +100,9 @@ bool VulkanSurfaceProducer::Initialize(scenic_lib::Session* mozart_session) {
   backend_context_->fMinAPIVersion = application_->GetAPIVersion();
   backend_context_->fFeatures = skia_features;
   backend_context_->fInterface.reset(interface.release());
-  // This should already be set to true by default when we create the
-  // GrVkBackendContext, but we'll set it to true explicitly.
-  backend_context_->fOwnsInstanceAndDevice = true;
+
+  logical_device_->ReleaseDeviceOwnership();
+  application_->ReleaseInstanceOwnership();
 
   context_ = GrContext::MakeVulkan(backend_context_);
 
