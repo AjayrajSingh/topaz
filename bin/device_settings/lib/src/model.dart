5d08b03f299ddf791bb9ef4fe86b198aaf1827a9
Erick Tryzelaar <etryzelaar@google.com>
2018-06-11 10:04:13 -0700

    [amber] rename amber namespace to fuchsia.amber
    F5-45
    
    Change-Id: I8fc3b6e27f3cf231656592fb194994a6b548f04e

--- bin/device_settings/lib/src/model.dart
+++ bin/device_settings/lib/src/model.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:fidl_amber/fidl.dart' as amber;
+import 'package:fidl_fuchsia_amber/fidl.dart' as amber;
 import 'package:lib.app.dart/app.dart';
 import 'package:lib.widgets/model.dart';
 
