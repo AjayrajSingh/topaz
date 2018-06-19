5929f194927745e943af3d2086fa515d71e8f18d
Jaeheon Yi <jaeheon@google.com>
2018-06-19 15:03:03 -0700

    [example] simple flutter app, with one nested flutter app.
    SCN-561 #done
    
    TEST: manual testing
    
    Change-Id: Ia1f1fc6a687f2609d33bf239218548db90f0eb65

---
+++ examples/ui/simple_flutter/lib/leaf.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Leaf Flutter'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+// Update with Tap to increment counter
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text(widget.title),
+        ),
+        body: new Center(
+            child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text('Hi!'),
+          ],
+        )));
+  }
+}
---
+++ examples/ui/simple_flutter/lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:lib.app.dart/app.dart';
+import 'package:lib.widgets/application.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Gesture Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Gesture Demo - Multiple Process'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int leftCounter = 0;
+  int rightCounter = 0;
+
+  List<MaterialColor> colors = <MaterialColor>[
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.blue,
+    Colors.purple
+  ];
+
+  void _onLeftDismissed(DismissDirection direction) {
+    setState(() {
+      leftCounter++;
+    });
+  }
+
+  void _onRightDismissed(DismissDirection direction) {
+    setState(() {
+      rightCounter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        child: new Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new Dismissible(
+                      key: new Key('left$leftCounter'),
+                      onDismissed: _onLeftDismissed,
+                      resizeDuration: null,
+                      child: new Card(
+                        child: new Container(
+                            width: 300.0,
+                            height: 300.0,
+                            color: colors[leftCounter % colors.length]),
+                      )),
+                  new Text('Left, $leftCounter')
+                ]),
+            new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new Dismissible(
+                      key: new Key('right$rightCounter'),
+                      onDismissed: _onRightDismissed,
+                      resizeDuration: null,
+                      child: new Card(
+                          child: new Container(
+                        width: 300.0,
+                        height: 300.0,
+                        child: new ApplicationWidget(
+                            url: 'leaf_flutter',
+                            launcher:
+                                StartupContext.fromStartupInfo().launcher),
+                      ))),
+                  new Text('Right, $rightCounter')
+                ]),
+          ],
+        ),
+      ),
+    );
+  }
+}
