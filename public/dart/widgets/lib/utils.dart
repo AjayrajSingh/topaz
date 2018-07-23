18271a2e7a2a11337d6abe39a66fa2fe019dc237
Eric Jia <ejia@google.com>
2018-07-23 13:03:34 -0700

    [utils] add change notifier list and map classes
    test:none
    
    Change-Id: I8d072a1b552b6570cf1ac724e7e5c1a6b4a9fa9a

--- public/dart/widgets/lib/utils.dart
+++ public/dart/widgets/lib/utils.dart
@@ -2,4 +2,5 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file
 
+export 'src/utils/change_notifiers.dart';
 export 'src/utils/pointer_events_listener.dart';
