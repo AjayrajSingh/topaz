b9cefa2a35d794af3651498d3d4e499bdb9c946c
Joshua Hsiao <jphsiao@google.com>
2018-07-25 09:13:47 -0700

    [mondrian-test] Adds mondrian test mod
    This simple mod that just shows a random color will be used
    to gather memgraph traces with a consistent test case.
    
    TEST=manual
    
    Change-Id: I6c25aa6c54a4c729a0bc83e81c3cec70e6e90ccf

---
+++ examples/mondrian_test/lib/main.dart
@@ -0,0 +1,23 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+import 'package:lib.app.dart/logging.dart';
+
+void main() {
+  setupLogger(name: 'mondrianTest');
+
+  Color randomColor =
+      new Color(0xFF000000 + new math.Random().nextInt(0xFFFFFF));
+
+  runApp(new MaterialApp(
+    title: 'Mondrian test',
+    home: new Container(
+      color: randomColor,
+    ),
+    theme: new ThemeData(canvasColor: randomColor),
+  ));
+}
