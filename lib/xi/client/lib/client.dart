c172517c1ce4315c5a3aa4a0a94a5cfa07f1023a
Colin Rothfels <crothfels@google.com>
2018-08-06 16:18:27 -0400

    [xi] Add interface and impl for core and view
    This separates the interface for the core from the interface for the
    view, and provides base impls for both. In subsequent work, there will
    be another impl for the view interface that supports agent-managed
    views.
    
    This CL is only adding these interfaces; it does not change the current
    code to use them.
    
    Progress on MS-1903
    Test: No functional change
    
    Change-Id: Ib628ce3fc1c36b1b5f50ed3c5db9ffed2efb5da9

--- lib/xi/client/lib/client.dart
+++ lib/xi/client/lib/client.dart
@@ -3,5 +3,9 @@
 // found in the LICENSE file.
 
 export 'src/client.dart';
+export 'src/core_impl.dart';
+export 'src/core_interface.dart';
 export 'src/handler_adapter.dart';
 export 'src/handler_interface.dart';
+export 'src/view_impl.dart';
+export 'src/view_interface.dart';
