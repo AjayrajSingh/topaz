5f9e4850d2167f6d3c277e9f3e5dce2ff8df317d
Rob Tsuk <robtsuk@google.com>
2018-08-10 16:07:32 -0700

    [ermine] Initial version of ermine
    Ermine is a Fuchsia User Shell intended to provide developers with a
    lovable environment for testing mods on Fuchsia.
    
    This initial version implements the same approach for view hosting,
    layout and control as //garnet/bin/developer/[tiles|tiles_ctl]. For
    simplicity it re-uses
    //garnet/public/fid/fuchsia.developer.tiles/tiles.fidl.
    
    This change contains three major parts:
    
    //topaz/bin/user_shell/ermine_user_shell
    
    This is the binary that is launched by device_runner and acts as a user
    shell. It draws a background color, takes care of launching new stories
    via the story provider and lays out the views that display each story.
    
    //topaz/bin/ermine_ctl
    
    This is a binary that knows how to ask ermine to add, remove and list
    the stories it is hosting via the fuchsia.developer.tiles interface.
    
    //topaz/packages
    
    This is a set of configuration changes needed to get ermine launched as
    a user shell.
    
    There are a few other changes supporting Rust development in Topaz.
    
    This change also deletes the previous program named ermine, which was
    not being used even before this change.
    
    Testing: built, booted, used ermine_ctl to add, remove and list stories.
    
    Change-Id: I2fd657188ca7acb3dd373aa97058b1aa3b1c0ba5

---
+++ bin/user_shell/ermine_user_shell/src/main.rs
@@ -0,0 +1,254 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use failure::{Error, ResultExt};
+use fidl::endpoints::{create_proxy, ClientEnd, RequestStream, ServerEnd, ServiceMarker};
+use fidl_fuchsia_developer_tiles as tiles;
+use fidl_fuchsia_math::SizeF;
+use fidl_fuchsia_modular::{StoryProviderProxy, UserShellContextMarker, UserShellContextProxy,
+                           UserShellMarker, UserShellRequest, UserShellRequestStream};
+use fidl_fuchsia_ui_input::{KeyboardEvent, KeyboardEventPhase, MODIFIER_LEFT_CONTROL,
+                            MODIFIER_RIGHT_CONTROL};
+use fidl_fuchsia_ui_policy::{KeyboardCaptureListenerHackMarker,
+                             KeyboardCaptureListenerHackRequest, PresentationProxy};
+use fidl_fuchsia_ui_viewsv1::{ViewManagerMarker, ViewManagerProxy, ViewProviderMarker,
+                              ViewProviderRequest::CreateView, ViewProviderRequestStream};
+use fidl_fuchsia_ui_viewsv1token::ViewOwnerMarker;
+use fuchsia_app::{self as component, client::connect_to_service};
+use fuchsia_async as fasync;
+use fuchsia_zircon as zx;
+use futures::{future::ready as fready, TryFutureExt, TryStreamExt};
+use lazy_static::lazy_static;
+use parking_lot::Mutex;
+use std::sync::Arc;
+
+mod view;
+
+use crate::view::{ErmineView, ErmineViewPtr};
+
+pub struct App {
+    view_manager: ViewManagerProxy,
+    views: Vec<ErmineViewPtr>,
+    user_shell_context: Option<UserShellContextProxy>,
+    story_provider: Option<StoryProviderProxy>,
+    presentation_proxy: Option<PresentationProxy>,
+    next_key: u32,
+}
+
+pub type AppPtr = Arc<Mutex<App>>;
+
+lazy_static! {
+    pub static ref APP: AppPtr = App::new().expect("Unable to create ermine app");
+}
+
+impl App {
+    pub fn new() -> Result<AppPtr, Error> {
+        let view_manager = connect_to_service::<ViewManagerMarker>()?;
+        Ok(Arc::new(Mutex::new(App {
+            view_manager,
+            views: vec![],
+            user_shell_context: None,
+            story_provider: None,
+            presentation_proxy: None,
+            next_key: 1,
+        })))
+    }
+
+    pub fn spawn_view_provider_server(chan: fasync::Channel) {
+        fasync::spawn(
+            ViewProviderRequestStream::from_channel(chan)
+                .try_for_each(move |req| {
+                    let CreateView { view_owner, .. } = req;
+                    APP.lock()
+                        .create_view(view_owner)
+                        .expect("Create view failed");
+                    futures::future::ready(Ok(()))
+                }).unwrap_or_else(|e| eprintln!("error running view_provider server: {:?}", e)),
+        )
+    }
+
+    pub fn create_view(&mut self, req: ServerEnd<ViewOwnerMarker>) -> Result<(), Error> {
+        let (view, view_server_end) = create_proxy()?;
+        let (view_listener, view_listener_server) = zx::Channel::create()?;
+        let view_listener_request = ServerEnd::new(view_listener_server);
+        let (mine, theirs) = zx::EventPair::create()?;
+        self.view_manager.create_view(
+            view_server_end,
+            req,
+            ClientEnd::new(view_listener),
+            theirs,
+            None,
+        )?;
+        let (scenic, scenic_request) = create_proxy()?;
+        self.view_manager.get_scenic(scenic_request)?;
+        let view_ptr = ErmineView::new(view_listener_request, view, mine, scenic)?;
+        self.views.push(view_ptr);
+        Ok(())
+    }
+
+    pub fn setup_keyboard_hack(&mut self) -> Result<(), Error> {
+        let (presentation_proxy, presentation_request) = create_proxy()?;
+        self.user_shell_context
+            .clone()
+            .unwrap()
+            .get_presentation(presentation_request)?;
+        self.presentation_proxy = Some(presentation_proxy);
+
+        let mut hotkey_event = KeyboardEvent {
+            event_time: 0,
+            device_id: 0,
+            phase: KeyboardEventPhase::Released,
+            hid_usage: 0,
+            code_point: 0x67,
+            modifiers: MODIFIER_LEFT_CONTROL | MODIFIER_RIGHT_CONTROL,
+        };
+        let (event_listener_client, event_listener_server) = zx::Channel::create()?;
+        let event_listener = ClientEnd::new(event_listener_client);
+        let event_listener_request =
+            ServerEnd::<KeyboardCaptureListenerHackMarker>::new(event_listener_server);
+
+        self.presentation_proxy
+            .clone()
+            .unwrap()
+            .capture_keyboard_event_hack(&mut hotkey_event, event_listener)?;
+
+        fasync::spawn(
+            event_listener_request
+                .into_stream()
+                .unwrap()
+                .try_for_each(move |event| match event {
+                    KeyboardCaptureListenerHackRequest::OnEvent { .. } => {
+                        println!("ermine: hotkey support goes here");
+                        futures::future::ready(Ok(()))
+                    }
+                }).unwrap_or_else(|e| eprintln!("keyboard hack listener error: {:?}", e)),
+        );
+
+        Ok(())
+    }
+
+    pub fn take_user_shell_context(
+        &mut self, user_shell_context: ClientEnd<UserShellContextMarker>,
+    ) -> Result<(), Error> {
+        self.user_shell_context = Some(user_shell_context.into_proxy().unwrap());
+        let (story_provider, story_provider_end) = create_proxy()?;
+        self.user_shell_context
+            .clone()
+            .unwrap()
+            .get_story_provider(story_provider_end)?;
+
+        self.story_provider = Some(story_provider);
+        self.setup_keyboard_hack()?;
+
+        Ok(())
+    }
+
+    pub fn spawn_user_shell_server(chan: fasync::Channel) {
+        fasync::spawn(
+            UserShellRequestStream::from_channel(chan)
+                .try_for_each(move |req| {
+                    let UserShellRequest::Initialize {
+                        user_shell_context, ..
+                    } = req;
+                    APP.lock()
+                        .take_user_shell_context(user_shell_context)
+                        .unwrap();
+                    futures::future::ready(Ok(()))
+                }).unwrap_or_else(|e| eprintln!("error running user shell server: {:?}", e)),
+        )
+    }
+
+    fn next_story_key(&mut self) -> u32 {
+        let next_key = self.next_key;
+        self.next_key += 1;
+        next_key
+    }
+
+    pub fn setup_story(
+        &mut self, key: u32, story_id: &str, module_name: String, allow_focus: bool,
+    ) -> Result<(), Error> {
+        self.views[0].lock().setup_story(
+            key,
+            story_id,
+            module_name,
+            allow_focus,
+            self.story_provider.as_ref().unwrap(),
+        )?;
+
+        Ok(())
+    }
+
+    pub fn add_story(&mut self, module_name: String, allow_focus: bool) -> u32 {
+        let key_to_use = self.next_story_key();
+        let f = self.story_provider.as_ref().unwrap().create_story(None);
+        fasync::spawn(
+            f.map_ok(move |r| {
+                APP.lock()
+                    .setup_story(key_to_use, &r, module_name, allow_focus)
+                    .unwrap();
+            }).unwrap_or_else(|e| eprintln!("create_story error: {:?}", e)),
+        );
+        key_to_use
+    }
+
+    pub fn remove_story(&mut self, key: u32) {
+        self.views[0].lock().remove_story(key);
+    }
+
+    pub fn list_stories(&self) -> (Vec<u32>, Vec<String>, Vec<SizeF>, Vec<bool>) {
+        self.views[0].lock().list_stories()
+    }
+
+    pub fn spawn_tiles_server(chan: fasync::Channel) {
+        fasync::spawn(
+            tiles::ControllerRequestStream::from_channel(chan)
+                .try_for_each(move |req| match req {
+                    tiles::ControllerRequest::AddTileFromUrl {
+                        url,
+                        allow_focus,
+                        responder,
+                        ..
+                    } => {
+                        let key = APP.lock().add_story(url, allow_focus);
+                        fready(responder.send(key))
+                    }
+                    tiles::ControllerRequest::AddTileFromViewProvider { responder, .. } => {
+                        fready(responder.send(0))
+                    }
+                    tiles::ControllerRequest::RemoveTile { key, .. } => {
+                        APP.lock().remove_story(key);
+                        fready(Ok(()))
+                    }
+                    tiles::ControllerRequest::ListTiles { responder } => {
+                        let (mut keys, mut urls, mut sizes, mut focusabilties) =
+                            APP.lock().list_stories();
+                        fready(responder.send(
+                            &mut keys.iter_mut().map(|a| *a),
+                            &mut urls.iter_mut().map(|a| &**a),
+                            &mut sizes.iter_mut(),
+                            &mut focusabilties.iter_mut().map(|a| *a),
+                        ))
+                    }
+                }).unwrap_or_else(|e| eprintln!("error running Tiles controller server: {:?}", e)),
+        )
+    }
+}
+
+fn main() -> Result<(), Error> {
+    let mut executor = fasync::Executor::new().context("Error creating executor")?;
+
+    let fut = component::server::ServicesServer::new()
+        .add_service((UserShellMarker::NAME, move |channel| {
+            App::spawn_user_shell_server(channel);
+        })).add_service((ViewProviderMarker::NAME, move |channel| {
+            App::spawn_view_provider_server(channel);
+        })).add_service((tiles::ControllerMarker::NAME, move |chan| {
+            App::spawn_tiles_server(chan)
+        })).start()
+        .context("Error starting services server")?;
+
+    executor.run_singlethreaded(fut)?;
+
+    Ok(())
+}
---
+++ bin/user_shell/ermine_user_shell/src/view.rs
@@ -0,0 +1,426 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use failure::Error;
+use fidl::encoding::OutOfLine;
+use fidl::endpoints::{create_proxy, ClientEnd, ServerEnd, ServiceMarker};
+use fidl_fuchsia_math::{InsetF, RectF, SizeF};
+use fidl_fuchsia_modular::{Intent, StoryProviderProxy};
+use fidl_fuchsia_ui_gfx::{self as gfx, ColorRgba};
+use fidl_fuchsia_ui_input::{InputConnectionMarker, InputConnectionProxy, InputListenerMarker,
+                            InputListenerRequest};
+use fidl_fuchsia_ui_scenic::{SessionListenerMarker, SessionListenerRequest};
+use fidl_fuchsia_ui_viewsv1::{CustomFocusBehavior, ViewContainerListenerMarker,
+                              ViewContainerListenerRequest, ViewLayout, ViewListenerMarker,
+                              ViewListenerRequest, ViewProperties};
+use fuchsia_async as fasync;
+use fuchsia_scenic::{EntityNode, ImportNode, Material, Rectangle, Session, SessionPtr, ShapeNode};
+use fuchsia_zircon::{self as zx, Channel};
+use futures::{future::ready as fready, TryFutureExt, TryStreamExt};
+use itertools::Itertools;
+use parking_lot::Mutex;
+use std::collections::BTreeMap;
+use std::sync::Arc;
+
+struct ViewData {
+    key: u32,
+    url: String,
+    // not currently used, as the Tiles FIDL interface doesn't have
+    // any way to ruturn it but I hope to expose it via ermine_ctl
+    // soon.
+    _story_id: String,
+    allow_focus: bool,
+    bounds: Option<RectF>,
+    host_node: EntityNode,
+}
+
+impl ViewData {
+    pub fn new(
+        key: u32, url: String, story_id: String, allow_focus: bool, host_node: EntityNode,
+    ) -> ViewData {
+        ViewData {
+            key: key,
+            url: url,
+            _story_id: story_id,
+            bounds: None,
+            allow_focus: allow_focus,
+            host_node: host_node,
+        }
+    }
+}
+
+pub struct ErmineView {
+    // Must keep the view proxy alive or the view goes away.
+    _view: fidl_fuchsia_ui_viewsv1::ViewProxy,
+    view_container: fidl_fuchsia_ui_viewsv1::ViewContainerProxy,
+    input_connection_proxy: InputConnectionProxy,
+    session: SessionPtr,
+    import_node: ImportNode,
+    background_node: ShapeNode,
+    container_node: EntityNode,
+    views: BTreeMap<u32, ViewData>,
+    width: f32,
+    height: f32,
+}
+
+pub type ErmineViewPtr = Arc<Mutex<ErmineView>>;
+
+impl ErmineView {
+    pub fn new(
+        view_listener_request: ServerEnd<ViewListenerMarker>,
+        view: fidl_fuchsia_ui_viewsv1::ViewProxy, mine: zx::EventPair,
+        scenic: fidl_fuchsia_ui_scenic::ScenicProxy,
+    ) -> Result<ErmineViewPtr, Error> {
+        let (session_listener_client, session_listener_server) = zx::Channel::create()?;
+        let session_listener = ClientEnd::new(session_listener_client);
+
+        let (session_proxy, session_request) = create_proxy()?;
+        scenic.create_session(session_request, Some(session_listener))?;
+        let session = Session::new(session_proxy);
+
+        let (view_container_proxy, view_container_request) = create_proxy()?;
+
+        view.get_container(view_container_request)?;
+
+        let (service_provider_proxy, service_provider_req) = create_proxy()?;
+
+        view.get_service_provider(service_provider_req)?;
+        let (input_connection_proxy, input_connection_request) =
+            create_proxy::<InputConnectionMarker>()?;
+        service_provider_proxy.connect_to_service(
+            &InputConnectionMarker::NAME,
+            input_connection_request.into_channel(),
+        )?;
+
+        let view_controller = ErmineView {
+            _view: view,
+            view_container: view_container_proxy,
+            input_connection_proxy: input_connection_proxy,
+            session: session.clone(),
+            import_node: ImportNode::new(session.clone(), mine),
+            background_node: ShapeNode::new(session.clone()),
+            container_node: EntityNode::new(session.clone()),
+            views: BTreeMap::new(),
+            width: 0.0,
+            height: 0.0,
+        };
+
+        let view_controller = Arc::new(Mutex::new(view_controller));
+
+        Self::setup_session_listener(&view_controller, session_listener_server);
+        Self::setup_view_listener(&view_controller, view_listener_request);
+        Self::setup_view_container_listener(&view_controller)?;
+        Self::setup_view_input_listener(&view_controller)?;
+        Self::finish_setup_scene(&view_controller);
+
+        Ok(view_controller)
+    }
+
+    fn setup_session_listener(
+        view_controller: &ErmineViewPtr, session_listener_server: zx::Channel,
+    ) {
+        let session_listener_request =
+            ServerEnd::<SessionListenerMarker>::new(session_listener_server);
+        let view_controller = view_controller.clone();
+        fasync::spawn(
+            session_listener_request
+                .into_stream()
+                .unwrap()
+                .map_ok(move |request| match request {
+                    SessionListenerRequest::OnScenicEvent { events, .. } => {
+                        view_controller.lock().handle_session_events(events)
+                    }
+                    _ => (),
+                }).try_collect::<()>()
+                .unwrap_or_else(|e| eprintln!("session listener error: {:?}", e)),
+        );
+    }
+
+    fn setup_view_listener(
+        view_controller: &ErmineViewPtr, view_listener_request: ServerEnd<ViewListenerMarker>,
+    ) {
+        let view_controller = view_controller.clone();
+        fasync::spawn(
+            view_listener_request
+                .into_stream()
+                .unwrap()
+                .try_for_each(
+                    move |ViewListenerRequest::OnPropertiesChanged {
+                              properties,
+                              responder,
+                          }| {
+                        view_controller.lock().handle_properies_changed(&properties);
+                        fready(responder.send())
+                    },
+                ).unwrap_or_else(|e| eprintln!("view listener error: {:?}", e)),
+        );
+    }
+
+    fn setup_view_container_listener(view_controller: &ErmineViewPtr) -> Result<(), Error> {
+        let view_controller = view_controller.clone();
+        let (view_container_listener_client, view_container_listener_server) =
+            zx::Channel::create()?;
+        let view_container_listener = ClientEnd::new(view_container_listener_client);
+        let view_container_listener_request =
+            ServerEnd::<ViewContainerListenerMarker>::new(view_container_listener_server);
+
+        view_controller
+            .lock()
+            .view_container
+            .set_listener(Some(view_container_listener))?;
+
+        fasync::spawn(
+            view_container_listener_request
+                .into_stream()
+                .unwrap()
+                .try_for_each(move |event| match event {
+                    ViewContainerListenerRequest::OnChildAttached { responder, .. } => {
+                        view_controller.lock().update();
+                        fready(responder.send())
+                    }
+                    ViewContainerListenerRequest::OnChildUnavailable {
+                        responder,
+                        child_key,
+                    } => {
+                        view_controller.lock().remove_story(child_key);
+                        fready(responder.send())
+                    }
+                }).unwrap_or_else(|e| eprintln!("view listener error: {:?}", e)),
+        );
+
+        Ok(())
+    }
+
+    // Currently does nothing but will be hooked up in the future.
+    fn setup_view_input_listener(view_controller: &ErmineViewPtr) -> Result<(), Error> {
+        let view_controller = view_controller.lock();
+        let (event_listener_client, event_listener_server) = zx::Channel::create()?;
+
+        let event_listener = ClientEnd::new(event_listener_client);
+        let event_listener_request = ServerEnd::<InputListenerMarker>::new(event_listener_server);
+
+        view_controller
+            .input_connection_proxy
+            .set_event_listener(Some(event_listener))?;
+
+        fasync::spawn(
+            event_listener_request
+                .into_stream()
+                .unwrap()
+                .try_for_each(move |event| match event {
+                    InputListenerRequest::OnEvent { responder, .. } => fready(responder.send(true)),
+                }).unwrap_or_else(|e| eprintln!("view listener error: {:?}", e)),
+        );
+
+        Ok(())
+    }
+
+    fn finish_setup_scene(view_controller: &ErmineViewPtr) {
+        let vc = view_controller.lock();
+        vc.setup_scene();
+        vc.present();
+    }
+
+    fn setup_scene(&self) {
+        self.import_node
+            .resource()
+            .set_event_mask(gfx::METRICS_EVENT_MASK);
+        self.import_node.add_child(&self.background_node);
+        self.import_node.add_child(&self.container_node);
+        let material = Material::new(self.session.clone());
+        material.set_color(ColorRgba {
+            red: 0x40,
+            green: 0x40,
+            blue: 0x40,
+            alpha: 0x80,
+        });
+        self.background_node.set_material(&material);
+    }
+
+    fn update(&mut self) {
+        let center_x = self.width * 0.5;
+        let center_y = self.height * 0.5;
+        self.background_node.set_shape(&Rectangle::new(
+            self.session.clone(),
+            self.width,
+            self.height,
+        ));
+        self.background_node
+            .set_translation(center_x, center_y, 0.0);
+        self.present();
+    }
+
+    fn present(&self) {
+        fasync::spawn(
+            self.session
+                .lock()
+                .present(0)
+                .map_ok(|_| ())
+                .unwrap_or_else(|e| eprintln!("present error: {:?}", e)),
+        );
+    }
+
+    fn handle_session_events(&mut self, events: Vec<fidl_fuchsia_ui_scenic::Event>) {
+        events.iter().for_each(|event| match event {
+            fidl_fuchsia_ui_scenic::Event::Gfx(gfx::Event::Metrics(_event)) => {
+                self.update();
+            }
+            _ => (),
+        });
+    }
+
+    fn handle_properies_changed(&mut self, properties: &fidl_fuchsia_ui_viewsv1::ViewProperties) {
+        if let Some(ref view_properties) = properties.view_layout {
+            self.width = view_properties.size.width;
+            self.height = view_properties.size.height;
+            self.update();
+        }
+    }
+
+    fn add_child_view_for_story(
+        &mut self, key: u32, url: String, story_id: String, allow_focus: bool, view_owner: Channel,
+    ) {
+        let host_node = EntityNode::new(self.session.clone());
+        let host_import_token = host_node.export_as_request();
+
+        self.view_container
+            .add_child(key, ClientEnd::new(view_owner), host_import_token)
+            .unwrap();
+
+        self.import_node.add_child(&host_node);
+        let view_data = ViewData::new(key, url, story_id, allow_focus, host_node);
+        self.views.insert(key, view_data);
+        self.update();
+        self.layout();
+    }
+
+    pub fn setup_story(
+        &mut self, key: u32, story_id: &str, module_name: String, allow_focus: bool,
+        story_provider: &StoryProviderProxy,
+    ) -> Result<(), Error> {
+        let (story_controller, story_controller_end) = create_proxy()?;
+        story_provider.get_controller(story_id, story_controller_end)?;
+        let (view_owner_client, view_owner_server) = Channel::create()?;
+        story_controller.start(ServerEnd::new(view_owner_client))?;
+
+        let mut intent = Intent {
+            action: Some("view".to_string()),
+            handler: Some(module_name.clone()),
+            parameters: None,
+        };
+
+        story_controller.add_module(None, "root", &mut intent, None)?;
+
+        self.add_child_view_for_story(
+            key,
+            module_name,
+            story_id.to_string(),
+            allow_focus,
+            view_owner_server,
+        );
+
+        Ok(())
+    }
+
+    pub fn remove_story(&mut self, key: u32) {
+        if self.views.remove(&key).is_some() {
+            self.view_container
+                .remove_child(key, None)
+                .unwrap_or_else(|e| {
+                    eprintln!(
+                        "view_container.remove_child failed for key {} with {}",
+                        key, e
+                    );
+                });
+            self.layout();
+            self.update();
+        }
+    }
+
+    pub fn list_stories(&self) -> (Vec<u32>, Vec<String>, Vec<SizeF>, Vec<bool>) {
+        let mut keys = Vec::new();
+        let mut urls = Vec::new();
+        let mut sizes = Vec::new();
+        let mut fs = Vec::new();
+        for (key, view) in &self.views {
+            let bounds = view.bounds.as_ref().unwrap_or(&RectF {
+                x: 0.0,
+                y: 0.0,
+                width: 0.0,
+                height: 0.0,
+            });
+            keys.push(*key);
+            urls.push(view.url.clone());
+            sizes.push(SizeF {
+                width: bounds.width,
+                height: bounds.height,
+            });
+            fs.push(true);
+        }
+        (keys, urls, sizes, fs)
+    }
+
+    fn inset(rect: &mut RectF, border: f32) {
+        let inset = border.min(rect.width / 0.3).min(rect.height / 0.3);
+        rect.x += inset;
+        rect.y += inset;
+        let inset_width = inset * 2.0;
+        rect.width = rect.width - inset_width;
+        rect.height = rect.height - inset_width;
+    }
+
+    pub fn layout(&mut self) {
+        if self.views.is_empty() {
+            return;
+        }
+
+        let num_tiles = self.views.len();
+
+        let columns = (num_tiles as f32).sqrt().ceil() as usize;
+        let rows = (columns + num_tiles - 1) / columns;
+        let tile_height = (self.height / rows as f32).floor();
+
+        for (row_index, view_chunk) in itertools::enumerate(&self.views.iter_mut().chunks(columns))
+        {
+            let tiles_in_row = if row_index == rows - 1 && (num_tiles % columns) != 0 {
+                num_tiles % columns
+            } else {
+                columns
+            };
+            let tile_width = (self.width / tiles_in_row as f32).floor();
+            for (column_index, (_key, view)) in view_chunk.enumerate() {
+                let mut tile_bounds = RectF {
+                    height: tile_height,
+                    width: tile_width,
+                    x: column_index as f32 * tile_width,
+                    y: row_index as f32 * tile_height,
+                };
+                Self::inset(&mut tile_bounds, 10.0);
+                let mut view_properties = ViewProperties {
+                    custom_focus_behavior: Some(Box::new(CustomFocusBehavior {
+                        allow_focus: view.allow_focus,
+                    })),
+                    view_layout: Some(Box::new(ViewLayout {
+                        inset: InsetF {
+                            bottom: 0.0,
+                            left: 0.0,
+                            right: 0.0,
+                            top: 0.0,
+                        },
+                        size: SizeF {
+                            width: tile_bounds.width,
+                            height: tile_bounds.height,
+                        },
+                    })),
+                };
+                self.view_container
+                    .set_child_properties(view.key, Some(OutOfLine(&mut view_properties)))
+                    .unwrap();
+                view.host_node
+                    .set_translation(tile_bounds.x, tile_bounds.y, 0.0);
+                view.bounds = Some(tile_bounds);
+            }
+        }
+    }
+}
