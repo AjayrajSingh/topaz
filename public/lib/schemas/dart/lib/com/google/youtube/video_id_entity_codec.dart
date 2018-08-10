d9cd73ee705a83fd76082468b197e0f0a5b6ddeb
Ross Wang <rosswang@google.com>
2018-08-09 18:20:45 -0700

    [youtube] Using passthrough codec for video IDs
    There's no need to do any transformation on video IDs at this time.
    
    MI4-1233 #comment
    Test: video request (fails due to video codec error)
    Change-Id: I495c86209a375ef7c25a55e3f76bae363fb96e09
    
    Change-Id: Ieb7a390d024dc0e4b28dde2ed4bf3427fbb0168a

--- public/lib/schemas/dart/lib/com/google/youtube/video_id_entity_codec.dart
+++ public/lib/schemas/dart/lib/com/google/youtube/video_id_entity_codec.dart
@@ -2,16 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:convert' show json;
-
-import 'package:lib.app.dart/logging.dart';
 import 'package:lib.schemas.dart/entity_codec.dart';
 
 const String _kYoutubeVideoIdEntityUri = 'com.fuchsia.youtube.videoid';
-
-// All YouTube video IDs are 11 characters in length.
-// This is also enforced in the YouTube code.
-const int _kYoutubeVideoIdLength = 11;
 
 /// Convert a request to set the videoID to a form passable over a Link between
 /// modules.
@@ -20,32 +13,7 @@ class VideoIdEntityCodec extends EntityCodec<String> {
   VideoIdEntityCodec()
       : super(
           type: _kYoutubeVideoIdEntityUri,
-          encode: _toJson,
-          decode: _fromJson,
+          encode: (x) => x,
+          decode: (x) => x,
         );
-
-  static String _toJson(String videoId) {
-    log.fine('Encode set video Id request to json: $videoId');
-    if (videoId == null || videoId.length != _kYoutubeVideoIdLength) {
-      log.warning('Invalid videoId encoding; returning null.');
-      return 'null';
-    }
-    return json.encode(videoId);
-  }
-
-  static String _fromJson(String encoded) {
-    log.fine('Decode request to set video ID: $encoded');
-    if (encoded == null || encoded.isEmpty || encoded == 'null') {
-      return null;
-    }
-    Object decoded = json.decode(encoded);
-    if (decoded == null || decoded is! String) {
-      return null;
-    }
-    String videoId = decoded;
-    if (videoId.length != _kYoutubeVideoIdLength) {
-      return null;
-    }
-    return videoId;
-  }
 }
