edc8dc701876b1dd553e4b70b144b704cbecbd38
Chase Latta <chaselatta@google.com>
2018-08-13 12:53:04 -0700

    [dart-sdk] stub out fuchsia_driver package
    TEST=
    - fx set x64 --packages topaz/packages/buildbot
    - fx build
    
    Change-Id: Ied4b23a57bcade08aca18adb8b2b0057692e8521

---
+++ public/dart/fuchsia_driver/lib/fuchsia_driver.dart
@@ -0,0 +1,6 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// Fuchsia’s integration test driver built on top of the Flutter Driver.
+library fuchsia_driver;
