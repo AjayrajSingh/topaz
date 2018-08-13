afb00d067c33fff62a95086573d0f378cd1ef2ca
Chase Latta <chaselatta@google.com>
2018-08-13 11:15:26 -0700

    [dart-sdk] Stub out fuchsia package
    TEST=None
    
    Change-Id: I46b483123204b87c4fa04ae34d8f2752201f28ad

--- public/dart/fuchsia/lib/fuchsia.dart
+++ public/dart/fuchsia/lib/fuchsia.dart
@@ -2,11 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/// Fakes exported for the fuchsia system.
 library fuchsia;
 
 // ignore: unused_import
-import 'src/fuchsia_fakes.dart'
-  if (dart.library.fuchsia) 'dart:fuchsia';
+import 'src/fuchsia_fakes.dart' if (dart.library.fuchsia) 'dart:fuchsia';
 
-export 'src/fuchsia_fakes.dart'
-  if (dart.library.fuchsia) 'dart:fuchsia';
+export 'src/fuchsia_fakes.dart' if (dart.library.fuchsia) 'dart:fuchsia';
