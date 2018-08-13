e65d40fe8ab27153395af9113f18fb4553015886
Chase Latta <chaselatta@google.com>
2018-08-13 13:11:55 -0700

    [sledge] add apidoc line
    TEST=
    - fx set x64 --packages topaz/packages/buildbot
    - fx build
    
    Change-Id: I2d35c05447fd70a1ed48ad8b208f99d51cd2bfd5

--- public/dart/sledge/lib/sledge.dart
+++ public/dart/sledge/lib/sledge.dart
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/// Sledge is a utility which aims to simplify interacting with Ledger.
+library sledge;
+
 export 'src/document/document.dart';
 export 'src/document/document_id.dart';
 export 'src/schema/base_type.dart';
