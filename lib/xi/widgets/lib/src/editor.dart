d5423c2e7023104924fc02a2b0c19ebdf601304c
Colin Rothfels <crothfels@google.com>
2018-08-14 15:16:02 -0400

    [xi] Refactor keyboard event helpers
    Just cleanup. Having all the key handling logic in editor.dart seems like
    unnecessary noise.
    
    Test: tested manually
    Change-Id: I0db6c0b844be968aa7b4e4384dfc61034cee6ed6

--- lib/xi/widgets/lib/src/editor.dart
+++ lib/xi/widgets/lib/src/editor.dart
@@ -12,6 +12,7 @@ import 'package:flutter/services.dart';
 import 'package:lib.app.dart/logging.dart';
 import 'package:xi_client/client.dart';
 
+import 'key_info.dart';
 import 'line_cache.dart';
 import 'text_line.dart';
 import 'xi_app.dart';
@@ -27,159 +28,6 @@ class Editor extends StatefulWidget {
   @override
   EditorState createState() => new EditorState();
 }
-
-// Generated from data scraped from
-// https://source.android.com/devices/input/keyboard-devices.html
-Map<int, int> _androidToHid = <int, int>{
-  0x001d: 0x0004,
-  0x001e: 0x0005,
-  0x001f: 0x0006,
-  0x0020: 0x0007,
-  0x0021: 0x0008,
-  0x0022: 0x0009,
-  0x0023: 0x000a,
-  0x0024: 0x000b,
-  0x0025: 0x000c,
-  0x0026: 0x000d,
-  0x0027: 0x000e,
-  0x0028: 0x000f,
-  0x0029: 0x0010,
-  0x002a: 0x0011,
-  0x002b: 0x0012,
-  0x002c: 0x0013,
-  0x002d: 0x0014,
-  0x002e: 0x0015,
-  0x002f: 0x0016,
-  0x0030: 0x0017,
-  0x0031: 0x0018,
-  0x0032: 0x0019,
-  0x0033: 0x001a,
-  0x0034: 0x001b,
-  0x0035: 0x001c,
-  0x0036: 0x001d,
-  0x0008: 0x001e,
-  0x0009: 0x001f,
-  0x000a: 0x0020,
-  0x000b: 0x0021,
-  0x000c: 0x0022,
-  0x000d: 0x0023,
-  0x000e: 0x0024,
-  0x000f: 0x0025,
-  0x0010: 0x0026,
-  0x0007: 0x0027,
-  0x0042: 0x0028,
-  0x006f: 0x0029,
-  0x0043: 0x002a,
-  0x003d: 0x002b,
-  0x003e: 0x002c,
-  0x0045: 0x002d,
-  0x0046: 0x002e,
-  0x0047: 0x002f,
-  0x0048: 0x0030,
-  0x0049: 0x0031,
-  0x004a: 0x0033,
-  0x004b: 0x0034,
-  0x0044: 0x0035,
-  0x0037: 0x0036,
-  0x0038: 0x0037,
-  0x004c: 0x0038,
-  0x0073: 0x0039,
-  0x0083: 0x003a,
-  0x0084: 0x003b,
-  0x0085: 0x003c,
-  0x0086: 0x003d,
-  0x0087: 0x003e,
-  0x0088: 0x003f,
-  0x0089: 0x0040,
-  0x008a: 0x0041,
-  0x008b: 0x0042,
-  0x008c: 0x0043,
-  0x008d: 0x0044,
-  0x008e: 0x0045,
-  0x0078: 0x0046,
-  0x0074: 0x0047,
-  0x0079: 0x0048,
-  0x007c: 0x0049,
-  0x007a: 0x004a,
-  0x005c: 0x004b,
-  0x0070: 0x004c,
-  0x007b: 0x004d,
-  0x005d: 0x004e,
-  0x0016: 0x004f,
-  0x0015: 0x0050,
-  0x0014: 0x0051,
-  0x0013: 0x0052,
-  0x008f: 0x0053,
-  0x009a: 0x0054,
-  0x009b: 0x0055,
-  0x009c: 0x0056,
-  0x009d: 0x0057,
-  0x00a0: 0x0058,
-  0x0091: 0x0059,
-  0x0092: 0x005a,
-  0x0093: 0x005b,
-  0x0094: 0x005c,
-  0x0095: 0x005d,
-  0x0096: 0x005e,
-  0x0097: 0x005f,
-  0x0098: 0x0060,
-  0x0099: 0x0061,
-  0x0090: 0x0062,
-  0x009e: 0x0063,
-  0x0052: 0x0065,
-  0x001a: 0x0066,
-  0x00a1: 0x0067,
-  0x0056: 0x0078,
-  0x00a4: 0x007f,
-  0x0018: 0x0080,
-  0x0019: 0x0081,
-  0x009f: 0x0085,
-  0x00a2: 0x00b6,
-  0x00a3: 0x00b7,
-  0x0071: 0x00e0,
-  0x003b: 0x00e1,
-  0x0039: 0x00e2,
-  0x0075: 0x00e3,
-  0x0072: 0x00e4,
-  0x003c: 0x00e5,
-  0x003a: 0x00e6,
-  0x0076: 0x00e7,
-  0x0055: 0x00e8,
-  0x0058: 0x00ea,
-  0x0057: 0x00eb,
-  0x0081: 0x00ec,
-  0x0040: 0x00f0,
-  0x0004: 0x00f1,
-  0x007d: 0x00f2
-};
-
-// Android KeyEvent metaState values, names are capital in original
-// ignore: constant_identifier_names
-const int _META_ALT_LEFT_ON = 0x10;
-// ignore: constant_identifier_names
-const int _META_ALT_RIGHT_ON = 0x20;
-// ignore: constant_identifier_names
-const int _META_SHIFT_LEFT_ON = 0x40;
-// ignore: constant_identifier_names
-const int _META_SHIFT_RIGHT_ON = 0x80;
-// ignore: constant_identifier_names
-const int _META_CTRL_LEFT_ON = 0x2000;
-// ignore: constant_identifier_names
-const int _META_CTRL_RIGHT_ON = 0x4000;
-
-// Fuchsia modifier values. See:
-// //garnet/public/fidl/fuchsia.ui.input/input_event_constants.fidl
-const int _modifierShiftLeft = 2;
-const int _modifierShiftRight = 4;
-const int _modifierShiftMask = 6;
-const int _modifierCtrlLeft = 8;
-const int _modifierCtrlRight = 0x10;
-const int _modifierCtrlMask = 0x18;
-const int _modifierAltLeft = 0x20;
-const int _modifierAltRight = 0x40;
-// ignore: unused_element
-const int _modifierAltMask = 0x60;
-const int _modifierAltCtrlMask = 0x78;
 
 /// A simple class representing a line and column location in the view.
 class LineCol {
@@ -273,64 +121,40 @@ class EditorState extends State<Editor> implements XiViewHandler {
     _xiAppState.sendNotification(method, params ?? <dynamic>[]);
   }
 
-  int _metaStateToModifiers(int metaState) {
-    int modifiers = 0;
-    if ((metaState & _META_CTRL_LEFT_ON) != 0) {
-      modifiers |= _modifierCtrlLeft;
-    }
-    if ((metaState & _META_CTRL_RIGHT_ON) != 0) {
-      modifiers |= _modifierCtrlRight;
-    }
-    if ((metaState & _META_SHIFT_LEFT_ON) != 0) {
-      modifiers |= _modifierShiftLeft;
-    }
-    if ((metaState & _META_SHIFT_RIGHT_ON) != 0) {
-      modifiers |= _modifierShiftRight;
-    }
-    if ((metaState & _META_ALT_LEFT_ON) != 0) {
-      modifiers |= _modifierAltLeft;
-    }
-    if ((metaState & _META_ALT_RIGHT_ON) != 0) {
-      modifiers |= _modifierAltRight;
-    }
-    return modifiers;
-  }
-
-  void _doMovement(String direction, int modifiers) {
-    bool modifySel = ((modifiers & _modifierShiftMask) != 0);
+  void _doMovement(String direction, Modifiers modifiers) {
     // Note: the "and_modify_selection" part will probably become a parameter to the
     // movement method.
-    String method = modifySel
+    String method = modifiers.shift
         ? 'move_${direction}_and_modify_selection'
         : 'move_$direction';
     _sendNotification(method);
   }
 
-  void _handleHidKey(int hidUsage, int modifiers) {
+  void _handleHidKey(int hidUsage, Modifiers modifiers) {
     if (hidUsage == 0x2A) {
       // Keyboard DELETE (Backspace)
       _sendNotification('delete_backward');
     } else if (hidUsage == 0x28) {
       // Keyboard Return (ENTER)
       _sendNotification('insert_newline');
-    } else if ((modifiers & _modifierCtrlMask) != 0 && hidUsage == 0x04) {
+    } else if (modifiers.ctrl && hidUsage == 0x04) {
       // Keyboard a
       _doMovement('to_beginning_of_paragraph', modifiers);
-    } else if ((modifiers & _modifierCtrlMask) != 0 && hidUsage == 0x08) {
+    } else if (modifiers.ctrl && hidUsage == 0x08) {
       // Keyboard e
       _doMovement('to_end_of_paragraph', modifiers);
-    } else if ((modifiers & _modifierCtrlMask) != 0 && hidUsage == 0x0E) {
+    } else if (modifiers.ctrl && hidUsage == 0x0E) {
       // Keyboard k
       _sendNotification('delete_to_end_of_paragraph');
-    } else if ((modifiers & _modifierCtrlMask) != 0 && hidUsage == 0x17) {
+    } else if (modifiers.ctrl && hidUsage == 0x17) {
       // Keyboard t
       _sendNotification('transpose');
-    } else if ((modifiers & _modifierCtrlMask) != 0 && hidUsage == 0x1C) {
+    } else if (modifiers.ctrl && hidUsage == 0x1C) {
       // Keyboard y
       _sendNotification('yank');
-    } else if ((modifiers & _modifierCtrlMask) != 0 && hidUsage == 0x1D) {
+    } else if (modifiers.ctrl && hidUsage == 0x1D) {
       // Keyboard z
-      if ((modifiers & _modifierShiftMask) != 0) {
+      if (modifiers.shift) {
         _sendNotification('redo');
       } else {
         _sendNotification('undo');
@@ -353,16 +177,16 @@ class EditorState extends State<Editor> implements XiViewHandler {
     } else if (hidUsage == 0x51) {
       // Keyboard DownArrow
       _doMovement('down', modifiers);
-    } else if ((modifiers & _modifierAltRight) != 0 && hidUsage == 0x04) {
+    } else if (modifiers.altRight && hidUsage == 0x04) {
       // altgr-a inserts emoji, to test unicode ability
       _sendNotification('insert', <String, dynamic>{'chars': '\u{1f601}'});
-    } else if ((modifiers & _modifierAltRight) != 0 && hidUsage == 0x0f) {
+    } else if (modifiers.altRight && hidUsage == 0x0f) {
       // altgr-l inserts arabic lam, to test bidi ability
       _sendNotification('insert', <String, dynamic>{'chars': '\u{0644}'});
     }
   }
 
-  void _handleCodePoint(int codePoint, int modifiers) {
+  void _handleCodePoint(int codePoint, Modifiers modifiers) {
     if (codePoint == 9) {
       _sendNotification('insert_tab');
     } else if (codePoint == 10) {
@@ -379,20 +203,23 @@ class EditorState extends State<Editor> implements XiViewHandler {
       if (data is RawKeyEventDataAndroid) {
         log.info(
             'codePoint=${data.codePoint}, metaState=${data.metaState}, keyCode=${data.keyCode}');
-        int modifiers = _metaStateToModifiers(data.metaState);
+        var modifiers = Modifiers.fromAndroid(data.metaState);
         if (data.codePoint != 0) {
           _handleCodePoint(data.codePoint, modifiers);
-        } else if (_androidToHid.containsKey(data.keyCode)) {
-          _handleHidKey(_androidToHid[data.keyCode], modifiers);
+        } else {
+          int _hidKey = keyCodeFromAndroid(data.keyCode);
+          if (_hidKey != null) {
+            _handleHidKey(_hidKey, modifiers);
+          }
         }
       } else if (data is RawKeyEventDataFuchsia) {
         log.info(
             'codePoint=${data.codePoint}, modifiers=${data.modifiers}, hidUsage=${data.hidUsage}');
-        if (data.codePoint != 0 &&
-            (data.modifiers & _modifierAltCtrlMask) == 0) {
-          _handleCodePoint(data.codePoint, data.modifiers);
+        var modifiers = Modifiers.fromFuchsia(data.modifiers);
+        if (data.codePoint != 0 && !modifiers.ctrl) {
+          _handleCodePoint(data.codePoint, modifiers);
         } else {
-          _handleHidKey(data.hidUsage, data.modifiers);
+          _handleHidKey(data.hidUsage, modifiers);
         }
       }
     }
