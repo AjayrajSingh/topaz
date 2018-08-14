5594fb73af19b05b4e55e20994bf0d37a52b28c5
Chase Latta <chaselatta@google.com>
2018-08-14 13:30:07 -0700

    [dart-sdk] stub out scoped model package
    TEST=
    - fx set x64 --packages topaz/packages/buildbot
    - fx build
    
    Change-Id: I73df7b9eeac159b91685bd9fbab352d86a26df53

---
+++ public/dart/scoped_model/lib/scoped_model.dart
@@ -0,0 +1,6 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// A package which helps authors manage state within the flutter widget tree.
+library scoped_model;
