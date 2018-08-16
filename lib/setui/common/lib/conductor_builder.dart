aecf7deff76ab1da6a9456fac9b2ec2805a90952
Bryce Lee <brycelee@google.com>
2018-08-16 14:34:15 -0700

    [SetUI] Add Conductor Builder.
    This changelist adds the ConductorBuilder, the recommended method for
    generating a Conductor. Details surrounding the Syllabus and Roster
    are hidden from the caller.
    
    Test: fx run-host-tests lib_setui_common_test
    Change-Id: Ia5ecd894ff4e8eac69e9d4135c3d40841c27529a

---
+++ lib/setui/common/lib/conductor_builder.dart
@@ -0,0 +1,38 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:yaml/yaml.dart';
+
+import 'action.dart';
+import 'conductor.dart';
+import 'syllabus.dart';
+import 'syllabus_parser.dart';
+
+/// A builder for assembling a [Conductor]. Abstracts away details surrounding
+/// the [Roster] and [Syllabus].
+class ConductorBuilder {
+  final Roster _roster = new Roster();
+  final File _syllabusFile;
+
+  /// Constructs a builder with a [Syllabus] at the specified [File].
+  ConductorBuilder(this._syllabusFile);
+
+  /// Adds a [Blueprint] to be used by the [Conductor].
+  void addBlueprint(Blueprint blueprint) => _roster.add(blueprint);
+
+  /// Builds the [Conductor].
+  Future<Conductor> build() {
+    Completer<Conductor> completer = new Completer<Conductor>();
+
+    _syllabusFile.readAsString().then((String config) {
+      final Syllabus syllabus = SyllabusParser.parse(loadYamlDocuments(config));
+      completer.complete(new Conductor(syllabus, _roster));
+    });
+
+    return completer.future;
+  }
+}
---
+++ lib/setui/common/test/conductor_builder_test.dart
@@ -0,0 +1,56 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:lib_setui_common/action.dart';
+import 'package:lib_setui_common/conductor_builder.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+/// A well-formed syllabus with single use id.
+const String _syllabus = '''
+---
+entry: start
+...
+---
+start:
+  action: begin
+...
+''';
+
+class MockFile extends Mock implements File {}
+
+class MockBlueprint extends Mock implements Blueprint {}
+
+class MockAction extends Mock implements Action {}
+
+void main() {
+  // Make sure first action is launched.
+  test('test_start', () async {
+    final MockFile syllabusFile = new MockFile();
+    final MockBlueprint blueprint = new MockBlueprint();
+    final MockAction action = new MockAction();
+
+    final Completer<String> completer = new Completer<String>()
+      ..complete(_syllabus);
+
+    // Load predefined syllabus when
+    when(syllabusFile.readAsString()).thenAnswer((_) => completer.future);
+    when(blueprint.key).thenReturn('begin');
+    when(blueprint.assemble(any, any)).thenReturn(action);
+
+    final ConductorBuilder builder = new ConductorBuilder(syllabusFile)
+      ..addBlueprint(blueprint);
+
+    (await builder.build()).start();
+
+    // Ensure blueprint is assembled
+    verify(blueprint.assemble(any, any));
+
+    // Ensure action is launched
+    verify(action.launch());
+  });
+}
