4c96730c8e3e6330d67b510740cc6df61bd5558e
Robert Lord <lard@google.com>
2018-08-21 12:48:35 -0700

    [latin-ime] Connect keys to IMEService's InjectInput
    Previously, the latin-ime would just display a keyboard, but pressing
    the keys would do nothing. This change connects to the new InjectInput
    method on IMEService added in Ie9a6a5d48249373b05387e344643d1d3d1dbc8d2
    to actually tell the IMEService to type keys.
    
    The InjectInput method is temporary, TEXT-17 tracks eventually removing
    it and this in favor of using OT instead to sync text field state with
    the keyboard.
    
    TEXT-16 #done
    
    Test: Manually ensured pressing keys worked properly.
    
    Change-Id: I47059e01220defa0c044aba7648026d7307e3b24

--- app/latin-ime/main.dart
+++ app/latin-ime/main.dart
@@ -2,19 +2,61 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:fidl_fuchsia_ui_input/fidl.dart';
 import 'package:flutter/material.dart';
 import 'package:keyboard/keyboard.dart';
+import 'package:lib.app.dart/app.dart';
+import 'package:lib.app.dart/logging.dart';
 
-class _App extends StatelessWidget {
+class ImeKeyboard extends StatelessWidget {
+
+  const ImeKeyboard({ImeServiceProxy imeService})
+    : _imeService = imeService,
+      super();
+
+  final ImeServiceProxy _imeService;
+
+  void _onText(String text) {
+    final kbEvent = KeyboardEvent(
+      phase: KeyboardEventPhase.pressed,
+      codePoint: text.codeUnitAt(0),
+      hidUsage: 0,
+      eventTime: 0,
+      modifiers: 0,
+      deviceId: 0,
+    );
+    var event = InputEvent.withKeyboard(kbEvent);
+    _imeService.injectInput(event);
+  }
+
+  void _onDelete() {
+    final kbEvent = KeyboardEvent(
+      phase: KeyboardEventPhase.pressed,
+      codePoint: 0,
+      hidUsage: 0x2a,
+      eventTime: 0,
+      modifiers: 0,
+      deviceId: 0,
+    );
+    var event = InputEvent.withKeyboard(kbEvent);
+    _imeService.injectInput(event);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Theme(
-      data: new ThemeData.light(),
-      child: const Keyboard(),
-    );
+    return Keyboard(onText: _onText, onDelete: _onDelete);
   }
 }
 
 void main() {
-  runApp(new _App());
+  final context = StartupContext.fromStartupInfo();
+  var imeService = ImeServiceProxy();
+  connectToService<ImeService>(context.environmentServices, imeService.ctrl);
+
+  setupLogger();
+
+  runApp(Theme(
+    data: ThemeData.light(),
+    child: ImeKeyboard(imeService: imeService),
+  ));
 }
