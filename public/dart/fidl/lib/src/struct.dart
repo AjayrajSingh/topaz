16d9780e071a419cec6ed37a9c75db6e48fe4259
Ian McKellar <ianloic@google.com>
2018-08-31 10:44:04 -0700

    [dart][fidl] hashCode and equality operators
    Implement hashCode and equality operators for FIDL structs, unions and
    enums, including those that contain Dart Lists (ie: FIDL arrays and
    vectors).
    
    Strings and primitives define their own hashCode and operator== that
    work just fine. InterfacePointer and InterfaceRequest inherit hashCode
    and operator== based on object identity which is appropriate for them.
    
    Added tests to fidl_bindings_test
    
    TEST=fx shell runtests /pkgfs/packages/fidl_bindings_test/0/test
    
    Change-Id: Iaf0b763a3a4d41538993d711bf86ffd0c85580ae

--- public/dart/fidl/lib/src/struct.dart
+++ public/dart/fidl/lib/src/struct.dart
@@ -4,10 +4,27 @@
 
 // ignore_for_file: public_member_api_docs
 
+import 'hash_codes.dart';
+
 abstract class Struct {
   const Struct();
 
   List<Object> get $fields;
+
+  @override
+  int get hashCode => deepHash($fields);
+
+  @override
+  bool operator ==(dynamic other) {
+    if (identical(this, other)) {
+      return true;
+    }
+    if (runtimeType != other.runtimeType) {
+      return false;
+    }
+    final Struct otherStruct = other;
+    return deepEquals($fields, otherStruct.$fields);
+  }
 }
 
 typedef StructFactory<T> = T Function(List<Object> argv);
--- public/dart/fidl/lib/src/union.dart
+++ public/dart/fidl/lib/src/union.dart
@@ -4,11 +4,35 @@
 
 // ignore_for_file: public_member_api_docs
 
+import 'hash_codes.dart';
+
 abstract class Union {
   const Union();
 
   int get $index;
   Object get $data;
+
+  @override
+  int get hashCode => deepHash([$index, $data]);
+
+  @override
+  bool operator ==(dynamic other) {
+    if (identical(this, other)) {
+      return true;
+    }
+    if (runtimeType != other.runtimeType) {
+      return false;
+    }
+    final Union otherUnion = other;
+    if ($index != otherUnion.$index) {
+      return false;
+    }
+    if ($data is Iterable) {
+      return deepEquals($data, otherUnion.$data);
+    } else {
+      return $data == otherUnion.$data;
+    }
+  }
 }
 
 typedef UnionFactory<T> = T Function(int index, Object data);
