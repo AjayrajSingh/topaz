dd3ea222dd69892940be590855284653702c26b7
Nathan Rogers <nathanrogers@google.com>
2018-09-10 15:20:10 -0700

    [scenic] Name run_scenic_benchmark.sh parameters
    This is a prerequisite for supporting the optional argument flutter app
    name.
    
    TEST=fx shell "/pkgfs/packages/topaz_benchmarks/0/bin/benchmarks.sh /tmp --catapult-converter-args --bots fake_bot_name --masters fuchsia.try --execution-timestamp-ms 1534438419097 --log-url https://fake_url"
    
    Change-Id: Ie3138c41429292dbd961ca2315a1ee43b24771fe

--- tests/benchmarks/topaz_gfx_benchmarks.sh
+++ tests/benchmarks/topaz_gfx_benchmarks.sh
@@ -14,141 +14,156 @@ RUN_SCENIC_BENCHMARK="/pkgfs/packages/scenic_benchmarks/0/bin/run_scenic_benchma
 # Arguments to runbench_exec for Scenic benchmarks:
 #
 # runbench_exec
-#   "${OUT_DIR}/${BENCHMARK}.json"    # Output file path.
-#   "${RUN_SCENIC_BENCHMARK}"         # Scenic benchmark runner, followed by
-#                                     #   its arguments.
-#   "${OUT_DIR}"                      # Output directory.
-#   "${OUT_DIR}/${BENCHMARK}.json"    # Output file path.
-#   "${BENCHMARK}"                    # Label for benchmark.
-#   "test_binary"                     # Binary that is being benchmarked.
-#   --unshadowed --clipping_disabled  # Arguments.
+#   "${OUT_DIR}/${BENCHMARK}.json"             # Output file path.
+#   "${RUN_SCENIC_BENCHMARK}"                  # Scenic benchmark runner, followed by
+#                                              #   its arguments.
+#   --out_dir "${OUT_DIR}"                     # Output directory.
+#   --out_file "${OUT_DIR}/${BENCHMARK}.json"  # Output file path.
+#   --benchmark_label "${BENCHMARK}"           # Label for benchmark.
+#   --cmd "test_binary"                        # Binary that is being benchmarked.
+#   --unshadowed --clipping_disabled           # Renderer parameters.
 
 
 #
 # image_grid_flutter
 #
 BENCHMARK="fuchsia.scenic.image_grid_flutter_noclipping_noshadows"
-runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-    "${RUN_SCENIC_BENCHMARK}"                \
-    "${OUT_DIR}"                             \
-    "${OUT_DIR}/${BENCHMARK}.json"           \
-    "${BENCHMARK}"                           \
-    "set_root_view image_grid_flutter" --unshadowed --clipping_disabled
+runbench_exec "${OUT_DIR}/${BENCHMARK}.json"  \
+    "${RUN_SCENIC_BENCHMARK}"                 \
+    --out_dir "${OUT_DIR}"                    \
+    --out_file "${OUT_DIR}/${BENCHMARK}.json" \
+    --benchmark_label "${BENCHMARK}"          \
+    --cmd "set_root_view image_grid_flutter"  \
+    --unshadowed --clipping_disabled
 
 BENCHMARK="fuchsia.scenic.image_grid_flutter_noshadows"
-runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-    "${RUN_SCENIC_BENCHMARK}"                \
-    "${OUT_DIR}"                             \
-    "${OUT_DIR}/${BENCHMARK}.json"           \
-    "${BENCHMARK}"                           \
-    "set_root_view image_grid_flutter" --unshadowed --clipping_enabled
+runbench_exec "${OUT_DIR}/${BENCHMARK}.json"  \
+    "${RUN_SCENIC_BENCHMARK}"                 \
+    --out_dir "${OUT_DIR}"                    \
+    --out_file "${OUT_DIR}/${BENCHMARK}.json" \
+    --benchmark_label "${BENCHMARK}"          \
+    --cmd "set_root_view image_grid_flutter"  \
+    --unshadowed --clipping_enabled
 
 BENCHMARK="fuchsia.scenic.image_grid_flutter_ssdo"
-runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-    "${RUN_SCENIC_BENCHMARK}"                \
-    "${OUT_DIR}"                             \
-    "${OUT_DIR}/${BENCHMARK}.json"           \
-    "${BENCHMARK}"                           \
-    "set_root_view image_grid_flutter" --screen_space_shadows --clipping_enabled
+runbench_exec "${OUT_DIR}/${BENCHMARK}.json"  \
+    "${RUN_SCENIC_BENCHMARK}"                 \
+    --out_dir "${OUT_DIR}"                    \
+    --out_file "${OUT_DIR}/${BENCHMARK}.json" \
+    --benchmark_label "${BENCHMARK}"          \
+    --cmd "set_root_view image_grid_flutter"  \
+    --screen_space_shadows --clipping_enabled
 
 BENCHMARK="fuchsia.scenic.image_grid_flutter_shadow_map"
-runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-    "${RUN_SCENIC_BENCHMARK}"                \
-    "${OUT_DIR}"                             \
-    "${OUT_DIR}/${BENCHMARK}.json"           \
-    "${BENCHMARK}"                           \
-    "set_root_view image_grid_flutter" --shadow_map --clipping_enabled
+runbench_exec "${OUT_DIR}/${BENCHMARK}.json"  \
+    "${RUN_SCENIC_BENCHMARK}"                 \
+    --out_dir "${OUT_DIR}"                    \
+    --out_file "${OUT_DIR}/${BENCHMARK}.json" \
+    --benchmark_label "${BENCHMARK}"          \
+    --cmd "set_root_view image_grid_flutter"  \
+    --shadow_map --clipping_enabled
 
 BENCHMARK="fuchsia.scenic.image_grid_flutter_moment_shadow_map"
-runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-    "${RUN_SCENIC_BENCHMARK}"                \
-    "${OUT_DIR}"                             \
-    "${OUT_DIR}/${BENCHMARK}.json"           \
-    "${BENCHMARK}"                           \
-    "set_root_view image_grid_flutter" --moment_shadow_map --clipping_enabled
+runbench_exec "${OUT_DIR}/${BENCHMARK}.json"  \
+    "${RUN_SCENIC_BENCHMARK}"                 \
+    --out_dir "${OUT_DIR}"                    \
+    --out_file "${OUT_DIR}/${BENCHMARK}.json" \
+    --benchmark_label "${BENCHMARK}"          \
+    --cmd "set_root_view image_grid_flutter"  \
+    --moment_shadow_map --clipping_enabled
 
 #
 # image_grid_flutter x3
 #
 IMAGE_GRID_FLUTTER_X3_COMMAND="set_root_view tile_view image_grid_flutter image_grid_flutter image_grid_flutter"
 BENCHMARK="fuchsia.scenic.image_grid_flutter_x3_noclipping_noshadows"
-runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-    "${RUN_SCENIC_BENCHMARK}"                \
-    "${OUT_DIR}"                             \
-    "${OUT_DIR}/${BENCHMARK}.json"           \
-    "${BENCHMARK}"                           \
-    "${IMAGE_GRID_FLUTTER_X3_COMMAND}" --unshadowed --clipping_disabled
+runbench_exec "${OUT_DIR}/${BENCHMARK}.json"  \
+    "${RUN_SCENIC_BENCHMARK}"                 \
+    --out_dir "${OUT_DIR}"                    \
+    --out_file "${OUT_DIR}/${BENCHMARK}.json" \
+    --benchmark_label "${BENCHMARK}"          \
+    --cmd "${IMAGE_GRID_FLUTTER_X3_COMMAND}"  \
+    --unshadowed --clipping_disabled
 
 BENCHMARK="fuchsia.scenic.image_grid_flutter_x3_noshadows"
-runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-    "${RUN_SCENIC_BENCHMARK}"                \
-    "${OUT_DIR}"                             \
-    "${OUT_DIR}/${BENCHMARK}.json"           \
-    "${BENCHMARK}"                           \
-    "${IMAGE_GRID_FLUTTER_X3_COMMAND}" --unshadowed --clipping_enabled
+runbench_exec "${OUT_DIR}/${BENCHMARK}.json"  \
+    "${RUN_SCENIC_BENCHMARK}"                 \
+    --out_dir "${OUT_DIR}"                    \
+    --out_file "${OUT_DIR}/${BENCHMARK}.json" \
+    --benchmark_label "${BENCHMARK}"          \
+    --cmd "${IMAGE_GRID_FLUTTER_X3_COMMAND}"  \
+    --unshadowed --clipping_enabled
 
 BENCHMARK="fuchsia.scenic.image_grid_flutter_x3_ssdo"
-runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-    "${RUN_SCENIC_BENCHMARK}"                \
-    "${OUT_DIR}"                             \
-    "${OUT_DIR}/${BENCHMARK}.json"           \
-    "${BENCHMARK}"                           \
-    "${IMAGE_GRID_FLUTTER_X3_COMMAND}" --screen_space_shadows --clipping_enabled
+runbench_exec "${OUT_DIR}/${BENCHMARK}.json"  \
+    "${RUN_SCENIC_BENCHMARK}"                 \
+    --out_dir "${OUT_DIR}"                    \
+    --out_file "${OUT_DIR}/${BENCHMARK}.json" \
+    --benchmark_label "${BENCHMARK}"          \
+    --cmd "${IMAGE_GRID_FLUTTER_X3_COMMAND}"  \
+    --screen_space_shadows --clipping_enabled
 
 BENCHMARK="fuchsia.scenic.image_grid_flutter_x3_shadow_map"
-runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-    "${RUN_SCENIC_BENCHMARK}"                \
-    "${OUT_DIR}"                             \
-    "${OUT_DIR}/${BENCHMARK}.json"           \
-    "${BENCHMARK}"                           \
-    "${IMAGE_GRID_FLUTTER_X3_COMMAND}" --shadow_map --clipping_enabled
+runbench_exec "${OUT_DIR}/${BENCHMARK}.json"  \
+    "${RUN_SCENIC_BENCHMARK}"                 \
+    --out_dir "${OUT_DIR}"                    \
+    --out_file "${OUT_DIR}/${BENCHMARK}.json" \
+    --benchmark_label "${BENCHMARK}"          \
+    --cmd "${IMAGE_GRID_FLUTTER_X3_COMMAND}"  \
+    --shadow_map --clipping_enabled
 
 BENCHMARK="fuchsia.scenic.image_grid_flutter_x3_moment_shadow_map"
-runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-    "${RUN_SCENIC_BENCHMARK}"                \
-    "${OUT_DIR}"                             \
-    "${OUT_DIR}/${BENCHMARK}.json"           \
-    "${BENCHMARK}"                           \
-    "${IMAGE_GRID_FLUTTER_X3_COMMAND}" --moment_shadow_map --clipping_enabled
+runbench_exec "${OUT_DIR}/${BENCHMARK}.json"  \
+    "${RUN_SCENIC_BENCHMARK}"                 \
+    --out_dir "${OUT_DIR}"                    \
+    --out_file "${OUT_DIR}/${BENCHMARK}.json" \
+    --benchmark_label "${BENCHMARK}"          \
+    --cmd "${IMAGE_GRID_FLUTTER_X3_COMMAND}"  \
+    --moment_shadow_map --clipping_enabled
 
 # choreography
 CHOREOGRAPHY_COMMAND="run device_runner --test --enable_presenter --account_provider=dev_token_manager --device_shell=dev_device_shell --device_shell_args=--test_timeout_ms=60000 --user_shell=dev_user_shell --user_shell_args=--root_module=choreography --story_shell=mondrian"
 BENCHMARK="fuchsia.scenic.choreography_noclipping_noshadows"
-runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-    "${RUN_SCENIC_BENCHMARK}"                \
-    "${OUT_DIR}"                             \
-    "${OUT_DIR}/${BENCHMARK}.json"           \
-    "${BENCHMARK}"                           \
-    "${CHOREOGRAPHY_COMMAND}" --unshadowed --clipping_disabled
+runbench_exec "${OUT_DIR}/${BENCHMARK}.json"  \
+    "${RUN_SCENIC_BENCHMARK}"                 \
+    --out_dir "${OUT_DIR}"                    \
+    --out_file "${OUT_DIR}/${BENCHMARK}.json" \
+    --benchmark_label "${BENCHMARK}"          \
+    --cmd "${CHOREOGRAPHY_COMMAND}"           \
+    --unshadowed --clipping_disabled
 
 BENCHMARK="fuchsia.scenic.choreography_noshadows"
-runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-    "${RUN_SCENIC_BENCHMARK}"                \
-    "${OUT_DIR}"                             \
-    "${OUT_DIR}/${BENCHMARK}.json"           \
-    "${BENCHMARK}"                           \
-    "${CHOREOGRAPHY_COMMAND}" --unshadowed --clipping_enabled
+runbench_exec "${OUT_DIR}/${BENCHMARK}.json"  \
+    "${RUN_SCENIC_BENCHMARK}"                 \
+    --out_dir "${OUT_DIR}"                    \
+    --out_file "${OUT_DIR}/${BENCHMARK}.json" \
+    --benchmark_label "${BENCHMARK}"          \
+    --cmd "${CHOREOGRAPHY_COMMAND}"           \
+    --unshadowed --clipping_enabled
 
 BENCHMARK="fuchsia.scenic.choreography_ssdo"
-runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-    "${RUN_SCENIC_BENCHMARK}"                \
-    "${OUT_DIR}"                             \
-    "${OUT_DIR}/${BENCHMARK}.json"           \
-    "${BENCHMARK}"                           \
-    "${CHOREOGRAPHY_COMMAND}" --screen_space_shadows --clipping_enabled
+runbench_exec "${OUT_DIR}/${BENCHMARK}.json"  \
+    "${RUN_SCENIC_BENCHMARK}"                 \
+    --out_dir "${OUT_DIR}"                    \
+    --out_file "${OUT_DIR}/${BENCHMARK}.json" \
+    --benchmark_label "${BENCHMARK}"          \
+    --cmd "${CHOREOGRAPHY_COMMAND}"           \
+    --screen_space_shadows --clipping_enabled
 
 BENCHMARK="fuchsia.scenic.choreography_shadow_map"
-runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-    "${RUN_SCENIC_BENCHMARK}"                \
-    "${OUT_DIR}"                             \
-    "${OUT_DIR}/${BENCHMARK}.json"           \
-    "${BENCHMARK}"                           \
-    "${CHOREOGRAPHY_COMMAND}" --shadow_map --clipping_enabled
+runbench_exec "${OUT_DIR}/${BENCHMARK}.json"  \
+    "${RUN_SCENIC_BENCHMARK}"                 \
+    --out_dir "${OUT_DIR}"                    \
+    --out_file "${OUT_DIR}/${BENCHMARK}.json" \
+    --benchmark_label "${BENCHMARK}"          \
+    --cmd "${CHOREOGRAPHY_COMMAND}"           \
+    --shadow_map --clipping_enabled
 
 BENCHMARK="fuchsia.scenic.choreography_moment_shadow_map"
-runbench_exec "${OUT_DIR}/${BENCHMARK}.json" \
-    "${RUN_SCENIC_BENCHMARK}"                \
-    "${OUT_DIR}"                             \
-    "${OUT_DIR}/${BENCHMARK}.json"           \
-    "${BENCHMARK}"                           \
-    "${CHOREOGRAPHY_COMMAND}" --moment_shadow_map --clipping_enabled
+runbench_exec "${OUT_DIR}/${BENCHMARK}.json"  \
+    "${RUN_SCENIC_BENCHMARK}"                 \
+    --out_dir "${OUT_DIR}"                    \
+    --out_file "${OUT_DIR}/${BENCHMARK}.json" \
+    --benchmark_label "${BENCHMARK}"          \
+    --cmd "${CHOREOGRAPHY_COMMAND}"           \
+    --moment_shadow_map --clipping_enabled
