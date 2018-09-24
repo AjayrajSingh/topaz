268be5095cb8df4a4ec8ab8cc1edee4eef090fbc
Brian Goldman <bgoldman@google.com>
2018-09-23 19:32:58 -0700

    [repl] REPL server
    The REPL server is a test process that connects to multiple components
    and interacts with them.
    
    This is phase one, with a shell script that runs a device_runner command
    based on driver_example_mod_test. In future CLs, the REPL server will get
    its own test binary to operate from.
    
    Test: Added a debugger() line to driver_example_mod_test.dart and
    followed the instructions in tools/repl/lib/main.dart
    
    Change-Id: Ib44570d46e4793f436ecac82bce96152e933b694

---
+++ bin/repl/run_repl_server.sh
@@ -0,0 +1,22 @@
+#!/boot/bin/sh
+# Copyright 2018 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -o errexit
+
+# In the event of previous failures from other scripts or other components using
+# the GPU, ensure that all components using the display have been shut down.
+killall device_runner || true
+killall root_presenter || true
+killall scenic || true
+
+# TODO(bgoldman): Create a separate test instead of driver_example_mod_target_tests
+run_test \
+  device_runner --test --enable_presenter \
+  --account_provider=dev_token_manager \
+  --device_shell=dev_device_shell \
+  --device_shell_args=--test_timeout_ms=3600000 \
+  --user_shell=dev_user_shell \
+  --user_shell_args=--root_module=test_driver_module,--module_under_test_url=driver_example_mod_wrapper,--test_driver_url=driver_example_mod_target_tests \
+  --story_shell=dev_story_shell
