b3b3715e1d9a5204aaca67cc441ddfd5d7df5e48
Bryce Lee <brycelee@google.com>
2018-09-26 18:43:03 +0000

    Revert "[setui] Make Step JSON serializable."
    This reverts commit faac523f81149ffbb3294595df3dc3e17829277c.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > [setui] Make Step JSON serializable.
    > 
    > This change refactors Step into a JSON friendly object. As a result,
    > direct references to other Steps have been removed, replaced by
    > key references. It is now the responsibility of the Syllabus to resolve
    > keys into Steps.
    > 
    > Test: fx run-host-tests lib_setui_common_test
    > Change-Id: I3b970166dd397e166c23640881160c91483f6348
    
    TBR=anwilson@google.com,ejia@google.com,iamjack@google.com,brycelee@google.com
    
    Change-Id: I927951fba36f99cbd3def8e274fe2898b858697e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true

--- lib/setui/common/lib/step.dart
+++ lib/setui/common/lib/step.dart
@@ -9,14 +9,8 @@
 /// result and/or a default one defined. A [Step] is considered the final [Step]
 /// if there is no valid [Step] to transition to.
 class Step {
-  static const String _actionJsonKey = 'action';
-  static const String _keyJsonKey = 'key';
-  static const String _defaultTransitionKey = 'default_transition';
-  static const String _resultsKey = 'result';
-
-  String _defaultTransition;
-
-  final Map<String, String> _results = {};
+  Step _defaultTransition;
+  final Map<String, Step> _results = {};
 
   /// A unique identifier within the scope of the defined flow.
   final String key;
@@ -24,49 +18,32 @@ class Step {
   /// The action associated with the [Step].
   final String action;
 
-  Step(this.key, this.action, {Map<String, dynamic> results}) {
-    results?.forEach((key, action) => _results[key] = action);
-  }
-
-  factory Step.fromJson(Map<String, dynamic> json) {
-    final Step step = Step(json[_keyJsonKey], json[_actionJsonKey],
-        results: json[_resultsKey])
-      ..defaultTransition = json[_defaultTransitionKey];
-
-    return step;
-  }
-
-  Map<String, dynamic> toJson() => {
-        _keyJsonKey: key,
-        _actionJsonKey: action,
-        _defaultTransitionKey: _defaultTransition,
-        _resultsKey: _results,
-      };
+  Step(this.key, this.action);
 
   /// Returns the next [Step] to proceed to based on the [result].
-  String getNext([String result]) {
-    final String searchVal = _results[result];
+  Step getNext([String result]) {
+    final Step searchVal = _results[result];
     return searchVal != null || _defaultTransition == null
         ? searchVal
         : _defaultTransition;
   }
 
   /// Sets the default [Step] to move if no match is found in the set results.
-  set defaultTransition(String key) => _defaultTransition = key;
+  set defaultTransition(Step transition) => _defaultTransition = transition;
 
   /// Adds a transition from this [Step] for a given [resultCode]. When this
   /// step returns, the mappings specified here are used to determine the next
   /// [Step] to visit.
-  void addResult(String resultCode, String resultKey) {
-    _results[resultCode] = resultKey;
+  void addResult(String resultCode, Step result) {
+    _results[resultCode] = result;
   }
 
   @override
   String toString() => 'key: $key defaultTransition:$_defaultTransition}';
 
   /// Returns all next steps. Used by syllabus to discover all routes
-  Set<String> get nextSteps {
-    final Set<String> steps = new Set()..addAll(_results.values);
+  Set<Step> get nextSteps {
+    final Set<Step> steps = new Set()..addAll(_results.values);
 
     if (_defaultTransition != null) {
       steps.add(_defaultTransition);
@@ -74,10 +51,4 @@ class Step {
 
     return steps;
   }
-
-  @override
-  int get hashCode => key.hashCode;
-
-  @override
-  bool operator ==(Object other) => other is Step && other.key == key;
 }
--- lib/setui/common/test/step_test.dart
+++ lib/setui/common/test/step_test.dart
@@ -1,7 +1,6 @@
 // Copyright 2018 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-import 'dart:convert';
 
 import 'package:lib_setui_common/step.dart';
 import 'package:test/test.dart';
@@ -9,8 +8,8 @@ import 'package:test/test.dart';
 void main() {
   // Verify basic [StepBuilder] functionality.
   test('test_builder', () {
-    const String childKey1 = 'step1';
-    const String childKey2 = 'step2';
+    final Step child1 = new Step('step1', 'action1');
+    final Step child2 = new Step('step2', 'action2');
 
     const String result1 = 'result1';
     const String result2 = 'result2';
@@ -20,8 +19,8 @@ void main() {
 
     // Create a parent node with two unique child actions.
     final Step parent = new Step(name, action)
-      ..addResult(result1, childKey1)
-      ..addResult(result2, childKey2);
+      ..addResult(result1, child1)
+      ..addResult(result2, child2);
 
     // Ensure name is properly set.
     expect(parent.key, name);
@@ -30,48 +29,36 @@ void main() {
     expect(parent.action, action);
 
     // Check mappings from result to child step.
-    expect(parent.getNext(result1), childKey1);
-    expect(parent.getNext(result2), childKey2);
+    expect(parent.getNext(result1), child1);
+    expect(parent.getNext(result2), child2);
 
     // Expect nothing to be returned without a default set.
     expect(parent.getNext('unknown'), null);
 
     // Make sure next steps size and members is consistent.
-    final Set<String> nextSteps = parent.nextSteps;
+    final Set<Step> nextSteps = parent.nextSteps;
 
     expect(nextSteps.length, 2);
-    expect(nextSteps.contains(childKey1), true);
-    expect(nextSteps.contains(childKey2), true);
+    expect(nextSteps.contains(child1), true);
+    expect(nextSteps.contains(child2), true);
   });
 
   // Verify default step behavior.
   test('test_default', () {
-    const String childKey = 'step1';
-    const String defaultKey = 'defaultKey';
+    final Step child1 = new Step('step1', 'action1');
+    final Step defaultStep = new Step('default', 'action');
 
     const String result1 = 'result1';
 
     // Create parent with a single child node.
     final Step parent = new Step('parent', 'start')
-      ..addResult(result1, childKey)
-      ..defaultTransition = defaultKey;
+      ..addResult(result1, child1)
+      ..defaultTransition = defaultStep;
 
     // Ensure default is returned when no matching Step is found.
-    expect(parent.getNext('unknown'), defaultKey);
+    expect(parent.getNext('unknown'), defaultStep);
 
     // Make sure the mapping still works in the presence of a default.
-    expect(parent.getNext(result1), childKey);
-  });
-
-  // Ensures JSON encoding and decoding works properly
-  test('test_json', () {
-    final Step step1 = Step('fooKey', 'barAction')
-      ..defaultTransition = 'bazDefault'
-      ..addResult('result1', 'result1Key')
-      ..addResult('result2', 'result2Key');
-
-    final Step step2 = Step.fromJson(jsonDecode(jsonEncode(step1)));
-
-    expect(step1, step2);
+    expect(parent.getNext(result1), child1);
   });
 }
