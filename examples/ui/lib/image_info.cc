1dc02b4a95bad540e267eab7e53496f8825dad1f
Dustin Green <dustingreen@google.com>
2018-10-05 23:40:54 -0700

    [Codec] Temp "default" until YV12 rolls
    Tested: CQ + a previous identical change during add of NV12
    Change-Id: If156389dbdb42930f7c0e533a209588b03409377

--- examples/ui/lib/image_info.cc
+++ examples/ui/lib/image_info.cc
@@ -18,6 +18,8 @@ SkImageInfo MakeSkImageInfo(const fuchsia::images::ImageInfo& image_info) {
                                kBGRA_8888_SkColorType, kOpaque_SkAlphaType);
     case fuchsia::images::PixelFormat::YUY2:
     case fuchsia::images::PixelFormat::NV12:
+    // TODO(dustingreen): Remove after YV12 rolls in.
+    default:
       FXL_CHECK(false) << "Not implemented: "
                        << static_cast<int>(image_info.pixel_format);
       return SkImageInfo();
