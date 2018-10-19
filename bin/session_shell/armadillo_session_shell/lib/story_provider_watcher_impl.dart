f51521954ece1c08022194e918b290376afef3b3
David Murphy <dave.murphy@gmail.com>
2018-10-18 20:37:39 -0700

    [story_shell] Add onHandlingBackGesture for session shell coordination
    Adds a signal Story Shell and Session Shell can use to coordinate
    gesture handling on Story state that indicates whether the Story Shell
    wants to use back gestures to dismiss mods, or can defer to Session
    Shell for dismissing stories.
    
    Whenever the handling state changes, onHandlingBackGesture is triggered
    which precipitates a change in story state.
    
    Test: manual
    
    Change-Id: I94f51e048d968ac187f017cf6ba3d9aba16f5436

--- bin/session_shell/armadillo_session_shell/lib/story_provider_watcher_impl.dart
+++ bin/session_shell/armadillo_session_shell/lib/story_provider_watcher_impl.dart
@@ -22,9 +22,12 @@ class StoryProviderWatcherImpl extends StoryProviderWatcher {
   StoryProviderWatcherImpl({this.onStoryChanged, this.onStoryDeleted});
 
   @override
-  void onChange(StoryInfo storyInfo,
-                StoryState storyState,
-                StoryVisibilityState storyVisibilityState) {
+  void onChange(
+    StoryInfo storyInfo,
+    StoryState storyState,
+    StoryVisibilityState storyVisibilityState,
+    bool handleBackGesture,
+  ) {
     // TODO(SY-684): Handle story visibility state changes.
     onStoryChanged?.call(storyInfo, storyState);
   }
