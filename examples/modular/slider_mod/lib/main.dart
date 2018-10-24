5556706055e0ed8512acf5734b1607924e0809c1
Chase Latta <chaselatta@google.com>
2018-10-24 12:56:54 -0700

    [dart-sdk] Add initial API for fuchsia_logger
    TEST= manually tested by calling logs and seeing
    them show up in stdout.
    
    Change-Id: I78487f9ab14509c5638cd315ef4016f9b47435c5

--- examples/modular/slider_mod/lib/main.dart
+++ examples/modular/slider_mod/lib/main.dart
@@ -2,24 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/material.dart';
+import 'package:fuchsia_logger/logger.dart';
 import 'package:fuchsia_modular/module.dart';
-import 'src/blocs/app_bloc.dart';
-import 'src/blocs/bloc_provider.dart';
-import 'src/widgets/slider_scaffold.dart';
+
+import 'src/handlers/root_intent_handler.dart';
 
 void main() {
-
-  // TODO replace the existing code with this line when MF-19 is fixed
-  // Module().registerIntentHandler(RootIntentHandler());
-
-  Module();
-  runApp(
-    MaterialApp(
-      home: BlocProvider<AppBloc>(
-        bloc: AppBloc(),
-        child: SliderScaffold(),
-      ),
-    ),
-  );
+  setupLogger(name: 'slider_mod');
+  Module().registerIntentHandler(RootIntentHandler());
 }
---
+++ public/dart/fuchsia_logger/test/internal/log_writer_test.dart
@@ -0,0 +1,16 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:test/test.dart';
+
+void main() {
+  //TODO(MS-2256) need to implement tests
+  group('fuchsia log writer tests', () {
+    test('placeholder', () {});
+  });
+
+  group('stdout log writer tests', () {
+    test('placeholder', () {});
+  });
+}
