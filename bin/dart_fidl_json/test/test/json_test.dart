6ba182aa6119785728e3f3de33ab32239713e394
Bryce Lee <brycelee@google.com>
2018-10-29 12:15:29 -0700

    [fidl][json] Add proper support for numerical array/vectors.
    Previously, fidl json would parse and assign numerical array/vectors
    as a list of the primitive type. FIDL generation however uses
    specialized lists defined in dart:typed_data. This changelist corrects
    this conversion.
    
    Test: fx run-host-tests dart_fidl_json_test
    Change-Id: I272230136bdf549b7622ad27b4f1f5b3b0e59edb

--- bin/dart_fidl_json/test/test/json_test.dart
+++ bin/dart_fidl_json/test/test/json_test.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:fidl_dart_fidl_json_test_fidl_json/json.dart';
 import 'package:fidl_test_dart_fidl_json/fidl.dart';
@@ -20,16 +21,24 @@ void main() {
 
   // Ensure struct encode/decode.
   test('test_struct', () async {
-    const ExampleStruct struct =
-        ExampleStruct(bar: 1, foo: 'test', structs: null, vals: ['foo', 'bar']);
+    ExampleStruct struct = ExampleStruct(
+        bar: 1,
+        foo: 'test',
+        structs: null,
+        vals: ['foo', 'bar'],
+        integers: Uint32List.fromList([1, 2]));
 
     expect(
         struct,
         ExampleStructConverter.fromJson(
             jsonDecode(jsonEncode(ExampleStructConverter.toJson(struct)))));
 
-    const ExampleStruct structWithVector = ExampleStruct(
-        bar: 1, foo: 'test', structs: [ExampleStruct2(baz: 2)], vals: ['foo']);
+    ExampleStruct structWithVector = ExampleStruct(
+        bar: 1,
+        foo: 'test',
+        structs: [ExampleStruct2(baz: 2)],
+        vals: ['foo'],
+        integers: Uint32List.fromList([3, 4]));
     expect(
         structWithVector,
         ExampleStructConverter.fromJson(jsonDecode(
@@ -38,8 +47,8 @@ void main() {
 
   // Ensure union encode/decode.
   test('test_union', () async {
-    const ExampleUnion exampleUnion = ExampleUnion.withStruct1(
-        ExampleStruct(bar: 1, foo: 'test', structs: null, vals: null));
+    const ExampleUnion exampleUnion = ExampleUnion.withStruct1(ExampleStruct(
+        bar: 1, foo: 'test', structs: null, vals: null, integers: null));
 
     expect(
         exampleUnion,
