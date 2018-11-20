e7bc921284a9d66bd81509437b585e03b966597c
Jason Campbell <jasoncampbell@google.com>
2018-11-19 23:46:13 -0800

    [cleanup] remove app/contacts
    MS-2283 #done
    
    TEST =
    - fx set x64 --packages topaz/packages/buildbot
    - fx build
    
    Change-Id: I16f8cd3d626072f56b2dfd371387ce4530b125dc

--- app/contacts/modules/contacts_picker/lib/stores.dart
+++
@@ -1,6 +0,0 @@
-// Copyright 2017 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-export 'src/stores/contact_item_store.dart';
-export 'src/stores/contacts_picker_store.dart';
