60e3220d1d1505e7eff164f031344aab0e68017f
Courtney Ligh <cligh@google.com>
2018-12-06 17:02:29 -0800

    [modular-examples] Add example mod to test elevation handling.
    Test=Manually tested.
    
    Change-Id: Iaaf382174a829530f5eb8e5ee9a248e3eac0e64a

---
+++ examples/modular/multilevel_mod/lib/main.dart
@@ -0,0 +1,10 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:fuchsia_modular/module.dart';
+import 'src/handlers/root_intent_handler.dart';
+
+void main() {
+  Module().registerIntentHandler(RootIntentHandler());
+}
