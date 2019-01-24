b612aace2a8a87f7b039dff25fca5da8463d1941
Sarah Chan <spqchan@google.com>
2019-01-24 15:57:45 -0800

    [audio] Reland Audio and SettingStore Migration to FIDL async
    CL was reverted here:
    https://fuchsia-review.googlesource.com/c/topaz/+/248653
    
    There are no new changes. I updated the clients using Audio so
    this CL will no longer break them.
    
    The old versions are kept as legacy code, so that it can still
    be used by stuff using non async FIDL.
    
    Test: manual
    Change-Id: I62192ef9896284bc17df77791fcbdb1087504c93

--- public/lib/settings/lib/setting_store_factory.dart
+++ public/lib/settings/lib/setting_store_factory.dart
@@ -1,40 +1,30 @@
-// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:fidl/fidl.dart';
-import 'package:fidl_fuchsia_devicesettings/fidl.dart';
-import 'package:fidl_fuchsia_sys/fidl.dart';
-import 'package:lib.app.dart/app.dart';
+import 'package:fidl_fuchsia_devicesettings/fidl_async.dart';
+import 'package:fuchsia_services/services.dart';
 import 'package:lib.app.dart/logging.dart';
 import 'package:settings_protos/audio.pb.dart';
 import 'package:settings_protos/setting_store.dart';
 
 /// A factory for accessing the default stores for the setting types.
 class SettingStoreFactory {
-  /// Used by stores that require access to fidl services.
-  final ServiceProvider _provider;
-
   /// Passed into each store to access device settings.
   final DeviceSettingsManagerProxy _deviceSettingsManagerService =
       DeviceSettingsManagerProxy();
 
-  SettingStoreFactory(this._provider) {
-    connectToService(_provider, _deviceSettingsManagerService.ctrl);
-    _deviceSettingsManagerService.ctrl.onConnectionError =
-        _handleSettingsConnectionError;
-    _deviceSettingsManagerService.ctrl.error.then(
-        (ProxyError error) => _handleSettingsConnectionError(error: error));
+  SettingStoreFactory() {
+    try {
+      connectToEnvironmentService(_deviceSettingsManagerService);
+    } catch (error) {
+      log.severe('Unable to connect to device settings service', error);
+    }
   }
 
   /// Returns the setting store for [Audio].
   SettingStore<Audio> createAudioStore() {
     return new SettingStore<Audio>(
         _deviceSettingsManagerService, 'Audio', new Audio());
-  }
-
-  /// Handles connection error to the settings service.
-  void _handleSettingsConnectionError({ProxyError error}) {
-    log.severe('Unable to connect to device settings service', error);
   }
 }
