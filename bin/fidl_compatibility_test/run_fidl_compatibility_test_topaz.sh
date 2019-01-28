ada09ff85156feb119aae3c69ac7e6fa9f33cef1
Leo Lung <leolung@google.com>
2019-01-28 10:29:32 -0800

    [fuchsia-pkg] Convert CQ blocking components to URLs
    TESTED=fx shell '/pkgfs/packages/fidl_compatibility_test_topaz/0/test/run_fidl_compatibility_test_topaz.sh'
    fx shell '/pkgfs/packages/goodbye_dart_test/0/test/goodbye_dart_test'
    
    CF-410 #comment
    
    Change-Id: If302ec108b5024658b1ec433066c7c2d32435e37

--- bin/fidl_compatibility_test/run_fidl_compatibility_test_topaz.sh
+++ bin/fidl_compatibility_test/run_fidl_compatibility_test_topaz.sh
@@ -4,5 +4,5 @@
 # garnet/bin/fidl_compatibility_test/run_fidl_compatibility_test_garnet.sh. (The
 # Garnet file should be similar to this file, but omit the Dart server.)
 
-export FIDL_COMPATIBILITY_TEST_SERVERS=fidl_compatibility_test_server_cpp,fidl_compatibility_test_server_dart,fidl_compatibility_test_server_go,fidl_compatibility_test_server_rust
+export FIDL_COMPATIBILITY_TEST_SERVERS=fuchsia-pkg://fuchsia.con/fidl_compatibility_test_server_cpp#meta/fidl_compatibility_test_server_cpp.cmx,fuchsia-pkg://fuchsia.con/fidl_compatibility_test_server_dart#meta/fidl_compatibility_test_server_dart.cmx,fuchsia-pkg://fuchsia.con/fidl_compatibility_test_server_go#meta/fidl_compatibility_test_server_go.cmx,fuchsia-pkg://fuchsia.con/fidl_compatibility_test_server_rust#meta/fidl_compatibility_test_server_rust.cmx
 /pkgfs/packages/fidl_compatibility_test_bin/0/bin/app
