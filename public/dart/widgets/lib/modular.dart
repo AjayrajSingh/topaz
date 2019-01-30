c76387f7f845ddde19c480108c86e817d8eb1666
Andrew Wilson <apwilson@google.com>
2019-01-29 17:05:26 -0800

    [lib.widgets] Remove obsolete modular related code.
    Test: CQ
    
    Change-Id: If3527c770d9d57d0ba8ab9a79bcd3f759fd2bbbe

--- public/dart/widgets/lib/modular.dart
+++
@@ -1,8 +0,0 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// WARNING(MS-1613): All Fuchsia system dependencies will be separated out from
-// this Flutter specific Dart package. If you need Fuchsia system interactions
-// use //topaz/public/lib/app_driver/dart or the Dart FIDL Bindings directly.
-export 'src/modular/dank_session_shell_widget.dart';
