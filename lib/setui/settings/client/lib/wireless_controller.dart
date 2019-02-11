58800a1961f7b4193334c15f74aea1f29024acdd
Eric Jia <ejia@google.com>
2019-02-11 15:31:29 -0800

    [setui][wifi] update wireless controller to adapt to wifi changes, fuchsia.git edition
    test:compiled
    addressed feedback from pre-fuchsia.git cl
    
    Change-Id: I8ddf1384788337153f9bb0e9c4d81632d836e52d

--- lib/setui/settings/client/lib/wireless_controller.dart
+++ lib/setui/settings/client/lib/wireless_controller.dart
@@ -3,73 +3,88 @@
 // found in the LICENSE file.
 
 import 'package:fidl_fuchsia_setui/fidl.dart';
+import 'package:flutter/widgets.dart';
 import 'package:lib_setui_settings_common/setting_adapter.dart';
 import 'package:lib_setui_settings_common/setting_controller.dart';
 
+/// A controller for interacting with the wireless networks.
 class WirelessController extends SettingController<WirelessState> {
   WirelessController(SettingAdapter adapter) : super(adapter);
+
+  final WirelessUiState uiState = WirelessUiState();
+
+  /// Enter in a password and connect to the currently selected point
+  Future<void> password(String password) async {
+    uiState._passwordSubmitted(password);
+    await _connect();
+  }
+
+  /// Called when the user dismisses the password dialog
+  void dismissPassword() {
+    uiState._passwordDismissed();
+  }
+
+  /// Connects to the specified [WirelessAccessPoint].
+  ///
+  /// If the access point requires password, then show password dialog if needed
+  Future<void> select(WirelessNetwork network) async {
+    if (uiState._networkSelected(network)) {
+      await _connect();
+    }
+  }
+
+  Future<void> _connect() async {
+    // TODO: mutate
+    uiState._reset();
+  }
 }
 
-/// STOPSHIP(brycelee): re-enable once Migration is complete
-// /// A controller for interacting with the wireless networks.
-// class WirelessController extends SettingController<WirelessState> {
-//   WirelessController(SettingAdapter adapter) : super(adapter);
+class WirelessUiState extends ChangeNotifier {
+  final ValueNotifier<CurrentScreen> _screen =
+      ValueNotifier(CurrentScreen.main);
+  final ValueNotifier<WirelessNetwork> _selectedAccessPoint =
+      ValueNotifier<WirelessNetwork>(null);
+  final ValueNotifier<String> _password = ValueNotifier(null);
 
-//   final WirelessUiState uiState = WirelessUiState();
+  WirelessUiState() {
+    _screen.addListener(notifyListeners);
+    _password.addListener(notifyListeners);
+    _selectedAccessPoint.addListener(notifyListeners);
+  }
 
-//   /// Enter in a password and connect to the currently selected point
-//   Future<void> password(String password) async {
-//     uiState._passwordShowing.value = false;
-//     await _connect(uiState.selectedAccessPoint, password: password);
-//   }
+  // Returns true if the selection should result in immediate connection
+  bool _networkSelected(WirelessNetwork network) {
+    _selectedAccessPoint.value = network;
+    if (network.wpaAuth != WpaAuth.noneOpen) {
+      _screen.value = CurrentScreen.passwordShowing;
+    } else {
+      _screen.value = CurrentScreen.connecting;
+      return true;
+    }
+    return false;
+  }
 
-//   /// Called when the user dismisses the password dialog
-//   void dismissPassword() {
-//     _reset();
-//   }
+  void _passwordSubmitted(String password) {
+    _password.value = password;
+    _screen.value = CurrentScreen.connecting;
+  }
 
-//   void _reset() {
-//     uiState._passwordShowing.value = false;
-//     uiState._selectedAccessPoint.value = null;
-//   }
+  void _passwordDismissed() {
+    _reset();
+  }
 
-//   /// Connects to the specified [WirelessAccessPoint].
-//   ///
-//   /// If the access point requires password, then show password dialog if needed
-//   Future<void> connect(WirelessAccessPoint accessPoint) async {
-//     uiState._selectedAccessPoint.value = accessPoint;
+  void _reset() {
+    _screen.value = CurrentScreen.main;
+    _selectedAccessPoint.value = null;
+    _password.value = null;
+  }
 
-//     if (accessPoint.security == WirelessSecurity.secured &&
-//         accessPoint.password == null) {
-//       uiState._passwordShowing.value = true;
-//     } else {
-//       await _connect(accessPoint);
-//     }
-//   }
+  CurrentScreen get screen => _screen.value;
+  WirelessNetwork get selectedAccessPoint => _selectedAccessPoint.value;
+}
 
-//   Future<void> _connect(WirelessAccessPoint accessPoint,
-//       {String password}) async {
-//     final updatedAccessPoint = WirelessAccessPoint.clone(accessPoint,
-//         status: ConnectionStatus.connected, password: password);
-
-//     await update(SettingsObject(
-//         settingType: SettingType.wireless,
-//         data: SettingData.withWireless(
-//             WirelessState(accessPoints: [updatedAccessPoint]))));
-//     _reset();
-//   }
-// }
-
-// class WirelessUiState extends ChangeNotifier {
-//   final ValueNotifier<bool> _passwordShowing = ValueNotifier<bool>(false);
-//   final ValueNotifier<WirelessAccessPoint> _selectedAccessPoint =
-//       ValueNotifier<WirelessAccessPoint>(null);
-
-//   WirelessUiState() {
-//     _passwordShowing.addListener(notifyListeners);
-//     _selectedAccessPoint.addListener(notifyListeners);
-//   }
-
-//   bool get passwordShowing => _passwordShowing.value;
-//   WirelessAccessPoint get selectedAccessPoint => _selectedAccessPoint.value;
-// }
+enum CurrentScreen {
+  main,
+  passwordShowing,
+  connecting,
+}
